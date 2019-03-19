c31d4f2379e965175aa59d52f1f000b189ea584e
Stephen von Takach <steve@acaprojects.com>
2019-03-19 16:00:51 +1100

    initial thoughts on routes

---
+++ src/controllers/build.cr
@@ -0,0 +1,22 @@
+
+class Build < Application
+  # list the available builds / built files
+  def index
+
+  end
+
+  # grab the list of available versions of file / which are built
+  def show
+
+  end
+
+  # build a drvier, optionally based on the version specified
+  def create
+
+  end
+
+  # delete a built driver
+  def delete
+
+  end
+end
---
+++ src/controllers/edit.cr
@@ -0,0 +1,32 @@
+
+class Edit < Application
+  # list of drivers and specs
+  def index
+
+  end
+
+  # contents of a driver or spec
+  def show
+
+  end
+
+  # create a new driver
+  def create
+
+  end
+
+  # update an existing driver or spec
+  def update
+
+  end
+
+  # delete a driver or spec
+  def delete
+
+  end
+
+  # commits changes to the upstream repo
+  post "/commit" do
+
+  end
+end
