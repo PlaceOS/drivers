f8a4983294c2441d22f7118c83a8836bb6cd7390
Stephen von Takach <steve@acaprojects.com>
2019-03-19 16:00:51 +1100

    initial thoughts on routes

---
+++ src/controllers/test.cr
@@ -0,0 +1,17 @@
+
+class Test < Application
+  # Specs available
+  def index
+
+  end
+
+  # Run a spec
+  def create
+
+  end
+
+  # WS watch the output from running specs
+  ws "/output" do |socket|
+
+  end
+end
