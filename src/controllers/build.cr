707addc451b156fd6e498fba02d0cc3598317b14
Stephen von Takach <steve@acaprojects.com>
2019-03-19 22:42:41 +1100

    inital work on test framework API

--- src/controllers/build.cr
+++ src/controllers/build.cr
@@ -1,22 +1,65 @@
-
 class Build < Application
-  # list the available builds / built files
+  # list the available files
   def index
+    io = IO::Memory.new
+    result = Process.run(
+      "git", {"ls-files"},
+      input: Process::Redirect::Close,
+      output: io,
+      error: Process::Redirect::Close
+    )
 
+    head :internal_server_error if result.exit_status != 0
+
+    render json: io.to_s.split("\n").select { |file|
+      file.ends_with?(".cr") && !file.ends_with?("_spec.cr") && file.starts_with?("drivers/")
+    }
   end
 
   # grab the list of available versions of file / which are built
-  def show
+  get "/commits" do
+    driver = params["driver"]
+    count = params["id"]
 
+    io = IO::Memory.new
+    result = Process.run(
+      "git", {"-no-pager", "log", "--format=short", "--no-color", "-n", count, driver},
+      input: Process::Redirect::Close,
+      output: io,
+      error: Process::Redirect::Close
+    )
+
+    head :internal_server_error if result.exit_status != 0
+
+    commits = io.to_s.split("commit ").reject(&.empty?).map(&.split("\n", 3).map &.strip)
+    commits.map! do |commit|
+      {
+        commit:  commit[0],
+        author:  commit[1].split(" ", 2)[1],
+        message: commit[2],
+      }
+    end
+    render json: commits
   end
 
   # build a drvier, optionally based on the version specified
   def create
+    driver = params["driver"]
+    commit = params["commit"]? || "head"
 
+    # https://stackoverflow.com/questions/215718/reset-or-revert-a-specific-file-to-a-specific-revision-using-git
+    result = Process.run("git", {"checkout", commit, "--", driver})
+    head :internal_server_error if result.exit_status != 0
+
+    # complile the driver
+    # TODO::
+
+    # reset the file back to head
+    Process.run("git", {"checkout", "--", driver})
+    head :ok
   end
 
   # delete a built driver
   def delete
-
   end
 end
--- src/controllers/test.cr
+++ src/controllers/test.cr
@@ -1,17 +1,13 @@
-
 class Test < Application
   # Specs available
   def index
-
   end
 
   # Run a spec
   def create
-
   end
 
   # WS watch the output from running specs
   ws "/output" do |socket|
-
   end
 end
