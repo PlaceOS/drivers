83cf2673c52cd8a91092788121c5b4f9570030dc
Stephen von Takach <steve@acaprojects.com>
2019-04-05 23:24:31 +1100

    split out the git commands into their own class

--- src/controllers/application.cr
+++ src/controllers/application.cr
@@ -1,7 +1,2 @@
 abstract class Application < ActionController::Base
-  before_action :set_date_header
-
-  def set_date_header
-    response.headers["Date"] = HTTP.format_time(Time.now)
-  end
 end
--- src/controllers/build.cr
+++ src/controllers/build.cr
@@ -1,17 +1,9 @@
 class Build < Application
   # list the available files
   def index
-    io = IO::Memory.new
-    result = Process.run(
-      "git", {"ls-files"},
-      input: Process::Redirect::Close,
-      output: io,
-      error: Process::Redirect::Close
-    )
+    result = GitCommands.ls
 
-    head :internal_server_error if result.exit_status != 0
-
-    render json: io.to_s.split("\n").select { |file|
+    render json: result.select { |file|
       file.ends_with?(".cr") && !file.ends_with?("_spec.cr") && file.starts_with?("drivers/")
     }
   end
@@ -19,27 +11,9 @@ class Build < Application
   # grab the list of available versions of file / which are built
   get "/commits" do
     driver = params["driver"]
-    count = params["id"]
+    count = (params["id"] || 10).to_i
 
-    io = IO::Memory.new
-    result = Process.run(
-      "git", {"-no-pager", "log", "--format=short", "--no-color", "-n", count, driver},
-      input: Process::Redirect::Close,
-      output: io,
-      error: Process::Redirect::Close
-    )
-
-    head :internal_server_error if result.exit_status != 0
-
-    commits = io.to_s.split("commit ").reject(&.empty?).map(&.split("\n", 3).map &.strip)
-    commits.map! do |commit|
-      {
-        commit:  commit[0],
-        author:  commit[1].split(" ", 2)[1],
-        message: commit[2],
-      }
-    end
-    render json: commits
+    render json: GitCommands.commits(driver, count)
   end
 
   # build a drvier, optionally based on the version specified
@@ -47,19 +21,16 @@ class Build < Application
     driver = params["driver"]
     commit = params["commit"]? || "head"
 
-    # https://stackoverflow.com/questions/215718/reset-or-revert-a-specific-file-to-a-specific-revision-using-git
-    result = Process.run("git", {"checkout", commit, "--", driver})
-    head :internal_server_error if result.exit_status != 0
+    GitCommands.checkout(driver, commit) do
+      # complile the driver
+      # TODO::
+    end
 
-    # complile the driver
-    # TODO::
-
-    # reset the file back to head
-    Process.run("git", {"checkout", "--", driver})
-    head :ok
+    head :created
   end
 
   # delete a built driver
   def delete
+    
   end
 end
