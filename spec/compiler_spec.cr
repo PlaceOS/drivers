93bdbdbe13d16e555147a860290f1f871655079d
Stephen von Takach <steve@acaprojects.com>
2019-04-12 17:59:52 +1000

    add repo cloning support
    add documentation

--- spec/compiler_spec.cr
+++ spec/compiler_spec.cr
@@ -2,10 +2,12 @@ require "./spec_helper"
 
 describe EngineDrivers::Compiler do
   it "should compile a driver" do
+    # Test the executable is created
     result = EngineDrivers::Compiler.build_driver("drivers/aca/spec_helper.cr")
     result[:exit_status].should eq(0)
     File.exists?(result[:executable]).should eq(true)
 
+    # Check it functions as expected
     io = IO::Memory.new
     Process.run(result[:executable], {"-h"},
       input: Process::Redirect::Close,
--- src/engine-drivers/compiler.cr
+++ src/engine-drivers/compiler.cr
@@ -5,8 +5,6 @@ class EngineDrivers::Compiler
   BIN_DIR = "#{Dir.current}/bin/drivers"
 
   @@drivers_dir = Dir.current
-  @@busy = false
-  @@message = "idle"
 
   def self.drivers_dir=(path)
     @@drivers_dir = path
@@ -31,29 +29,24 @@ class EngineDrivers::Compiler
     result = 1
 
     get_lock(repository).synchronize do
-      begin
-        @@busy = true
+      # Make sure we have an actual version hash of the file
+      if commit == "head"
+        commit = EngineDrivers::GitCommands.commits(source_file, 1, repository)[0][:commit]
+      end
 
-        # Make sure we have an actual version hash of the file
-        if commit == "head"
-          commit = EngineDrivers::GitCommands.commits(source_file, 1, repository)[0][:commit]
-        end
-        @@message = "compiling #{source_file} @ #{commit}"
+      # Want to expose some kind of status signalling
+      # @@message = "compiling #{source_file} @ #{commit}"
 
-        exe_output = "#{BIN_DIR}/#{commit}_#{exec_name}"
-        EngineDrivers::GitCommands.checkout(source_file, commit) do
-          result = Process.run(
-            "./bin/exec_from",
-            {repository, "crystal", "build", "-o", exe_output, "./src/build.cr"},
-            {"COMPILE_DRIVER" => source_file},
-            input: Process::Redirect::Close,
-            output: io,
-            error: io
-          ).exit_status
-        end
-      ensure
-        @@busy = false
-        @@message = "idle"
+      exe_output = "#{BIN_DIR}/#{commit}_#{exec_name}"
+      EngineDrivers::GitCommands.checkout(source_file, commit) do
+        result = Process.run(
+          "./bin/exec_from",
+          {repository, "crystal", "build", "-o", exe_output, "./src/build.cr"},
+          {"COMPILE_DRIVER" => source_file},
+          input: Process::Redirect::Close,
+          output: io,
+          error: io
+        ).exit_status
       end
     end
 
@@ -75,32 +68,19 @@ class EngineDrivers::Compiler
     # NOTE:: supports recursive locking so can perform multiple repository
     # operations in a single lock. i.e. clone + shards install
     get_lock(repository).synchronize do
-      begin
-        @@busy = true
-        @@message = "installing shards"
-
-        result = Process.run(
-          "./bin/exec_from",
-          {repository, "shards", "--no-color", "install"},
-          input: Process::Redirect::Close,
-          output: io,
-          error: io
-        ).exit_status
-      ensure
-        @@busy = false
-        @@message = "idle"
-      end
+      result = Process.run(
+        "./bin/exec_from",
+        {repository, "shards", "--no-color", "install"},
+        input: Process::Redirect::Close,
+        output: io,
+        error: io
+      ).exit_status
     end
 
     {
       exit_status: result,
       output:      io.to_s,
     }
-  end
-
-  def self.clone(repository_uri, folder_name, username = nil, password = nil)
-    io = IO::Memory.new
-    result = 1
   end
 
   # Proxy the get lock requests to git commands
