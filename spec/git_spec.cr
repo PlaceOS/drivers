68ecdd0b4eccb1506035dc7b928cd26b05693dce
Stephen von Takach <steve@acaprojects.com>
2019-04-15 19:02:37 +1000

    finalise git clone method
    also add revisions listing spec

--- spec/git_spec.cr
+++ spec/git_spec.cr
@@ -6,4 +6,10 @@ describe EngineDrivers::GitCommands do
     (files.size > 0).should eq(true)
     files.includes?("shard.yml").should eq(true)
   end
+
+  it "should list the revisions to a file in a repository" do
+    changes = EngineDrivers::GitCommands.commits("shard.yml")
+    (changes.size > 0).should eq(true)
+    changes.map { |commit| commit[:subject] }.includes?("restructure application").should eq(true)
+  end
 end
