9bb03f4b0784f51781a8c045ae96b19eb968ec87
Stephen von Takach <steve@acaprojects.com>
2019-05-23 09:35:40 +1000

    add request tracking support

--- src/controllers/application.cr
+++ src/controllers/application.cr
@@ -1,2 +1,10 @@
+require "uuid"
+
 abstract class Application < ActionController::Base
+  before_action :set_request_id
+
+  def set_request_id
+    # Support request tracking
+    response.headers["X-Request-ID"] = request.id = request.headers["X-Request-ID"]? || UUID.random.to_s
+  end
 end
