40ecaab27949f04e53a08c45371298abcbad2e57
Stephen von Takach <steve@acaprojects.com>
2019-05-23 08:45:59 +1000

    complete spec for the build API

--- spec/build_spec.cr
+++ spec/build_spec.cr
@@ -14,11 +14,28 @@ describe Build do
       result.status_code.should eq(201)
     end
 
-    it "should list a driver" do
+    it "should list compiled versions" do
       result = curl("GET", "/build/drivers%2Faca%2Fspec_helper.cr/")
       result.status_code.should eq(200)
       drivers = Array(String).from_json(result.body)
       drivers[0].starts_with?("drivers_aca_spec_helper_cr_").should eq(true)
+    end
+
+    it "should list possible versions" do
+      result = curl("GET", "/build/commits/?driver=drivers%2Faca%2Fspec_helper.cr")
+      result.status_code.should eq(200)
+      commits = JSON.parse(result.body)
+      commits.size.should eq(2)
+    end
+
+    it "should delete all compiled versions of a driver" do
+      result = curl("DELETE", "/build/drivers%2Faca%2Fspec_helper.cr/")
+      result.status_code.should eq(200)
+
+      result = curl("GET", "/build/drivers%2Faca%2Fspec_helper.cr/")
+      result.status_code.should eq(200)
+      drivers = Array(String).from_json(result.body)
+      drivers.size.should eq(0)
     end
   end
 end
