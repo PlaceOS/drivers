a9ff9d6845691c92d106df72a98126d8393addbb
Stephen von Takach <steve@acaprojects.com>
2019-06-12 15:35:51 +1000

    render index.html when loading the root route

---
+++ src/controllers/welcome.cr
@@ -0,0 +1,14 @@
+class Welcome < Application
+  base "/"
+
+  STATIC_FILE_PATH = File.join(File.expand_path(ENV["PUBLIC_WWW_PATH"]? || "./www"), "index.html")
+
+  def index
+    file_path = STATIC_FILE_PATH
+    response.content_type = MIME.from_filename(file_path, "application/octet-stream")
+    response.content_length = File.size(file_path)
+    File.open(file_path) do |file|
+      IO.copy(file, response)
+    end
+  end
+end
