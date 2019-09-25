c3500dd951e78b09197b54b87bf7218af1a01f88
Caspian Baska <caspianbaska@gmail.com>
2019-09-25 16:15:24 +1000

    (helper) factor into module

--- spec/helper_spec.cr
+++ spec/helper_spec.cr
@@ -1,18 +1,21 @@
 require "./spec_helper"
 
-describe EngineDrivers::Helper do
+class Dummy
+  include EngineDrivers::Helper
+end
+
+describe EngineDrivers::Helper, focus: true do
+  dummy = Dummy.new
   it "should list drivers" do
-    helper = EngineDrivers::Helper.new
-    drivers = helper.drivers("private_drivers")
+    drivers = dummy.drivers("private_drivers")
     (drivers.size > 0).should eq(true)
     drivers.includes?("drivers/aca/private_helper.cr").should eq(true)
   end
 
   it "should build a driver" do
-    helper = EngineDrivers::Helper.new
-    commits = helper.commits("drivers/aca/private_helper.cr", "private_drivers")
+    commits = dummy.commits("drivers/aca/private_helper.cr", "private_drivers")
     commit = commits[0][:commit]
-    result = helper.compile_driver("drivers/aca/private_helper.cr", "private_drivers", commit)
+    result = dummy.compile_driver("drivers/aca/private_helper.cr", "private_drivers", commit)
     result[:exit_status].should eq(0)
     # "/Users/steve/Documents/projects/crystal-engine/crystal-engine-drivers/bin/drivers/drivers_aca_private_helper_cr_4f6e0cd"
     result[:executable].ends_with?("/drivers_aca_private_helper_cr_4f6e0cd").should eq(true)
