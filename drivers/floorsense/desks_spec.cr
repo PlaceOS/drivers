21a2de9044affdb2c0d612f52c19b81bf3878a13
Stephen von Takach <steve@acaprojects.com>
2019-10-29 15:29:20 +1100

    doc(floorsense): update spec comments

--- drivers/floorsense/desks_spec.cr
+++ drivers/floorsense/desks_spec.cr
@@ -2,11 +2,12 @@ EngineSpec.mock_driver "Floorsense::Desks" do
   # Send the request
   retval = exec(:get_token)
 
-  # sms should send a HTTP request
+  # We should request a new token from Floorsense
   expect_http_request do |request, response|
     if io = request.body
       data = io.gets_to_end
 
+      # The request is param encoded
       if data == "username=srvc_acct&password=password%21"
         response.status_code = 200
         response.output.puts %({"type":"response","result":true,"message":"Authentication successful","info":{"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzbWFydGFsb2NrLWQ1MGJjZC5sb2NhbGRvbWFpbiIsInN1YiI6ImFjYSIsImF1ZCI6ImFwaSIsImV4cCI6MTU3MjMwODMzMiwiaWF0IjoxNTcyMzA0NzMyfQ.KMlzvjYPFw9e5d5LQjb1BF5R1Je9KkgoigkNOUZnR4U","sessionid":"ace555fe-4914-4203-b0a3-a1a6f532fef7"}})
@@ -19,6 +20,6 @@ EngineSpec.mock_driver "Floorsense::Desks" do
     end
   end
 
-  # What the sms function should return
+  # What the function should return (for use in making further requests)
   retval.get.should eq("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzbWFydGFsb2NrLWQ1MGJjZC5sb2NhbGRvbWFpbiIsInN1YiI6ImFjYSIsImF1ZCI6ImFwaSIsImV4cCI6MTU3MjMwODMzMiwiaWF0IjoxNTcyMzA0NzMyfQ.KMlzvjYPFw9e5d5LQjb1BF5R1Je9KkgoigkNOUZnR4U")
 end
