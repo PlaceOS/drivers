330f67686366672ba95b6d148c7f6cf4135a4777
Stephen von Takach <steve@acaprojects.com>
2020-03-02 01:10:52 +1100

    feat(bose control space): initial work on driver

---
+++ drivers/bose/control_space_serial_spec.cr
@@ -0,0 +1,10 @@
+EngineSpec.mock_driver "Bose::ControlSpaceSerial" do
+  exec(:set_parameter_group, 12)
+  should_send("SS C\r")
+  status[:parameter_group].should eq(12)
+
+  exec(:get_parameter_group)
+  should_send("GS\r")
+  responds("S FF\r")
+  status[:parameter_group].should eq(255)
+end
