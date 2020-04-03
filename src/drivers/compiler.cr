4cc21b07d5806cf02b57e4c8b549d20720e89cb7
Caspian Baska <caspianbaska@gmail.com>
2020-04-03 11:15:59 +1100

    feat(compiler): optionally tag binaries with an id

--- src/drivers/compiler.cr
+++ src/drivers/compiler.cr
@@ -1,150 +1,148 @@
+require "exec_from"
+
 require "./git_commands"
 
 module PlaceOS::Drivers
   class Compiler
-    @@drivers_dir = Dir.current
-    @@repository_dir = File.expand_path("./repositories")
-    @@bin_dir = "#{Dir.current}/bin/drivers"
+    class_property drivers_dir : String = Dir.current
+    class_property repository_dir : String = File.expand_path("./repositories")
+    class_property bin_dir : String = "#{Dir.current}/bin/drivers"
 
-    {% for name in [:drivers_dir, :repository_dir, :bin_dir] %}
-      def self.{{name.id}}
-        @@{{name.id}}
-      end
-
-      def self.{{name.id}}=(path)
-        @@{{name.id}} = path
-      end
-    {% end %}
-
-    def self.is_built?(source_file, commit = "HEAD", repository_drivers = @@drivers_dir)
+    def self.is_built?(
+      source_file : String,
+      commit : String = "HEAD",
+      repository_drivers : String = @@drivers_dir,
+      binary_directory : String = @@bin_dir,
+      id : String? = nil
+    )
       # Make sure we have an actual version hash of the file
       commit = self.normalize_commit(commit, source_file, repository_drivers)
-      executable_path = File.join(@@bin_dir, self.executable_name(source_file, commit))
-      File.exists?(executable_path) ? executable_path : nil
+      executable_path = File.join(binary_directory, self.executable_name(source_file, commit, id))
+
+      executable_path if File.exists?(executable_path)
     end
 
-    # repository is required to have a local `build.cr` file to support compilation
-    def self.build_driver(source_file, commit = "HEAD", repository_drivers = @@drivers_dir, git_checkout = true, debug = false)
+    # Repository is required to have a local `build.cr` file to support compilation
+    def self.build_driver(
+      source_file : String,
+      commit : String = "HEAD",
+      repository_drivers : String = @@drivers_dir,
+      binary_directory : String = @@bin_dir,
+      id : String? = nil,
+      git_checkout : Bool = true,
+      debug : Bool = false
+    )
       # Ensure the bin directory exists
-      Dir.mkdir_p @@bin_dir
-      io = IO::Memory.new
+      Dir.mkdir_p binary_directory
 
       # Make sure we have an actual version hash of the file
       commit = normalize_commit(commit, source_file, repository_drivers)
-      driver_executable = self.executable_name(source_file, commit)
-      executable_path = ""
-      result = 1
+      driver_executable = executable_name(source_file, commit, id)
+      executable_path = nil
 
-      GitCommands.file_lock(repository_drivers, source_file) do
+      result = GitCommands.file_lock(repository_drivers, source_file) do
         git_checkout = false if commit == "HEAD"
 
-        # TODO: Expose some kind of status signalling
+        # TODO: Expose some kind of status signalling compilation
         # @@message = "compiling #{source_file} @ #{commit}"
 
-        executable_path = File.join(@@bin_dir, driver_executable)
+        executable_path = File.join(binary_directory, driver_executable)
         build_script = File.join(repository_drivers, "src/build.cr")
 
         # If we are building head and don't want to check anything out
         # then we can assume we definitely want to re-build the driver
         begin
-          File.delete(executable_path) if !git_checkout
+          File.delete(executable_path) if !git_checkout && File.exists?(executable_path)
         rescue
-          # deleting a non-existant file will raise an exception
-        end
-
-        args = if debug
-                 {repository_drivers, "crystal", "build", "--no-color", "--error-trace", "--debug", "-o", executable_path, build_script}
-               else
-                 {repository_drivers, "crystal", "build", "--no-color", "--error-trace", "-o", executable_path, build_script}
-               end
-
-        compile_proc = ->do
-          result = Process.run(
-            "./bin/exec_from",
-            args,
-            {
-              "COMPILE_DRIVER" => source_file,
-              "DEBUG"          => debug ? "1" : "0",
-            },
-            input: Process::Redirect::Close,
-            output: io,
-            error: io
-          ).exit_code
+          # Deleting a non-existant file will raise an exception
         end
 
         # When developing you may not want to have to commit
         if git_checkout
           GitCommands.checkout(source_file, commit, repository_drivers) do
-            compile_proc.call
+            _compile(repository_drivers, executable_path, build_script, source_file, debug)
           end
         else
-          compile_proc.call
+          _compile(repository_drivers, executable_path, build_script, source_file, debug)
         end
       end
 
       {
-        exit_status: result,
-        output:      io.to_s,
+        exit_status: result[:exit_code],
+        output:      result[:output].to_s,
         driver:      driver_executable,
         version:     commit,
-        executable:  executable_path,
+        executable:  executable_path || "",
         repository:  repository_drivers,
       }
     end
 
-    def self.compiled_drivers(source_file)
-      # Get the executable name without commits to collect all versions
-      exec_base = self.driver_slug(source_file)
+    def self._compile(
+      repository_drivers : String,
+      executable_path : String,
+      build_script : String,
+      source_file : String,
+      debug : Bool
+    )
+      arguments = ["build", "--no-color", "--error-trace", "-o", executable_path, build_script]
+      arguments.insert(1, "--debug") if debug
 
-      Dir.children(@@bin_dir).reject do |file|
-        !file.starts_with?(exec_base) || file.includes?(".")
+      ExecFrom.exec_from(
+        directory: repository_drivers,
+        command: "crystal",
+        arguments: arguments,
+        environment: {
+          "COMPILE_DRIVER" => source_file,
+          "DEBUG"          => debug ? "1" : "0",
+        })
+    end
+
+    # TODO: Accept an optional binary_directory rather than using @@bin_dir
+    def self.compiled_drivers(source_file : String? = nil, id : String? = nil)
+      if source_file.nil?
+        Dir.children(@@bin_dir).reject do |file|
+          file.includes?(".") || File.directory?(file)
+        end
+      else
+        # Get the executable name without commits to collect all versions
+        exec_base = self.driver_slug(source_file)
+        Dir.children(@@bin_dir).select do |file|
+          correct_base = file.starts_with?(exec_base) && !file.includes?(".")
+          # Select for IDs
+          id.nil? ? correct_base : (correct_base && file.ends_with?(id))
+        end
       end
     end
 
-    def self.compiled_drivers
-      Dir.children(@@bin_dir).reject { |file| file.includes?(".") || File.directory?(file) }
-    end
-
-    def self.repositories(working_dir = @@repository_dir)
+    def self.repositories(working_dir : String = @@repository_dir)
       Dir.children(working_dir).reject { |file| File.file?(file) }
     end
 
     # Runs shards install to ensure driver builds will succeed
     def self.install_shards(repository, working_dir = @@repository_dir)
-      io = IO::Memory.new
-      result = 1
-
       repo_dir = File.expand_path(File.join(working_dir, repository))
-
       # NOTE:: supports recursive locking so can perform multiple repository
       # operations in a single lock. i.e. clone + shards install
       GitCommands.repo_lock(repo_dir).write do
         # First check if the dependencies are satisfied
-        result = Process.run(
-          "./bin/exec_from",
-          {repo_dir, "shards", "--no-color", "check"},
-          input: Process::Redirect::Close,
-          output: io,
-          error: io
-        ).exit_code
+        result = ExecFrom.exec_from(repo_dir, "shards", {"--no-color", "check"})
+        check_output = result[:output].to_s
+        check_exit_code = result[:exit_code]
 
-        # Otherwise install shards
-        if result != 0 || !io.to_s.includes?("Dependencies are satisfied")
-          io.clear
-          result = Process.run(
-            "./bin/exec_from",
-            {repo_dir, "shards", "--no-color", "install"},
-            input: Process::Redirect::Close,
-            output: io,
-            error: io
-          ).exit_code
+        if check_exit_code == 0 || check_output.includes?("Dependencies are satisfied")
+          {
+            exit_status: check_exit_code,
+            output:      check_output,
+          }
+        else
+          # Otherwise install shards
+          result = ExecFrom.exec_from(repo_dir, "shards", {"--no-color", "install"})
+          {
+            exit_status: result[:exit_code],
+            output:      result[:output].to_s,
+          }
         end
       end
-
-      {
-        exit_status: result,
-        output:      io.to_s,
-      }
     end
 
     def self.clone_and_install(
@@ -176,8 +174,13 @@ module PlaceOS::Drivers
     end
 
     # Generate executable name from driver file path and commit
-    def self.executable_name(driver_source : String, commit : String)
-      "#{self.driver_slug(driver_source)}_#{commit}"
+    # Optionally provide an id.
+    def self.executable_name(driver_source : String, commit : String, id : String?)
+      if id.nil?
+        "#{self.driver_slug(driver_source)}_#{commit}"
+      else
+        "#{self.driver_slug(driver_source)}_#{commit}_#{id}"
+      end
     end
 
     def self.current_commit(source_file, repository)
