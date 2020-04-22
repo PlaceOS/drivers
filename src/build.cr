f9b5b0f553aa36f5a369c8f2f7e0d1f73fa533b4
Kim Burgess <kim@place.technology>
2020-04-22 22:37:22 +1000

    fix(build): support `crystal docs` tool
    Fixes an issue where running `crystal docs` would result in a compiler
    error and block doc generation.

--- src/build.cr
+++ src/build.cr
@@ -1,8 +1,15 @@
-{% if env("COMPILE_DRIVER").ends_with?("_spec.cr") %}
-  require "driver/driver-specs/runner"
-{% else %}
-  require "driver"
-{% end %}
+{% unless flag?("docs") %}
+  {% driver_src = env("COMPILE_DRIVER") %}
 
-# Dynamically require the desired driver
-{{ ("require \"../" + env("COMPILE_DRIVER") + "\"").id }}
+  {% if driver_src %}
+    {% if driver_src.ends_with?("_spec.cr") %}
+      require "driver/driver-specs/runner"
+    {% else %}
+      require "driver"
+    {% end %}
+
+    require "../{{driver_src.id}}"
+  {% else %}
+    {{ raise "Build target is not set. Use COMPILE_DRIVER env var to specify driver to build." }}
+  {% end %}
+{% end %}
