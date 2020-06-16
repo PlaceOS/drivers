9dff2a50ec5ef45f76cfcd3ffa51d0c99628632b
Philip Kheav <pkheav@Philips-MacBook-Pro.local>
2020-06-16 21:26:30 +0100

    fix(samsung): fix all warnings when running with spec

--- drivers/samsung/displays/md_series_spec.cr
+++ drivers/samsung/displays/md_series_spec.cr
@@ -2,4 +2,6 @@ DriverSpecs.mock_driver "Samsung::Displays::MdSeries" do
   exec(:volume, 24)
   # header + command + id + size + value
   should_send("\xAA\x12\x00\x01\x18")
+  #responds("\xAA\xFF\x00\x03\x41\x12Volume")
+  #status[:volume].should eq(24)
 end
