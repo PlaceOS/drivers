d1eb655eeb2bd53f550074ae15699e058d323452
Stephen von Takach <steve@vontaka.ch>
2020-07-20 16:58:30 +1000

    feat: initial work on cisco meraki driver

---
+++ drivers/cisco/meraki/dashboard_spec.cr
@@ -0,0 +1,18 @@
+DriverSpecs.mock_driver "Cisco::Meraki::Dashboard" do
+  # Send the request
+  retval = exec(:fetch, "/api/v0/organizations")
+
+  # The dashboard should send a HTTP request
+  expect_http_request do |request, response|
+    headers = request.headers
+    if headers["X-Cisco-Meraki-API-Key"]? == "configure for the dashboard API"
+      response.status_code = 202
+      response << %([{"id":"org id","name":"place tech"}])
+    else
+      response.status_code = 401
+    end
+  end
+
+  # Should return the payload
+  retval.get.should eq %([{"id":"org id","name":"place tech"}])
+end
