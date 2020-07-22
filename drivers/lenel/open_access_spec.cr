b1b3125c44473afdd0ae1674138464ca9e0f2af7
Kim Burgess <kim@place.technology>
2020-07-22 22:33:13 +1000

    refactor(lenel): http client pattern
    Implement as POC to demonstrate HTTP::Client compatability.

--- drivers/lenel/open_access_spec.cr
+++ drivers/lenel/open_access_spec.cr
@@ -34,15 +34,12 @@ DriverSpecs.mock_driver "Lenel::OpenAccess" do
   expect_http_request do |req, res|
     req.method.should eq("GET")
     req.path.should eq("/version")
-    res.headers["Content-Type"] = "application/json"
-    res.status_code = 200
-    res.output << <<-JSON
-      {
-        "product_name": "OnGuard 7.6",
-        "product_version": "7.6.001",
-        "version": "1.0"
-      }
-    JSON
+    req.headers["Session-Token"].should eq("abc123")
+    respond_with 200, {
+      product_name: "OnGuard 7.6",
+      product_version: "7.6.001",
+      version: "1.0"
+    }
   end
   version = version.get.not_nil!
   version["product_version"].should eq("7.6.001")
