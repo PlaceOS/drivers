ba965f78d09756447a01514cb498b1e3bf7792f6
Stephen von Takach <steve@vontaka.ch>
2020-09-07 22:01:29 +1000

    feat(meraki): add captive portal

---
+++ drivers/cisco/meraki/captive_portal_spec.cr
@@ -0,0 +1,15 @@
+require "openssl"
+
+DriverSpecs.mock_driver "Cisco::Meraki::CaptivePortal" do
+  date = Time.unix(1599477274).in(Time::Location.load("Australia/Sydney")).to_s("%Y%m%d")
+  hexdigest = OpenSSL::Digest.new("SHA256").update("guest@email.com-#{date}-anything really").hexdigest
+
+  # Check the hex codes match
+  retval = exec(:generate_guest_data, "guest@email.com", 1599477274, "Australia/Sydney")
+  retval.get.should eq hexdigest
+
+  # check it matches on of the codes
+  codes = hexdigest.scan(/.{4}/).map { |code| code[0] }
+  retval = exec(:generate_guest_token, "guest@email.com", 1599477274, "Australia/Sydney")
+  codes.includes?(retval.get.not_nil!.as_s).should eq true
+end
