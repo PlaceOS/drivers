6a2b1974266be91e2ce6fcdfb6edd41472e4c0db
Philip Kheav <pkheav@Philips-MacBook-Pro.local>
2020-09-10 16:26:44 +0100

    fix(nec): add working spec test

--- drivers/nec/display/all_spec.cr
+++ drivers/nec/display/all_spec.cr
@@ -1,3 +1,5 @@
 DriverSpecs.mock_driver "Nec::Display::All" do
-  should_send("\x01\x30\x2A\x30\x41\x30\x36\x02\x30\x31\x44\x36\x03\x1F\x0D")
+  should_send("\x010\x2A0A06\x0201D6\x03\x1F\x0D")
+     responds("\x0100\x2AB12\x020200D60000040001\x03\x1F\x0D")
+  status[:power].should eq(true)
 end
