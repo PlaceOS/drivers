9c8e0160a56d8ae03aab70c2de9993b9e6797733
Philip Kheav <pkheav@Philips-MacBook-Pro.local>
2020-09-25 17:57:51 +0100

    feat(nec): add spec for power control

--- drivers/nec/display/display_spec.cr
+++ drivers/nec/display/display_spec.cr
@@ -2,6 +2,11 @@ DriverSpecs.mock_driver "Nec::Display::All" do
   # do_poll
   # power?
   should_send("\x010*0A06\x0201D6\x03\x1F\x0D")
-     responds("\x0100*B12\x020200D60000040001\x03\x1F\x0D")
+     responds("\x0100*B12\x020200D60000040004\x03\x1A\x0D")
+  status[:power].should eq(false)
+
+  exec(:power, true)
+  should_send("\x010*0A0C\x02C203D60001\x03\x18\x0D")
+     responds("\x0100*B0E\x0200C203D60001\x03\x1D\x0D")
   status[:power].should eq(true)
 end
