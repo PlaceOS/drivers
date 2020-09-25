e144c601250620958f0a865bc37890e64d827b1a
Philip Kheav <pkheav@Philips-MacBook-Pro.local>
2020-09-25 15:48:10 +0100

    fix(nec): don't cast type when not needed

--- drivers/nec/display/display_spec.cr
+++ drivers/nec/display/display_spec.cr
@@ -1,4 +1,6 @@
 DriverSpecs.mock_driver "Nec::Display::All" do
+  # do_poll
+  # power?
   should_send("\x010*0A06\x0201D6\x03\x1F\x0D")
      responds("\x0100*B12\x020200D60000040001\x03\x1F\x0D")
   status[:power].should eq(true)
