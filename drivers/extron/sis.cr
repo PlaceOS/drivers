c05f54d4039844795e13ca2be8cd07c5e4879720
Kim Burgess <kim@place.technology>
2020-09-28 18:18:17 +1000

    simplify SIS module

--- drivers/extron/sis.cr
+++ drivers/extron/sis.cr
@@ -1,5 +1,3 @@
-require "./sis/*"
-
 # Implementation, types and utilities for working with the Extron Simple
 # Instruction Set (SIS) device control protocol.
 #
@@ -7,11 +5,10 @@ require "./sis/*"
 # processing and general audio-visual products via SSH, telnet and serial
 # control.
 module Extron::SIS
-  DELIMITER = 0x0d
+  TELNET_PORT = 21
+  SSH_PORT = 22023
 
-  # Transmitted when a connection is opened with the device.
-  struct CopyrightMessage
-  end
+  DELIMITER = "\r\n"
 
   # Illegal characters for use in property names.
   SPECIAL_CHARS = "+-,@=‘[]{}<>`“;:|\?".chars
@@ -33,6 +30,10 @@ module Extron::SIS
     FileNotFound = 28
   end
 
+  alias Input = UInt8
+
+  alias Output = UInt8
+
   # Layers for targetting signal distribution operations.
   enum SwitchLayer : UInt8
     All = 0x21 # '!'
@@ -44,5 +45,45 @@ module Extron::SIS
     end
   end
 
+  # Struct for representing a signal path.
+  record Route, input : Input, output : Output, layer = SwitchLayer::All do
+    def to_s(io : IO)
+      io << input
+      io << '*'
+      io << output
+      io << layer
+    end
+  end
+
+  # Structure for representing a SIS device command.
+  #
+  # Commands are composed from a set of *fields*. The contents and types of these
+  # are arbitrary, however they must be capable of serialising to an IO.
+  struct Command(*T)
+    def initialize(*fields : *T)
+      @fields = fields
+    end
+
+    # Serialises `self` in a format suitable for log messages.
+    def to_s(io : IO)
+      io << '‹'
+      to_io io
+      io << '›'
+    end
+
+    # Writes `self` to the passed *io*.
+    def to_io(io : IO, format = IO::ByteFormat::SystemEndian)
+      @fields.each.flatten.each do |field|
+        io << field
+      end
+    end
+
+    # Syntactical suger for `Command` definition. Provides the ability to express
+    # command fields in the same way as `Byte` objects and other similar
+    # collections from the Crystal std lib.
+    macro [](*fields)
+      Extron::SIS::Command.new {{*fields}}
+    end
+  end
 end
 
--- drivers/extron/sis_spec.cr
+++ drivers/extron/sis_spec.cr
@@ -4,26 +4,25 @@ require "./sis"
 describe Extron::SIS::Command do
   it "forms a Command from arbitrary field types" do
     command = Extron::SIS::Command.new 42, 'a', "foo"
-    command.fields[0].should eq(42)
-    command.fields[1].should eq('a')
-    command.fields[2].should eq("foo")
   end
 
   it "serialises the command to an IO" do
-    command = Extron::SIS::Command.tie 1, 2
+    command = Extron::SIS::Command[1, '*', 2, Extron::SIS::SwitchLayer::All]
     io = IO::Memory.new
     io.write_bytes command
     io.to_s.should eq("1*2!")
   end
 
   it "provides a string representation suitable for logging" do
-    command = Extron::SIS::Command.tie 1, 2
+    command = Extron::SIS::Command[1, '*', 2, Extron::SIS::SwitchLayer::All]
     command.to_s.should eq("‹1*2!›")
   end
 
-  it "performs bounds checks for args" do
-    expect_raises(ArgumentError) do
-      Extron::SIS::Command.tie -1
-    end
+  it "flattens nested fields" do
+    routes = [Extron::SIS::Route.new(1, 2), Extron::SIS::Route.new(3, 4)]
+    command = Extron::SIS::Command["\e+Q", routes, '\r']
+    io = IO::Memory.new
+    io.write_bytes command
+    io.to_s.should eq("\e+Q1*2!3*4!\r")
   end
 end
