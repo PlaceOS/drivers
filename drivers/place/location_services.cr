52ecce5d444a34007cbd66e92be929a655a5cea0
Stephen von Takach <steve@vontaka.ch>
2020-10-01 18:09:08 +1000

    feat(location services): update to latest interface
    also add additional logging

--- drivers/place/location_services.cr
+++ drivers/place/location_services.cr
@@ -10,9 +10,10 @@ class Place::LocationServices < PlaceOS::Driver
   # Runs through all the services that support the Locatable interface
   # requests location information on the identifier for all of them
   # concatenates the results and returns them as a single array
-  def locate_user(identifier : String)
+  def locate_user(email : String? = nil, username : String? = nil)
+    logger.debug { "searching for #{email}, #{username}" }
     located = [] of JSON::Any
-    system.implementing(Interface::Locatable).locate_user(identifier).get.each do |locations|
+    system.implementing(Interface::Locatable).locate_user(email, username).get.each do |locations|
       located.concat locations.as_a
     end
     located
