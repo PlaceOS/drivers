2049e6eb937d62cbd230925953cebf6fc8c2449b
Stephen von Takach <steve@vontaka.ch>
2020-10-01 15:26:23 +1000

    feat(place location services): add a helper driver
    for collection location information from compatible modules in a system

---
+++ drivers/place/location_services.cr
@@ -0,0 +1,20 @@
+module Place; end
+
+require "placeos-driver/interface/locatable"
+
+class Place::LocationServices < PlaceOS::Driver
+  descriptive_name "PlaceOS Location Services"
+  generic_name :LocationServices
+  description %(collects location data from compatible services and combines the data)
+
+  # Runs through all the services that support the Locatable interface
+  # requests location information on the identifier for all of them
+  # concatenates the results and returns them as a single array
+  def locate_user(identifier : String)
+    located = [] of JSON::Any
+    system.implementing(Interface::Locatable).locate_user(identifier).get.each do |locations|
+      located.concat locations.as_a
+    end
+    located
+  end
+end
