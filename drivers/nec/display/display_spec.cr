8538a9b8c5c6b23b0c3cf86a7aff6a5a6cf6e680
Philip Kheav <pkheav@Philips-MacBook-Pro.local>
2020-10-02 17:23:01 +0100

    fix(nec): add spec for do_poll

--- drivers/nec/display/display_spec.cr
+++ drivers/nec/display/display_spec.cr
@@ -4,22 +4,28 @@ DriverSpecs.mock_driver "Nec::Display::All" do
   should_send("\x010*0A06\x0201D6\x03\x1F\x0D")
      responds("\x0100*B12\x020200D60000040001\x03\x1F\x0D")
   status[:power].should eq(true)
-
-  exec(:mute_status)
+  # mute_status
   should_send("\x010*0C06\x02008D\x03\x12\x0D")
      responds("\x0100*D12\x0200008D0000000002\x03\x12\x0D")
   status[:audio_mute].should eq(false)
+  # volume_status
+  should_send("\x010*0C06\x020062\x03\x6A\x0D")
+     responds("\x0100*D12\x020000620000000032\x03\x69\x0D")
+  status[:volume].should eq(50)
+  # video_input
+  should_send("\x010*0C06\x020060\x03\x68\x0D")
+    responds("\x0100*D12\x020000600000000011\x03\x6A\x0D")
+  status[:input].should eq("Hdmi")
+  # audio_input
+  should_send("\x010*0C06\x02022E\x03\x1B\x0D")
+     responds("\x0100*D12\x0200022E0000000001\x03\x18\x0D")
+  status[:audio].should eq("Audio1")
 
   exec(:mute_audio)
   should_send("\x010*0E0A\x02008D0001\x03\x62\x0D")
      responds("\x0100*F12\x0200008D0000000001\x03\x13\x0D")
   status[:audio_mute].should eq(true)
   status[:volume].should eq(0)
-
-  exec(:volume_status)
-  should_send("\x010*0C06\x020062\x03\x6A\x0D")
-     responds("\x0100*D12\x020000620000000032\x03\x69\x0D")
-  status[:volume].should eq(50)
 
   exec(:volume, 25)
   should_send("\x010*0E0A\x0200620019\x03\x13\x0D")
