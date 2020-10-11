77ae4969523c86152234d172f0dd704542362308
Kim Burgess <kim@place.technology>
2020-10-11 20:56:52 +1000

    switch from pars3k to pars

--- drivers/extron/sis/response.cr
+++ drivers/extron/sis/response.cr
@@ -1,50 +1,50 @@
-require "pars3k"
+require "pars"
 
 # Parsers for responses and asynchronous messages originating from Extron SIS
 # devices.
 module Extron::SIS::Response
-  include Pars3k
+  include Pars
 
   # Parses a response packet with specified *expected* parser.
   #
   # Returns the parser output, a parse error or a device error.
   def self.parse(data : Bytes, as expected : T) forall T
-    (expected / DeviceError).parse String.new(data)
+    (expected | DeviceError).parse String.new(data)
   end
 
-  # :nodoc:
-  def self.rest_of_buffer(full = true)
-    Parser(String).new do |ctx|
-      result = full ? ctx.parsing : ctx.parsing[ctx.position..-1]
-      ParseResult(String).new result, ctx.next
-    end
-  end
+  Delimiter = Parse.string SIS::DELIMITER
+
+  # Parse a full command response as a String
+  Raw = ((Parse.char ^ Delimiter) * (0..) << Delimiter * (0..1)).map &.join
 
   # Error codes returned from the device.
-  DeviceError = Parse.char('E') >> Parse.int.transform { |e| SIS::Error.new e }
+  DeviceError = Parse.char('E') >> Parse.integer.map { |e| SIS::Error.new e.to_i }
 
   # Copyright message shown on connect.
-  Copyright = Parse.string("(c) Copyright") + rest_of_buffer
+  Copyright = (Parse.string("(c) Copyright") + Raw).map &.join
 
   # Part of the copyright banner, but appears on a new line so will tokenize as
   # as standalone message.
-  Clock = rest_of_buffer.transform { |date| Time.parse_utc date, "%a, %b %d, %Y, %T" }
+  Clock = Raw.map { |date| Time.parse_utc date, "%a, %b %d, %Y, %T" }
+
+  # Quick response, occurs following quick tie, or siwtching interaction from
+  # the device's front panel.
+  Qik = Parse.string("Qik") >> Parse.constant(Ok.new nil)
 
   # Signal route update.
-  Tie = do_parse({
-    output <= (Parse.string("Out") >> Parse.int.transform &->Output.new(Int32)),
+  Tie = Parse.do({
+    output <= Parse.string("Out") >> Parse.integer.map &->Input.new(String),
     _ <= Parse.char(' '),
-    input <= (Parse.string("In") >> Parse.int.transform &->Input.new(Int32)),
+    input <= Parse.string("In") >> Parse.integer.map &->Input.new(String),
     _ <= Parse.char(' '),
-    layer <= Parse.word.transform &->SwitchLayer.parse(String),
-    Parse.constant SIS::Tie.new input, output, layer
+    layer <= Parse.word.map &->SwitchLayer.parse(String),
+    Parse.const SIS::Tie.new input, output, layer
   })
 
   # Parses for device messages that can be safely ignored - these exist mainly
   # to flush initial connect banners
-  Ignorable = (Copyright / Clock).transform { |x| Ignored.new x }
-  alias Ignored = Box
+  Ignorable = (Copyright | Clock).map { Ok }
 
   # Async messages that can be expected outside of a command -> response flow.
-  Unsolicited = DeviceError / Tie / Ignorable
+  Unsolicited = DeviceError | Tie | Ignorable
 end
