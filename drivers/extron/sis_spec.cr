1e6fbe32aa840a34484c3f4313c8eb55b3031c3c
Kim Burgess <kim@place.technology>
2020-10-12 17:29:34 +1000

    neaten up sis spec

--- drivers/extron/sis_spec.cr
+++ drivers/extron/sis_spec.cr
@@ -1,73 +1,82 @@
 require "spec"
 require "./sis"
 
-describe Extron::SIS::Command do
+include Extron::SIS
+
+describe Command do
   it "forms a Command from arbitrary field types" do
-    command = Extron::SIS::Command.new 42, 'a', "foo"
+    command = Command.new 42, 'a', "foo"
   end
 
   it "serialises the command to an IO" do
-    command = Extron::SIS::Command[1, '*', 2, Extron::SIS::SwitchLayer::All]
+    command = Command[1, '*', 2, SwitchLayer::All]
     io = IO::Memory.new
     io.write_bytes command
     io.to_s.should eq("1*2!")
   end
 
   it "provides a string representation suitable for logging" do
-    command = Extron::SIS::Command[1, '*', 2, Extron::SIS::SwitchLayer::All]
+    command = Command[1, '*', 2, SwitchLayer::All]
     command.to_s.should eq("‹1*2!›")
   end
 
   it "flattens nested fields" do
     routes = [
-      [1, '*', 2, Extron::SIS::SwitchLayer::All],
-      [3, '*', 4, Extron::SIS::SwitchLayer::All]
+      [1, '*', 2, SwitchLayer::All],
+      [3, '*', 4, SwitchLayer::All]
     ]
-    command = Extron::SIS::Command["\e+Q", routes, '\r']
+    command = Command["\e+Q", routes, '\r']
     io = IO::Memory.new
     io.write_bytes command
     io.to_s.should eq("\e+Q1*2!3*4!\r")
   end
 end
 
-describe Extron::SIS::Response do
-  describe Extron::SIS::Response::DeviceError do
+describe Response do
+  describe Response::DeviceError do
     it "parses to a SIS::Error" do
-      error = Extron::SIS::Response::DeviceError.parse "E17"
-      error.should eq(Extron::SIS::Error::Timeout)
+      error = Response::DeviceError.parse "E17"
+      error.should eq(Error::Timeout)
     end
   end
 
-  describe Extron::SIS::Response::Copyright do
+  describe Response::Copyright do
     it "parses and provides the full banner" do
       message = "(c) Copyright YYYY, Extron Electronics, Model Name, Vx.xx, nn-nnnn-nn"
-      parsed = Extron::SIS::Response::Copyright.parse message
+      parsed = Response::Copyright.parse message
       parsed.should be_a(String)
       parsed.should eq(message)
     end
 
     it "does not parse other messages" do
-      parsed = Extron::SIS::Response::Copyright.parse "foo"
-      parsed.should be_a(Extron::SIS::Response::ParseError)
+      parsed = Response::Copyright.parse "foo"
+      parsed.should be_a(Response::ParseError)
     end
   end
 
-  describe Extron::SIS::Response::Clock do
+  describe Response::Clock do
     it "parses" do
       clock = "Fri, Feb 13, 2009, 23:31:30"
-      parsed = Extron::SIS::Response::Clock.parse clock
+      parsed = Response::Clock.parse clock
       parsed.as(Time).to_unix.should eq(1234567890)
     end
   end
 
-  describe Extron::SIS::Response::Tie do
+  describe Response::Tie do
     it "parses" do
-      tie = Extron::SIS::Response::Tie.parse "Out2 In1 All"
-      tie.should be_a Extron::SIS::Tie
-      tie = tie.as Extron::SIS::Tie
+      tie = Response::Tie.parse "Out2 In1 All"
+      tie.should be_a Tie
+      tie = tie.as Tie
       tie.input.should eq(1)
       tie.output.should eq(2)
-      tie.layer.should eq(Extron::SIS::SwitchLayer::All)
+      tie.layer.should eq(SwitchLayer::All)
+    end
+  end
+
+  describe ".parse" do
+    it "builds a parser that includes device errors" do
+      resp = Response.parse "Out4 In2 Aud", as: Response::Tie
+      typeof(resp).should eq (Tie | Error | Response::ParseError)
     end
   end
 end
