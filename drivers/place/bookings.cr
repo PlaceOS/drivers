5007c7c5c3fa23912ad9bbe970ae5739908a34c5
Philip Kheav <philip.kheav@hotmail.com>
2020-10-19 13:25:09 +0100

    fix(bookings): remove o365 specific attendee status

--- drivers/place/bookings.cr
+++ drivers/place/bookings.cr
@@ -114,13 +114,12 @@ class Place::Bookings < PlaceOS::Driver
     logger.debug { "booking event #{title}, from #{starting}, to #{ending}, in #{@time_zone.name}, on #{@calendar_id}" }
 
     room_is_organizer = owner.nil?
-    room_status = room_is_organizer ? "organizer" : "accepted"
     event = calendar.create_event(
       title,
       starting,
       ending,
       "",
-      [PlaceCalendar::Event::Attendee.new(@calendar_id, @calendar_id, room_status, true, room_is_organizer)],
+      [PlaceCalendar::Event::Attendee.new(@calendar_id, @calendar_id, nil, true, room_is_organizer)],
       @time_zone.name,
       nil,
       @calendar_id
