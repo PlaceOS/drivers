23a7193799bd313de0176953adcfa3b844e4e7c5
Kim Burgess <kim@place.technology>
2020-11-25 12:03:06 +1000

    feat(extron/matrix): base driver for signal distrubution
    Adds Extron matrix switcher driver and base SIS protocol implementation.

---
+++ drivers/extron/sis.cr
@@ -0,0 +1,74 @@
+require "./sis/*"
+
+# Implementation, types and utilities for working with the Extron Simple
+# Instruction Set (SIS) device control protocol.
+#
+# This protocol is used for control of all Extron signal distribution,
+# processing and general audio-visual products via SSH, telnet and serial
+# control.
+module Extron::SIS
+  TELNET_PORT =    21
+  SSH_PORT    = 22023
+
+  DELIMITER = "\r\n"
+
+  # Illegal characters for use in property names.
+  SPECIAL_CHARS = "+-,@=‘[]{}<>`“;:|?".chars
+
+  # Symbolic type for representating a successfull interactions no useful data.
+  struct Ok; end
+
+  # Device error numbers
+  enum Error
+    InvalidInput           =  1
+    InvalidCommand         = 10
+    InvalidPresent         = 11
+    InvalidOutput          = 12
+    InvalidParameter       = 13
+    InvalidForConfig       = 14
+    Timeout                = 17
+    Busy                   = 22
+    PrivilegesViolation    = 24
+    DeviceNotPresent       = 25
+    MaxConnectionsExceeded = 26
+    InvalidEventNumber     = 27
+    FileNotFound           = 28
+
+    def retryable?
+      timeout? || busy?
+    end
+  end
+
+  alias Input = UInt8
+
+  alias Output = UInt8
+
+  # Layers for targetting signal distribution operations.
+  enum SwitchLayer : UInt8
+    All = 0x21 # '!'
+    Aud = 0x24 # '$'
+    Vid = 0x25 # '%'
+    RGB = 0x26 # '&'
+    def includes_video?
+      All || Vid || RGB
+    end
+
+    def includes_audio?
+      All || Aud
+    end
+  end
+
+  # Struct for representing a matrix signal path.
+  record Tie, input : Input, output : Output, layer : SwitchLayer
+
+  # Struct for representing a broadcast signal path, or single output switch.
+  record Switch, input : Input, layer : SwitchLayer
+
+  alias IOSize = UInt16
+
+  # IO capacity for a switching layer.
+  record MatrixSize, inputs : IOSize, outputs : IOSize
+
+  # IO capacity for a full device.
+  record SwitcherInformation, video : MatrixSize, audio : MatrixSize
+end
