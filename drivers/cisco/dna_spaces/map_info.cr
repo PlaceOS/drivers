a27b4edb72c7426f979748eb3f77ce11acf7dd7e
Stephen von Takach <steve@vontaka.ch>
2020-12-01 15:00:13 +1100

    fix(cisco dna spaces): update coordinate system and map dimensions

--- drivers/cisco/dna_spaces/map_info.cr
+++ drivers/cisco/dna_spaces/map_info.cr
@@ -1,5 +1,19 @@
 require "./events"
 
+class Cisco::DNASpaces::Dimension
+  include JSON::Serializable
+
+  getter length : Float64
+  getter width : Float64
+  getter height : Float64
+
+  @[JSON::Field(key: "offsetX")]
+  getter offset_x : Float64
+
+  @[JSON::Field(key: "offsetY")]
+  getter offset_y : Float64
+end
+
 class Cisco::DNASpaces::MapInfo
   include JSON::Serializable
 
@@ -11,4 +25,6 @@ class Cisco::DNASpaces::MapInfo
 
   @[JSON::Field(key: "imageHeight")]
   getter image_height : Float64
+
+  getter dimension : Cisco::DNASpaces::Dimension
 end
