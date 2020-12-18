b73cfe63a4963f884a10dade0a53aa801acc3b58
Philip Kheav <philip.kheav@hotmail.com>
2020-12-18 12:39:07 +0000

    feat(qsc): add mute/fader functions

--- drivers/qsc/q_sys_control.cr
+++ drivers/qsc/q_sys_control.cr
@@ -10,13 +10,16 @@ class Qsc::QSysControl< PlaceOS::Driver
   descriptive_name "QSC Audio DSP External Control"
   generic_name :Mixer
 
+  alias Group = NamedTuple(id: Int32, controls: Set(Int32))
+  alias Ids = Int32 | Array(Int32)
+  alias Val = Int32 | Float64
+
   @username : String? = nil
   @password : String? = nil
   @change_group_id : Int32 = 30
   @em_id : Int32 = 0 # TODO: figure out suitable default
-  alias Group = NamedTuple(id: Int32, controls: Set(Int32))
-  @change_groups = {} of Symbol => Group
   @emergency_subscribe : PlaceOS::Driver::Subscriptions::Subscription? = nil
+  @change_groups = {} of Symbol => Group
 
   def on_load
     on_update
@@ -85,7 +88,7 @@ class Qsc::QSysControl< PlaceOS::Driver
     send("cg #{control_id}\n", **options)
   end
 
-  def set_position(control_id : Int32, position : Int32, ramp_time : Int32? = nil)
+  def set_position(control_id : Int32, position : Int32, ramp_time : Val? = nil)
     if ramp_time
       send("cspr \"#{control_id}\" #{position} #{ramp_time}\n", wait: false)
       schedule.in(ramp_time.seconds + 200.milliseconds) { get_status(control_id) }
@@ -94,7 +97,7 @@ class Qsc::QSysControl< PlaceOS::Driver
     end
   end
 
-  def set_value(control_id : Int32, value : Int32, ramp_time : Int32? = nil, **options)
+  def set_value(control_id : Int32, value : Val, ramp_time : Val? = nil, **options)
     if ramp_time
       send("csvr \"#{control_id}\" #{value} #{ramp_time}\n", **options, wait: false)
       schedule.in(ramp_time.seconds + 200.milliseconds) { get_status(control_id) }
@@ -111,6 +114,71 @@ class Qsc::QSysControl< PlaceOS::Driver
     username ||= @username
     password ||= @password
     send("login #{username} #{password}\n", name: :login, priority: 99)
+  end
+
+  # Used to set a dial number/string
+  def set_string(control_ids : Ids, text : String)
+    ensure_array(control_ids).each do |id|
+      send("css \"#{id}\" \"#{text}\"\n").get
+      self[id] = text
+    end
+  end
+
+  # Used to trigger dialing etc
+  def trigger(action : String)
+    logger.debug { "Sending trigger to Qsys: ct #{action}" }
+    send("ct \"#{action}\"\n", wait: false)
+  end
+
+  # ---------------------
+  # Compatibility Methods
+  # ---------------------
+  def fader(fader_ids : Ids, level : Int32)
+    level = level / 10
+    # TODO: fader_type: :fader
+    ensure_array(fader_ids).each { |f_id| set_value(f_id, level, name: "fader#{f_id}") }
+  end
+
+  def faders(fader_ids : Ids, level : Int32)
+    fader(fader_ids, level)
+  end
+
+  def mute(mute_ids : Ids, state : Bool = true)
+    level = state ? 1 : 0
+    # TODO: fader_type: :mute
+    ensure_array(mute_ids).each { |m_id| set_value(m_id, level) }
+  end
+
+  def mutes(mute_ids : Array(Int32), state : Bool)
+    mute(mute_ids, state)
+  end
+
+  def unmute(mute_ids : Array(Int32))
+    mute(mute_ids, false)
+  end
+
+  def mute_toggle(mute_id : Int32)
+    mute(mute_id, !self["fader#{mute_id}_mute"].try(&.as_bool))
+  end
+
+  def snapshot(name : String, index : Int32, ramp_time : Val = 1.5)
+    send("ssl \"#{name}\" #{index} #{ramp_time}\n", wait: false)
+  end
+
+  def save_snapshot(name : String, index : Int32)
+    send("sss \"#{name}\" #{index}\n", wait: false)
+  end
+
+  # For inter-module compatibility
+  def query_fader(fader_ids : Ids)
+    fad = ensure_array(fader_ids)[0]
+    #TODO
+    get_status(fad)#, fader_type: :fader)
+  end
+
+  def query_faders(fader_ids : Ids)
+    # TODO: fader_type: :fader
+    ensure_array(fader_ids).each { |f_id| get_status(f_id) }
   end
 
   private def create_change_group(name : Symbol) : Group
@@ -131,6 +199,10 @@ class Qsc::QSysControl< PlaceOS::Driver
     send("cgc #{next_id}\n", wait: false)
     send("cgsna #{next_id} 2000\n", wait: false)
     group
+  end
+
+  private def ensure_array(object)
+    object.is_a?(Array) ? object : [object]
   end
 
   def received(data, task)
