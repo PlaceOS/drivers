7eb20b7aec51375a1d8422b399e3e3356cb60aa4
Stephen von Takach <steve@vontaka.ch>
2021-01-12 17:35:16 +1100

    feat(place location services): add IP to Username mappings webhook
    webhook for posting user details

--- drivers/place/location_services.cr
+++ drivers/place/location_services.cr
@@ -1,11 +1,26 @@
 module Place; end
 
+require "json"
 require "placeos-driver/interface/locatable"
 
 class Place::LocationServices < PlaceOS::Driver
   descriptive_name "PlaceOS Location Services"
   generic_name :LocationServices
   description %(collects location data from compatible services and combines the data)
+
+  default_settings({
+    debug_webhook: false,
+  })
+
+  def on_load
+    on_update
+  end
+
+  @debug_webhook : Bool = false
+
+  def on_update
+    @debug_webhook = setting?(Bool, :debug_webhook) || false
+  end
 
   # Runs through all the services that support the Locatable interface
   # requests location information on the identifier for all of them
@@ -51,5 +66,23 @@ class Place::LocationServices < PlaceOS::Driver
       located.concat locations.as_a
     end
     located
+  end
+
+  # ===============================
+  # IP ADDRESS => MAC ADDRESS
+  # ===============================
+  SUCCESS_RESPONSE = {HTTP::Status::OK, {} of String => String, nil}
+
+  # Webhook handler for accepting IP address to username mappings
+  # This data is typically obtained via domain controller logs
+  def ip_mappings(method : String, headers : Hash(String, Array(String)), body : String)
+    logger.debug { "IP mappings webhook received: #{method},\nheaders #{headers},\nbody size #{body.size}" }
+    logger.debug { body } if @debug_webhook
+
+    # ip, username, domain, hostname
+    ip_map = Array(Tuple(String, String, String, String?)).from_json(body)
+    system.implementing(Interface::Locatable).ip_username_mappings(ip_map)
+
+    SUCCESS_RESPONSE
   end
 end
