2448f690902f73eaac0132b1cce0fd06b052cc27
Philip Kheav <philip.kheav@hotmail.com>
2021-01-20 10:51:34 +0000

    feat(p3000): add debugging

--- drivers/kramer/switches/protocol3000_spec.cr
+++ drivers/kramer/switches/protocol3000_spec.cr
@@ -1,3 +1,13 @@
 DriverSpecs.mock_driver "Kramer::Switcher::Protocol3000" do
+  settings({
+    kramer_id: "01"
+  })
+
+  # protocol_handshake
+  should_send("#\r")
+  responds("~01@ OK\x0D\x0A")
+  should_send("#INFO-IO?\r")
+  responds("~01@ OK\x0D\x0A")
+
   exec(:get_machine_info)
 end
