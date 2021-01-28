72218a10ea9a961a8c73b25f720b2a52af9c5323
Philip Kheav <philip.kheav@hotmail.com>
2021-01-28 13:30:12 +0000

    feat(p3000): add specs for on_load/on_update

--- drivers/kramer/switches/protocol3000_spec.cr
+++ drivers/kramer/switches/protocol3000_spec.cr
@@ -1,13 +1,27 @@
 DriverSpecs.mock_driver "Kramer::Switcher::Protocol3000" do
-  settings({
-    kramer_id: "01"
-  })
-
+  # on_load
+  # state
   # protocol_handshake
   should_send("#\r")
   responds("~01@ OK\x0D\x0A")
+  # get_machine_info
   should_send("#INFO-IO?\r")
   responds("~01@ OK\x0D\x0A")
 
-  exec(:get_machine_info)
+  settings({
+    kramer_id: "01",
+    kramer_login: true,
+    kramer_password: "pass"
+  })
+  # on_update
+  # state
+  # protocol_handshake
+  should_send("#01@\r")
+  responds("~01@ OK\x0D\x0A")
+  # login
+  should_send("#01@LOGIN pass\r")
+  responds("~01@ OK\x0D\x0A")
+  # get_machine_info
+  should_send("#01@INFO-IO?\r")
+  responds("~01@ OK\x0D\x0A")
 end
