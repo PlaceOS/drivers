8bd8b359b8d64cb5ac32f1aebcd054c605593f2c
Philip Kheav <philip.kheav@hotmail.com>
2021-01-28 18:08:22 +0000

    fix(p3000): use Int32 for all inputs and outputs

--- drivers/kramer/switches/protocol3000_spec.cr
+++ drivers/kramer/switches/protocol3000_spec.cr
@@ -24,4 +24,20 @@ DriverSpecs.mock_driver "Kramer::Switcher::Protocol3000" do
   # get_machine_info
   should_send("#01@INFO-IO?\r")
   responds("~01@ OK\x0D\x0A")
+
+  exec(:switch_video, 1, [1, 2, 3])
+  should_send("#01@VID 1>1,1>2,1>3\r")
+  responds("~01@VID 1>1,1>2,1>3 OK\x0D\x0A")
+
+  exec(:switch_audio, 2, [1, 2, 3])
+  should_send("#01@AUD 2>1,2>2,2>3\r")
+  responds("~01@AUD 2>1,2>2,2>3 OK\x0D\x0A")
+
+  exec(:route, {1 => [1, 2, 3]})
+  should_send("#01@ROUTE 12,1,1\r")
+  responds("~01@ROUTE 12,1,1 OK\x0D\x0A")
+  should_send("#01@ROUTE 12,2,1\r")
+  responds("~01@ROUTE 12,2,1 OK\x0D\x0A")
+  should_send("#01@ROUTE 12,3,1\r")
+  responds("~01@ROUTE 12,3,1 OK\x0D\x0A")
 end
