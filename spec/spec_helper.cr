01a0a1262a890ef486ee9a92b77014dcb8223be5
Stephen von Takach <steve@vontaka.ch>
2021-02-03 17:03:35 +1100

    fix(spec): update to latest compiler

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -7,11 +7,11 @@ require "../src/config"
 # Helper methods for testing controllers (curl, with_server, context)
 require "../lib/action-controller/spec/curl_context"
 
-require "placeos-compiler/drivers"
+require "placeos-compiler"
 
 Spec.before_suite do
   # Clone the private drivers
-  PlaceOS::Drivers::Compiler.clone_and_install(
+  PlaceOS::Compiler.clone_and_install(
     "private_drivers",
     "https://github.com/placeos/private-drivers"
   )
