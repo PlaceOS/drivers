f7bd7ad78df18247b26f9c5ee450075c5ec1cc74
Philip Kheav <philip.kheav@hotmail.com>
2021-02-10 13:31:19 +0000

    feat(kramer_hdmi): decode docs for basic specs

--- drivers/kramer/switches/vs_hdmi.cr
+++ drivers/kramer/switches/vs_hdmi.cr
@@ -18,10 +18,10 @@ class Kramer::Switcher::VsHdmi < PlaceOS::Driver
   end
 
   private def get_machine_type
-    #         id com,    video
+    #              id com,    video
     command = Bytes[62, 0x81, 0x81, 0xFF]
     send(command, name: :inputs) # num inputs
-    command[1] = 0x82
+    command = Bytes[62, 0x82, 0x81, 0xFF]
     send(command, name: :outputs) # num outputs
   end
 
@@ -52,35 +52,37 @@ class Kramer::Switcher::VsHdmi < PlaceOS::Driver
   def received(data, task)
     logger.debug { "Kramer sent 0x#{data.hexstring}" }
 
-    return if data[0] & 0b1000000 == 0 # Check we are the destination
+    # Check if we are the destination
+    return unless data[0].bit(6)
+    input = data[1] & 0b111_111
+    output = data[2] & 0b111_111
+    command = Command.from_value(data[0] & 0b111_111)
+    pp "command is #{command}"
 
-    data[1] = data[1] & 0b1111111 # input
-    data[2] = data[2] & 0b1111111 # output
-
-    case Command.from_value(data[0] & 0b111111)
+    case command
     when .define_machine?
-      if data[1] == 1
-        self[:video_inputs] = data[2]
-      elsif data[1] == 2
-        self[:video_outputs] = data[2]
+      if input == 1
+        self[:video_inputs] = output
+      elsif input == 2
+        self[:video_outputs] = output
       end
       @limits_known = true # Set here in case unsupported
     when .status_video?
-      if data[2] == 0 # Then data[1] has been applied to all the outputs
-        logger.debug { "Kramer switched #{data[1]} -> All" }
+      if output == 0 # Then input has been applied to all the outputs
+        logger.debug { "Kramer switched #{input} -> All" }
 
-        (1..self[:video_outputs].as_i).each { |i| self["video#{i}"] = data[1] }
+        (1..self[:video_outputs].as_i).each { |i| self["video#{i}"] = input }
       else
-        self["video#{data[2]}"] = data[1]
+        self["video#{output}"] = input
 
-        logger.debug { "Kramer switched #{data[1]} -> #{data[2]}" }
+        logger.debug { "Kramer switched #{input} -> #{output}" }
 
         # As we may not know the max number of inputs if get_machine_type didn't work
-        self[:video_inputs] = data[1] if data[1] > self[:video_inputs].as_i
-        self[:video_outputs] = data[2] if data[2] > self[:video_outputs].as_i
+        self[:video_inputs] = input if input > self[:video_inputs].as_i
+        self[:video_outputs] = output if output > self[:video_outputs].as_i
       end
     when .identify_machine?
-      logger.debug { "Kramer switcher protocol #{data[1]}.#{data[2]}" }
+      logger.debug { "Kramer switcher protocol #{input}.#{output}" }
     end
 
     task.try &.success
--- drivers/kramer/switches/vs_hdmi_spec.cr
+++ drivers/kramer/switches/vs_hdmi_spec.cr
@@ -1,2 +1,12 @@
 DriverSpecs.mock_driver "Kramer::Switcher::VsHdmi" do
+  # connected
+  # get_machine_type
+  # number of inputs
+  should_send(Bytes[62, 0x81, 0x81, 0xFF])
+  responds(Bytes[0x7E, 0x81, 0b11, 0x82])
+  status[:video_inputs].should eq(3)
+  # number of outputs
+  should_send(Bytes[62, 0x82, 0x81, 0xFF])
+  responds(Bytes[0x7E, 0x82, 0x90, 0x82])
+  status[:video_outputs].should eq(16)
 end
