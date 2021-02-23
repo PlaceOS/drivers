9820226cab7bd513fca7be0d8c0701ad04612a56
Anh (Duke) Nguyen <58082199+dukeraphaelng@users.noreply.github.com>
2021-02-23 13:24:58 +1100

    Epson projector (#62)
    * wip: epson projector conversion from ruby
    
    * --no-edit
    
    * wip
    
    * wip
    
    * docker-compose up --build not working
    
    * wip
    
    * fix(epson_proj): make compilable
    
    * fix(epson_proj): use MuteLayer enum
    
    * feat(epson_proj): add working send
    
    * fix(epson_proj): use Array to store errors
    
    * feat(epson_proj): add specs for do_poll
    
    * feat(epson_proj): add more specs
    
    * fix(epson_proj): store commands in hash
    
    * fix(epson_proj): move unmute_volume to instance var
    
    * feat(epson projector): simplify mute interface
    
    crystal tool format
    
    Co-authored-by: PlaceOS <support@place.technology>
    Co-authored-by: Philip Kheav <philip.kheav@hotmail.com>
    Co-authored-by: Stephen von Takach <steve@vontaka.ch>

---
+++ drivers/epson/projector/esc_vp21_spec.cr
@@ -0,0 +1,59 @@
+DriverSpecs.mock_driver "Epson::Projector::EscVp21" do
+  # connected
+  should_send("ESC/VP.net\x10\x03\x00\x00\x00\x00")
+  responds(":\r")
+  # do_poll
+  # power?
+  should_send("PWR?\r")
+  responds(":PWR=01\r")
+  status[:power].should eq(true)
+  # input?
+  should_send("SOURCE?\r")
+  responds(":SOURCE=30\r")
+  status[:input].should eq("HDMI")
+  # video_mute?
+  should_send("MSEL?\r")
+  responds(":MSEL=0\r")
+  status[:video_mute].should eq(false)
+  # volume?
+  should_send("VOL?\r")
+  responds(":VOL=10\r")
+  status[:volume].should eq(10)
+  # lamp
+  should_send("LAMP?\r")
+  responds(":LAMP=20\r")
+  status[:lamp_usage].should eq(20)
+
+  exec(:mute)
+  should_send("MUTE ON\r")
+  responds(":\r")
+  should_send("MUTE?\r")
+  responds(":MUTE=ON\r")
+  status[:video_mute].should eq(true)
+  status[:audio_mute].should eq(true)
+  status[:volume].should eq(0)
+
+  exec(:switch_to, "HDBaseT")
+  should_send("SOURCE 80\r")
+  responds(":\r")
+  should_send("SOURCE?\r")
+  responds(":SOURCE=80\r")
+  status[:input].should eq("HDBaseT")
+  status[:video_mute].should eq(false)
+
+  exec(:mute_audio, false)
+  should_send("VOL 10\r")
+  responds(":\r")
+  should_send("VOL?\r")
+  responds(":VOL=10\r")
+  status[:volume].should eq(10)
+  status[:audio_mute].should eq(false)
+
+  exec(:volume, 50)
+  should_send("VOL 50\r")
+  responds(":\r")
+  should_send("VOL?\r")
+  responds(":VOL=50\r")
+  status[:volume].should eq(50)
+  status[:audio_mute].should eq(false)
+end
