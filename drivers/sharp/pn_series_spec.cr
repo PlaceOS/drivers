61d173ec7fba472197b66bb880776ac678e844f6
Philip Kheav <philip.kheav@hotmail.com>
2021-02-23 02:11:14 +0000

    Sharp display (#99)
    * feat(sharp_pn): initial commit
    
    * feat(sharp_pn): port over rest of driver
    
    * feat(sharp_pn): add basic spec
    
    * feat(sharp_pn): add spec for do_poll
    
    * feat(sharp_pn): add delay/timeout for commands
    
    * fix(sharp_pn): remove debugging
    
    * chore(sharp_pn): crystal tool format

---
+++ drivers/sharp/pn_series_spec.cr
@@ -0,0 +1,75 @@
+DriverSpecs.mock_driver "Sharp::PnSeries" do
+  # connected
+  # send_credentials
+  should_send("\x0D\x0A")
+  responds("OK\x0D\x0A")
+  should_send("\x0D\x0A")
+  responds("Password:Login incorrect\x0D\x0A")
+
+  # Settings can only be accessed after on_load and connected
+  settings({
+    username: "user",
+    password: "pass",
+  })
+
+  # Retrying send_credentials
+  sleep 5
+  should_send("user\x0D\x0A")
+  responds("OK\x0D\x0A")
+  should_send("pass\x0D\x0A")
+  responds("Password:OK\x0D\x0A")
+
+  exec(:do_poll)
+  should_send("POWR????\x0D\x0A")
+  responds("POWR 001\x0D\x0A")
+  status[:warming].should eq(false)
+  status[:power].should eq(true)
+  should_send("INF1????\x0D\x0A")
+  responds("INF1 P802B\x0D\x0A")
+  status[:model_number].should eq("P802B")
+  should_send("PWOD????\x0D\x0A")
+  responds("PWOD 002\x0D\x0A")
+  status[:power_on_delay].should eq(2)
+  should_send("MUTE????\x0D\x0A")
+  responds("MUTE 000\x0D\x0A")
+  status[:audio_mute].should eq(false)
+  should_send("VOLM????\x0D\x0A")
+  responds("VOLM 010\x0D\x0A")
+  status[:volume].should eq(10)
+
+  exec(:switch_to, "hdmi")
+  should_send("INPS0010\x0D\x0A")
+  responds("WAIT\x0D\x0A")
+  responds("OK\x0D\x0A")
+  sleep 2
+  should_send("INPS????\x0D\x0A")
+  responds("INPS  10\x0D\x0A")
+  status[:input].should eq("HDMI")
+  should_send("VLMP????\x0D\x0A")
+  responds("VLMP  15\x0D\x0A")
+  status[:brightness].should eq(15)
+  should_send("CONT????\x0D\x0A")
+  responds("CONT  20\x0D\x0A")
+  status[:contrast].should eq(20)
+
+  exec(:switch_audio, "component")
+  should_send("ASCA   1\x0D\x0A")
+  responds("ASCA   1\x0D\x0A")
+  status[:audio_input].should eq("component")
+
+  exec(:volume, 100)
+  should_send("VOLM  31\x0D\x0A")
+  responds("ASCA   1\x0D\x0A")
+
+  exec(:power, false)
+  should_send("POWR   0\x0D\x0A")
+  responds("OK\x0D\x0A")
+  should_send("POWR????\x0D\x0A")
+  responds("POWR   0\x0D\x0A")
+  status[:warming].should eq(false)
+  status[:power].should eq(false)
+  should_send("MUTE????\x0D\x0A")
+  responds("MUTE   1\x0D\x0A")
+  status[:audio_mute].should eq(true)
+  status[:volume].should eq(0)
+end
