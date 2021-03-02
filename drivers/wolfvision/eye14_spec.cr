f55e6829ac3ea5b98daba387cc8c2f9468604ff1
Michał Połoński <polonski@gmail.com>
2021-03-02 13:01:59 +1100

    feat(eye14):all specs passing

--- drivers/wolfvision/eye14_spec.cr
+++ drivers/wolfvision/eye14_spec.cr
@@ -1,90 +1,107 @@
 DriverSpecs.mock_driver "Wolfvision::Eye14" do
+  ####
+  # POWER
   #
   exec(:power?)
-
-  # transmit "\x00\x30\x00\r"
-  # transmit("\x00\x30\x00")
-  # should_send("\x00\x30\x00")
-  # .should_send("\x00\x30\x00")  # power query
-  responds("\x00\x30\x01\x01") # respond with on
-  # power_status.should be_true
-  # .expect(status[:power]).to be(true)
-  # logger.info { power_status }
-  sleep 150.milliseconds
-  # status[:power].should be_true
-
+  sleep 1.second
+  should_send("\\x00\\x30\\x00") # power query
+  responds("\x00\x30\x01\x00\r") # respond with off
+  sleep 1.second
+  status[:power].should eq(false)
+  #
   exec(:power, true)
-  # .should_send("\x01\x30\x01\x01") # turn off device
-  responds("\x01\x30\x00") # respond with success
-  # .expect(status[:power]).to be(true)
-  sleep 150.milliseconds
-
-  exec(:power, false)
-  # .should_send("\x01\x30\x01\x00") # turn off device
-  # responds("\x01\x30\x00")        # respond with success
-  # .expect(status[:power]).to be(false)
-
-  sleep 150.milliseconds
-  exec(:power?)
-  # .should_send("\x00\x30\x00")  # power query
-  responds("\x00\x30\x01\x01") # respond with on
-  # .expect(status[:power]).to be(true)
-
-  sleep 150.milliseconds
-
-  exec(:zoom?)
-  # .should_send("\x00\x20\x00")
-  responds("\x00\x20\x02\x00\x09")
-  # .expect(status[:zoom]).to be(9)
-  sleep 150.milliseconds
-
-  exec(:zoom, 6)
-  # .should_send("\x01\x20\x02\x00\x06")
-  # .transmit("\x01\x20\x00")
-  # .expect(status[:zoom]).to be(6)
-
-  sleep 150.milliseconds
-
-  exec(:zoom?)
-  # .should_send("\x00\x20\x00")
-  responds("\x00\x20\x02\x00\x06")
-  # .expect(status[:zoom]).to be(6)
-
-  sleep 150.milliseconds
-
-  exec(:iris?)
-  # .should_send("\x00\x22\x00")
-  responds("\x00\x22\x02\x00\x20")
-  # .expect(status[:iris]).to be(32)
-
-  sleep 150.milliseconds
-
-  exec(:iris, 8)
-  # should_send("\x01\x22\x02\x00\x08")
-  # transmit("\x01\x22\x00")
-  # expect(status[:iris]).to be(8)
+  sleep 1.second
+  should_send("\\x01\\x30\\x01\\x01") # turn on device
+  responds("\x01\x30\x01\x01\r")      # respond with success
+  sleep 1.second
+  status[:power].should eq(true)
   #
+  exec(:power?)
+  sleep 2.seconds
+  should_send("\\x00\\x30\\x00") # power query
+  sleep 2.seconds
+  responds("\x00\x30\x01\x00\r") # respond with on
+  sleep 2.seconds
+  status[:power].should eq(false)
+  #
+  exec(:power, false)
+  sleep 2.seconds
+  should_send("\\x01\\x30\\x01\\x00") # turn off device
+  sleep 2.seconds
+  responds("\x01\x30\x01\x00\r") # respond with success
+  sleep 2.seconds
+  status[:power].should eq(false)
 
-  sleep 150.milliseconds
+  ####
+  # ZOOM
+  #
+  exec(:zoom?)
+  sleep 2.seconds
+  should_send("\\x00\\x20\\x00")
+  sleep 2.seconds
+  responds("\x00\x20\x02\x00\x09\r") # originally zoom is 9
+  sleep 2.seconds
+  status[:zoom].should eq(9)
+  #
+  exec(:zoom, 6)
+  sleep 2.seconds
+  should_send("\\x01\\x20\\x02\\x06") # set zoom to 6
+  sleep 2.seconds
+  responds("\x00\x20\x02\x00\x06\r")
+  sleep 2.seconds
+  status[:zoom].should eq(6)
+  #
+  exec(:zoom?)
+  sleep 2.seconds
+  should_send("\\x00\\x20\\x00")
+  sleep 2.seconds
+  responds("\x00\x20\x02\x00\x06\r")
+  sleep 2.seconds
+  status[:zoom].should eq(6)
 
-  # status[:iris].should eq(8)
-
+  ####
+  # IRIS
+  #
   exec(:iris?)
-  # should_send("\x00\x22\x00")
-  responds("\x00\x22\x02\x00\x08")
-  # .expect(status[:iris]).to be(8)
+  sleep 2.seconds
+  should_send("\\x00\\x22\\x00")
+  sleep 2.seconds
+  responds("\x00\x22\x02\x00\x20\r") # originally zoom is 20 hex 32 int
+  sleep 2.seconds
+  status[:iris].should eq(32)
+  #
+  exec(:iris, 8)
+  sleep 2.seconds
+  should_send("\\x01\\x22\\x02\\x08") # set zoom to 6
+  sleep 2.seconds
+  responds("\x00\x22\x02\x00\x08\r")
+  sleep 2.seconds
+  status[:iris].should eq(8)
+  #
+  exec(:iris?)
+  sleep 2.seconds
+  should_send("\\x00\\x22\\x00")
+  sleep 2.seconds
+  responds("\x00\x22\x02\x00\x08\r")
+  sleep 2.seconds
+  status[:iris].should eq(8)
 
-  sleep 150.milliseconds
-
+  ####
+  # AUTOFOCUS
+  #
   exec(:autofocus?)
-  # .should_send("\x00\x31\x00")
-  responds("\x00\x31\x01\x00")
-  # .expect(status[:autofocus]).to be(false)
-
-  sleep 150.milliseconds
-
+  sleep 2.seconds
+  should_send("\\x00\\x31\\x00")
+  sleep 2.seconds
+  responds("\x00\x31\x00\r")
+  sleep 2.seconds
+  status[:autofocus].should eq(false)
+  #
   exec(:autofocus)
-  # .should_send("\x01\x31\x01\x01")
-  # .transmit("\x01\x31\x00") # respond with success
-  # .expect(status[:autofocus]).to be(true)
+  sleep 2.seconds
+  should_send("\\x01\\x31\\x01\\x01")
+  sleep 2.seconds
+  responds("\x01\x31\x01\x01\r")
+  sleep 2.seconds
+  status[:autofocus].should eq(true)
 end
