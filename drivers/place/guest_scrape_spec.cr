dc6de2476c1f098e96318d2cdeaff9b07fdaddf5
Philip Kheav <philip.kheav@hotmail.com>
2021-03-10 13:02:38 +0000

    feat(guest scrape spec): add StaffAPI to spec

--- drivers/place/guest_scrape_spec.cr
+++ drivers/place/guest_scrape_spec.cr
@@ -1,3 +1,18 @@
-DriverSpecs.mock_driver "Place::Bookings" do
+DriverSpecs.mock_driver "Place::GuestScrape" do
+  system({
+    StaffAPI: {StaffAPI}
+  })
+
   exec(:get_bookings)
+end
+
+class StaffAPI < DriverSpecs::MockDriver
+  def zone(zone_id : String)
+    logger.info { "requesting zone #{zone_id}" }
+
+    {
+      id:   zone_id,
+      tags: ["level"]
+    }
+  end
 end
