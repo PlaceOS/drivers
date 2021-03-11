e20f84186b96eb5b05c2964063d129e140dd4070
Philip Kheav <philip.kheav@hotmail.com>
2021-03-11 12:52:26 +0000

    feat(guest scrape): don't convert to json

--- drivers/place/guest_scrape.cr
+++ drivers/place/guest_scrape.cr
@@ -24,17 +24,22 @@ class Place::GuestScrape < PlaceOS::Driver
     @poll_interval = (setting?(UInt32, :poll_interval) || 5).minutes
   end
 
-  alias System = PlaceOS::Client::API::Models::System
-
   def get_bookings
     logger.debug { "Getting bookings for zones" }
     logger.debug { @zone_ids.inspect }
 
-    systems = [] of System
+    system_ids = [] of String
     @zone_ids.each do |z_id|
-      staff_api.systems(zone_id: z_id).get.as_a.each do |s|
+      staff_api.systems(zone_id: z_id).get.as_a.each do |sys|
         # Use array union to prevent dupes incase the same system is in multiple zones
-        systems |= [System.from_json(s.to_json)]
+        system_ids |= [sys["id"].as_s]
+      end
+    end
+
+    module_ids = [] of String
+    system_ids.each do |sys_id|
+      staff_api.modules_from_system(sys_id).get.as_a.each do |mod|
+        module_ids |= [mod["id"].as_s] if mod["name"] == "Bookings"
       end
     end
   end
