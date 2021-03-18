c0473d8f142a2871e51be042eda1b7a015d07e7f
Philip Kheav <philip.kheav@hotmail.com>
2021-03-18 15:43:28 +0000

    feat(ise): decode xml

--- drivers/cisco/ise/guests.cr
+++ drivers/cisco/ise/guests.cr
@@ -6,10 +6,10 @@ class Cisco::Ise::Guests < PlaceOS::Driver
 
   default_settings({
     # We may grab this data through discovery mechanisms in the future but for now use a setting
-    auth_token: "",
-    sponsor_user_name: "",
+    auth_token: "auth_token",
+    sponsor_user_name: "sponsor_user_name",
     portal_id: "portal101",
-    sms_service_provider: ""
+    sms_service_provider: "Global Default"
   })
 
   @auth_token : String = ""
@@ -52,9 +52,12 @@ class Cisco::Ise::Guests < PlaceOS::Driver
     guest_details = GuestEvent.from_json payload
 
     # TODO: Ensure that this is getting the correct day due to timezone
+    # Use the server's local timezone for now
+    # but if needed we can pass in a timezone using to_local_in
+    # e.g. Time.unix(epoch).to_local_in(Time::Location.load("America/New_York"))
     # Also note that this uses American time formatting
-    from_date = Time.unix(guest_details.event_starting).at_beginning_of_day.to_s("%m/%d/%Y %H:%M")
-    to_date = Time.unix(guest_details.event_starting).at_end_of_day.to_s("%m/%d/%Y %H:%M")
+    from_date = Time.unix(guest_details.event_starting).to_local.at_beginning_of_day.to_s("%m/%d/%Y %H:%M")
+    to_date = Time.unix(guest_details.event_starting).to_local.at_end_of_day.to_s("%m/%d/%Y %H:%M")
 
     # Determine the name of the attendee for ISE
     guest_names = guest_details.attendee_name.split(" ")
@@ -77,32 +80,30 @@ class Cisco::Ise::Guests < PlaceOS::Driver
 
     # Now generate our XML body
     xml_string = %(
-    <?xml version="1.0" encoding="UTF-8"?>
-    <ns2:guestuser
-        xmlns:ns2="identity.ers.ise.cisco.com">
+      <?xml version="1.0" encoding="UTF-8"?>
+      <ns2:guestuser xmlns:ns2="identity.ers.ise.cisco.com">
         <guestAccessInfo>
-            <fromDate>#{from_date}</fromDate>
-            <toDate>#{to_date}</toDate>
-            <validDays>1</validDays>
+          <fromDate>#{from_date}</fromDate>
+          <toDate>#{to_date}</toDate>
+          <validDays>1</validDays>
         </guestAccessInfo>
         <guestInfo>
-            <company>#{company_name}</company>
-            <emailAddress>#{guest_details.attendee_email}</emailAddress>
-            <firstName>#{first_name}</firstName>
-            <lastName>#{last_name}</lastName>
-            <notificationLanguage>English</notificationLanguage>
-            <phoneNumber>9999998877</phoneNumber>
-            <smsServiceProvider>Global Default</smsServiceProvider>
-            <userName>#{username}</userName>
+          <company>#{company_name}</company>
+          <emailAddress>#{guest_details.attendee_email}</emailAddress>
+          <firstName>#{first_name}</firstName>
+          <lastName>#{last_name}</lastName>
+          <notificationLanguage>English</notificationLanguage>
+          <phoneNumber>9999998877</phoneNumber>
+          <smsServiceProvider>#{@sms_service_provider}</smsServiceProvider>
+          <userName>#{username}</userName>
         </guestInfo>
         <guestType>Daily</guestType>
         <personBeingVisited>#{guest_details.host}</personBeingVisited>
         <portalId>#{@portal_id}</portalId>
         <reasonForVisit>interview</reasonForVisit>
-    </ns2:guestuser>)
+      </ns2:guestuser>
+    )
 
-
-    # We need to POST to the Cisco ISE guest endpoint
     response = post("/guestuser/", body: xml_string, headers: {
       "Authorization" => "Basic #{@auth_token}"
     })
--- drivers/cisco/ise/guests_spec.cr
+++ drivers/cisco/ise/guests_spec.cr
@@ -16,18 +16,29 @@ DriverSpecs.mock_driver "Cisco::Ise::Guests" do
     ext_data: {"ext_data": "Some JSON"}
   }.to_json
 
-  # Call the function we
   exec(:guest_checkin, payload)
 
   # Now we can expext a POST to ISE creating that guest user based on the above details
   expect_http_request do |request, response|
-    if (data = request.body.try(&.gets_to_end)) && request.method == "POST" && request.path == "/guestuser"
+    if (data = request.body.try(&.gets_to_end)) && request.method == "POST" && request.path == "/guestuser/"
       # This is our interepted POST so parse the XML body
-      # request_body : IO
-      # request_body = IO.new(request.body)
-      parsed_body =  XML.parse(data)
+      parsed_body = XML.parse(data)
+      guest_user = parsed_body.first_element_child.not_nil!
 
-      # Check through the parsed body to ensure POST is correct
+      guest_access_info = guest_user.children.find { |c| c.name == "guestAccessInfo" }.not_nil!
+      from_date = guest_access_info.children.find { |c| c.name == "fromDate" }.not_nil!.content
+      to_date = guest_access_info.children.find { |c| c.name == "toDate" }.not_nil!.content
+
+      guest_info = guest_user.children.find { |c| c.name == "guestInfo" }.not_nil!
+      company = guest_info.children.find { |c| c.name == "company" }.not_nil!.content
+      email_address = guest_info.children.find { |c| c.name == "emailAddress" }.not_nil!.content
+      first_name = guest_info.children.find { |c| c.name == "firstName" }.not_nil!.content
+      last_name = guest_info.children.find { |c| c.name == "lastName" }.not_nil!.content
+      sms_service_provider = guest_info.children.find { |c| c.name == "smsServiceProvider" }.not_nil!.content
+      user_name = guest_info.children.find { |c| c.name == "userName" }.not_nil!.content
+
+      person_being_visited = guest_user.children.find { |c| c.name == "personBeingVisited" }.not_nil!.content
+      portal_id = guest_user.children.find { |c| c.name == "portalId" }.not_nil!.content
     end
   end
 end
