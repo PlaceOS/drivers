71b8296427d66a5ed5d85bb02c1cfee574517778
Philip Kheav <philip.kheav@hotmail.com>
2021-03-25 16:14:29 +0000

    fix(metasys): generate correct query params

--- drivers/johnson_controls/metasys_spec.cr
+++ drivers/johnson_controls/metasys_spec.cr
@@ -1,3 +1,20 @@
+require "placeos-driver"
+require "./metasys.cr"
+
 DriverSpecs.mock_driver "JohnsonControls::Metasys" do
+  exec(:get_token)
+
+  expect_http_request do |request, response|
+    response.status_code = 200
+    response << %({
+      "accessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IklFa3FIW...",
+      "expires": "#{JohnsonControls::Metasys::ISO8601.format(Time.utc + 1.day)}"
+    })
+  end
+
   exec(:get_alarms, 1, 2)
+
+  expect_http_request do |request, response|
+    pp request.query_params
+  end
 end
