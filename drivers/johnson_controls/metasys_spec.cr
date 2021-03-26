436b64ad0bdafeb9c995376152c47e1e40f9f1c3
Philip Kheav <philip.kheav@hotmail.com>
2021-03-26 15:50:21 +0000

    feat(metasys): add spec for auth token request

--- drivers/johnson_controls/metasys_spec.cr
+++ drivers/johnson_controls/metasys_spec.cr
@@ -2,9 +2,20 @@ require "placeos-driver"
 require "./metasys.cr"
 
 DriverSpecs.mock_driver "JohnsonControls::Metasys" do
+  username = "user"
+  password = "pass"
+  settings({
+    username: "user",
+    password: "pass"
+  })
+
   exec(:get_token)
 
   expect_http_request do |request, response|
+    body = JSON.parse(request.body.not_nil!)
+    body["username"].should eq username
+    body["password"].should eq password
+
     response.status_code = 200
     response << %({
       "accessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IklFa3FIW...",
@@ -15,6 +26,6 @@ DriverSpecs.mock_driver "JohnsonControls::Metasys" do
   exec(:get_alarms, 1, 2, 0, 255)
 
   expect_http_request do |request, response|
-    pp request
+    # pp request
   end
 end
