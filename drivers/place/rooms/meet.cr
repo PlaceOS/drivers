2a8cdbcd54b291ea6f4cadf6635af6f55d0e174c
Kim Burgess <kim@place.technology>
2021-04-08 10:47:40 +1000

    feat: driver impl for interface testing

--- drivers/place/rooms/meet.cr
+++ drivers/place/rooms/meet.cr
@@ -1,5 +1,5 @@
 require "placeos-driver"
-require "../router"
+require "placeos-driver/interface/powerable"
 
 class Place::Rooms::Meet < PlaceOS::Driver
   generic_name :System
@@ -7,19 +7,108 @@ class Place::Rooms::Meet < PlaceOS::Driver
   description <<-DESC
     Room level state and behaviours.
 
-    This driver provides a high-level API for interaction devices, systems and
-    integrations found within common workplace collaboration spaces. It's
+    This driver provides a high-level API for interaction with devices, systems
+    and integrations found within common workplace collaboration spaces. It's
     behavior will adapt to match the capabilities and configuration of other
     drivers present in the same system.
     DESC
 
   def on_load
+    load_mock_state
     on_update
   end
 
-
-  def on_update
+  enum InputType
+    Laptop
+    Wireless
+    PC
   end
 
-  include Place::Router::Core
+  enum OutputType
+    LCD
+  end
+
+  abstract class Node(Type)
+    include JSON::Serializable
+    property name : String
+    property type : Type
+    property mute : Bool? = nil
+    property n_id : UInt64 = rand(UInt64)
+  end
+
+  class Input < Node(InputType)
+    property outputs = [] of String
+    def initialize(@name, @type, @outputs); end
+  end
+
+  class Output < Node(OutputType)
+    property mod : String
+    property source : String? = nil
+    property inputs = [] of String
+    def initialize(@name, @type, @mod, @inputs); end
+  end
+
+  @inputs = {
+    "table" => Input.new("Table", InputType::Laptop, ["lcd"]),
+    "wireless" => Input.new("Vivi", InputType::Wireless, ["lcd"]),
+    "opc" => Input.new("PC", InputType::PC, ["lcd"])
+  }
+
+  @outputs = {
+    "lcd" => Output.new("LCD", OutputType::LCD, "Display_1", ["table", "wireless", "opc"])
+  }
+
+  private def update_o(name : String)
+    yield @outputs[name]
+    self["outputs/#{name}"] = @outputs[name]
+  end
+
+  private def load_mock_state
+    self[:inputs] = @inputs.map { |key, meta| self["input/#{key}"] = meta; key }
+    self[:outputs] = @outputs.map { |key, meta| self["output/#{key}"] = meta; key }
+  end
+
+  def on_update
+    self[:name] = system.name
+  end
+
+  def powerup
+    logger.debug { "Powering up" }
+    # Nothing to do...
+  end
+
+  def shutdown
+    logger.debug { "Shutting down" }
+    system.implementing(PlaceOS::Driver::Interface::Powerable).power false
+  end
+
+  def route(input : String, output : String)
+    logger.debug { "Routing #{input} -> #{output}" }
+    src = @inputs[input].n_id
+    dst = @outputs[output].n_id
+    lcd = system["Display_1"]
+    lcd.power true
+    case input
+    when "table"
+      lcd.switch_to "Hdmi"
+    when "wireless"
+      lcd.switch_to "Hdmi2"
+    when "opc"
+      lcd.switch_to "Option"
+    end
+    update_o(output) { |o| o.source = input }
+  end
+
+  def mute(input_or_output : String, state : Bool = true)
+    logger.debug { "#{state ? "Muting" : "Unmuting"} #{input_or_output}" }
+    case input_or_output
+    when "lcd"
+      system["Display_1"].mute_audio state
+      update_o("lcd") { |o| o.mute = state }
+    end
+  end
+
+  def unmute(input_or_output : String)
+    mute input_or_output, false
+  end
 end
--- drivers/place/rooms/meet_spec.cr
+++ drivers/place/rooms/meet_spec.cr
@@ -4,6 +4,50 @@ require "placeos-driver/interface/muteable"
 require "placeos-driver/interface/powerable"
 require "placeos-driver/interface/switchable"
 
+class Display < DriverSpecs::MockDriver
+  include PlaceOS::Driver::Interface::Powerable
+  include PlaceOS::Driver::Interface::Muteable
+
+  enum Inputs
+    Hdmi
+    Hdmi2
+    Option
+  end
+
+  include PlaceOS::Driver::Interface::InputSelection(Inputs)
+
+  def on_load
+    self[:power] = true
+    self[:input] = Inputs::Hdmi
+  end
+
+  # implement the abstract methods required by the interfaces
+  def power(state : Bool)
+    self[:power] = state
+  end
+
+  def switch_to(input : Inputs)
+    mute(false)
+    self[:input] = input
+  end
+
+  def mute(
+    state : Bool = true,
+    index : Int32 | String = 0,
+    layer : MuteLayer = MuteLayer::AudioVideo
+  )
+    self[:mute] = state
+  end
+end
+
 DriverSpecs.mock_driver "Place::Rooms::Meet" do
-  exec(:route, "a", "b").get.should eq("foo")
+  system({
+    Display: {Display},
+  })
+
+  status[:inputs].should eq(["table", "wireless", "opc"])
+  status[:output].should eq(["lcd"])
+
+  exec(:route, "table", "lcd").get
+  status["output/lcd"].as_h[:source].should eq("table")
 end
