5dfcb60475fc73da2305fa5811c5505b153664e6
Philip Kheav <philip.kheav@hotmail.com>
2021-04-14 13:28:52 +0100

    feat(desk_webhook): initial commit

--- drivers/place/desk_booking_webhook.cr
+++ drivers/place/desk_booking_webhook.cr
@@ -11,7 +11,7 @@ class Place::DeskBookingWebhook < PlaceOS::Driver
 
   default_settings({
     post_uri: "https://remote-server/path",
-    building: "zone-id",
+    zone_ids: ["zone-id"], # list of zone_ids to monitor
 
     custom_headers: {
       "API_KEY" => "123456",
@@ -22,34 +22,51 @@ class Place::DeskBookingWebhook < PlaceOS::Driver
 
     booking_category: "desk",
 
+    # Note: only use metadata_key and mapped_id_key if we need to map interally used resource_id to another value
+    metadata_key: "desks", # e.g. metadata_key would be "desks" for below example
+    mapped_id_key: nil, # e.g. mapped_id_key would be "other_id" for below example
+    # e.g. metadata response for /api/engine/v2/metadata/zone-123
+    # {
+    #   desks: { # metadata_key
+    #     description: "blah",
+    #     details: {
+    #       {id: "desk-1", name: "Desk 1", bookable: true, other_id: "d-1"},
+    #       {id: "desk-1", name: "Desk 2", bookable: true, other_id: "d-2"}
+    #     }
+    #   },
+    #   other_metadata_key: {}
+    # }
+
     debug: false,
   })
 
   def on_load
     monitor("staff/booking/changed") do |_subscription, payload|
       logger.debug { "received booking changed event #{payload}" }
-      fetch_and_post
+      process_update(payload)
     end
-    schedule.every(24.hours) { fetch_and_post }
     on_update
   end
 
   @time_period : Time::Span = 14.days
   @booking_category : String = "desk"
   @custom_headers = {} of String => String
-  @building = ""
+  @zone_ids = [] of Array(String)
   @post_uri = ""
   @debug : Bool = false
 
   def on_update
     @post_uri = setting(String, :post_uri)
-    @building = setting(String, :building)
+    @zone_ids = setting(Array(String), :zone_ids)
     @custom_headers = setting(Hash(String, String), :custom_headers)
     @time_period = setting(Int32, :days_from_now).days
     @booking_category = setting(String, :booking_category)
     @debug = setting(Bool, :debug)
+  end
 
-    fetch_and_post
+  private def process_update(update)
+    # Only do something if the update is for a zone specified in settings(:zone_ids)
+    return unless (@zone_ids && update[:zones]).present?
   end
 
   def fetch_and_post
