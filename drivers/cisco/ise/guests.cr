8207629a00bcbe0082640bff6c67a492c5168a49
Philip Kheav <philip.kheav@hotmail.com>
2021-04-15 16:45:46 +0100

    fix(ise): make compatible with both v1.4/v2.2

--- drivers/cisco/ise/guests.cr
+++ drivers/cisco/ise/guests.cr
@@ -1,7 +1,9 @@
 require "xml"
 
-# Docs https://developer.cisco.com/docs/identity-services-engine/3.0/#!guest-user/resource-definition
 # Tested with Cisco ISE API v2.2
+# https://developer.cisco.com/docs/identity-services-engine/3.0/#!guest-user/resource-definition
+# However, should work and conform to v1.4 requirements
+# https://www.cisco.com/c/en/us/td/docs/security/ise/1-4/api_ref_guide/api_ref_book/ise_api_ref_guest.html#79039
 
 class Cisco::Ise::Guests < PlaceOS::Driver
   # Discovery Information
@@ -13,16 +15,18 @@ class Cisco::Ise::Guests < PlaceOS::Driver
     # We may grab this data through discovery mechanisms in the future but for now use a setting
     username:          "user",
     password:          "pass",
-    sponsor_user_name: "sponsor",
-    portal_id:         "portal101",
+    portal_id:         "Required, ask cisco ISE admins",
     timezone:          "Australia/Sydney",
+    guest_type:        "Required, ask cisco ISE admins for valid subset of values", # e.g. Contractor
+    location:          "Remove if using ISE API v1.4, else ask cisco ISE admins for valid value", # e.g. New York
   })
 
   @basic_auth : String = ""
-  @sponsor_user_name : String = ""
   @portal_id : String = ""
   @sms_service_provider : String? = nil
+  @guest_type : String = "default_guest_type"
   @timezone : Time::Location = Time::Location.load("Australia/Sydney")
+  @location : String? = nil
 
   TYPE_HEADER = "application/vnd.com.cisco.ise.identity.guestuser.2.0+xml"
   TIME_FORMAT = "%m/%d/%Y %H:%M"
@@ -33,8 +37,9 @@ class Cisco::Ise::Guests < PlaceOS::Driver
 
   def on_update
     @basic_auth = "Basic #{Base64.strict_encode("#{setting?(String, :username)}:#{setting?(String, :password)}")}"
-    @sponsor_user_name = setting?(String, :sponsor_user_name) || "sponsor"
     @portal_id = setting?(String, :portal_id) || "portal101"
+    @guest_type = setting?(String, :guest_type) || "default_guest_type"
+    @location = setting?(String, :location)
     @sms_service_provider = setting?(String, :sms_service_provider)
 
     time_zone = setting?(String, :timezone).presence
@@ -45,16 +50,16 @@ class Cisco::Ise::Guests < PlaceOS::Driver
     event_start : Int64,
     attendee_email : String,
     attendee_name : String,
-    company_name : String? = nil,
-    phone_number : String? = nil,
-    sms_service_provider : String? = nil,
-    sponsor_user_name : String? = nil,
-    portal_id : String? = nil
+    company_name : String? = nil, # Mandatory but driver will extract from email if not passed
+    phone_number : String = "0123456789", # Mandatory, use a fake value as default
+    sms_service_provider : String? = nil, # Use this param to override the setting
+    guest_type : String? = nil, # Mandatory but use this param to override the setting
+    portal_id : String? = nil # Mandatory but use this param to override the setting
   )
-    return guest_crendentials("test") if setting?(Bool, :test)
-    sponsor_user_name ||= @sponsor_user_name
-    portal_id ||= @portal_id
+    return {username: UUID.random.to_s, password: UUID.random.to_s} if setting?(Bool, :test)
     sms_service_provider ||= @sms_service_provider
+    guest_type ||= @guest_type
+    portal_id ||= @portal_id
 
     time_object = Time.unix(event_start).in(@timezone)
     from_date = time_object.at_beginning_of_day.to_s(TIME_FORMAT)
@@ -75,7 +80,13 @@ class Cisco::Ise::Guests < PlaceOS::Driver
       <?xml version="1.0" encoding="UTF-8"?>
       <ns2:guestuser xmlns:ns2="identity.ers.ise.cisco.com">
         <guestAccessInfo>
-          <fromDate>#{from_date}</fromDate>
+          <fromDate>#{from_date}</fromDate>)
+
+    xml_string += %(
+          <location>#{@location}</location>
+    ) if @location
+
+    xml_string += %(
           <toDate>#{to_date}</toDate>
           <validDays>1</validDays>
         </guestAccessInfo>
@@ -85,20 +96,21 @@ class Cisco::Ise::Guests < PlaceOS::Driver
           <firstName>#{first_name}</firstName>
           <lastName>#{last_name}</lastName>
           <notificationLanguage>English</notificationLanguage>
+          <phoneNumber>#{phone_number}</phoneNumber>
     )
 
     xml_string += %(
-          <phoneNumber>#{phone_number}</phoneNumber>
           <smsServiceProvider>#{sms_service_provider}</smsServiceProvider>
-    ) if phone_number && sms_service_provider
+    ) if sms_service_provider
 
+    username = UUID.random.to_s
+    password = UUID.random.to_s
     xml_string += %(
-          <userName>#{UUID.random}</userName>
+          <userName>#{username}</userName>
+          <password>#{password}</password>
         </guestInfo>
-        <guestType>Daily</guestType>
-        <personBeingVisited>#{sponsor_user_name}</personBeingVisited>
+        <guestType>#{guest_type}</guestType>
         <portalId>#{portal_id}</portalId>
-        <reasonForVisit>interview</reasonForVisit>
       </ns2:guestuser>
     )
 
@@ -110,23 +122,6 @@ class Cisco::Ise::Guests < PlaceOS::Driver
 
     raise "failed to create guest, code #{response.status_code}\n#{response.body}" unless response.success?
 
-    guest_id = response.headers["Location"].split('/').last
-    guest_crendentials(guest_id)
-  end
-
-  def guest_crendentials(id : String)
-    return {username: UUID.random.to_s, password: UUID.random.to_s} if setting?(Bool, :test)
-    response = get("/guestuser/#{id}", headers: {
-      "Accept"        => TYPE_HEADER,
-      "Content-Type"  => TYPE_HEADER,
-      "Authorization" => @basic_auth,
-    })
-    parsed_body = XML.parse(response.body)
-    guest_user = parsed_body.first_element_child.not_nil!
-    guest_info = guest_user.children.find { |c| c.name == "guestInfo" }.not_nil!
-    {
-      username: guest_info.children.find { |c| c.name == "userName" }.not_nil!.content,
-      password: guest_info.children.find { |c| c.name == "password" }.not_nil!.content
-    }
+    {username: username, password: password}
   end
 end
--- drivers/cisco/ise/guests_spec.cr
+++ drivers/cisco/ise/guests_spec.cr
@@ -3,6 +3,17 @@ require "placeos-driver"
 require "./guests"
 
 DriverSpecs.mock_driver "Cisco::Ise::Guests" do
+  portal = "portal101"
+  phone = "0123456789"
+  type = "Contractor"
+  lo = "New York"
+
+  settings({
+    portal_id: portal,
+    guest_type: type,
+    location: lo
+  })
+
   start_time = Time.local(Time::Location.load("Australia/Sydney"))
   start_date = start_time.at_beginning_of_day.to_s(Cisco::Ise::Guests::TIME_FORMAT)
   end_date = start_time.at_end_of_day.to_s(Cisco::Ise::Guests::TIME_FORMAT)
@@ -11,16 +22,55 @@ DriverSpecs.mock_driver "Cisco::Ise::Guests" do
   first = "First Middle"
   last = "Last"
   name = "#{first} #{last}"
-  user = nil
-  sponsor = "sponsor"
 
-  resp = exec(:create_guest, start_time.to_unix, attendee_email, name, company_name)
+  exec(:create_guest, start_time.to_unix, attendee_email, name)
 
   guest_id = "e1bb8290-6ccb-11e3-8cdf-000c29c56fc6"
   location = "https://ise-pan:9060/ers/config/guestuser/#{guest_id}"
   # POST to /guestuser/
   expect_http_request do |request, response|
     # This is our interepted POST so parse the XML body
+    parsed_body = XML.parse(request.body.not_nil!)
+    guest_user = parsed_body.first_element_child.not_nil!
+
+    guest_access_info = guest_user.children.find { |c| c.name == "guestAccessInfo" }.not_nil!
+    from_date = guest_access_info.children.find { |c| c.name == "fromDate" }.not_nil!.content
+    from_date.should eq start_date
+    location = guest_access_info.children.find { |c| c.name == "location" }.not_nil!.content
+    location.should eq lo
+    to_date = guest_access_info.children.find { |c| c.name == "toDate" }.not_nil!.content
+    to_date.should eq end_date
+
+    guest_info = guest_user.children.find { |c| c.name == "guestInfo" }.not_nil!
+    company = guest_info.children.find { |c| c.name == "company" }.not_nil!.content
+    company.should eq "Test"
+    email_address = guest_info.children.find { |c| c.name == "emailAddress" }.not_nil!.content
+    email_address.should eq attendee_email
+    first_name = guest_info.children.find { |c| c.name == "firstName" }.not_nil!.content
+    first_name.should eq first
+    last_name = guest_info.children.find { |c| c.name == "lastName" }.not_nil!.content
+    last_name.should eq last
+    phone_number = guest_info.children.find { |c| c.name == "phoneNumber" }.not_nil!.content
+    phone_number.should eq phone
+    sms_service_provider = guest_info.children.find { |c| c.name == "smsServiceProvider" }
+    sms_service_provider.should eq nil
+
+    guest_type = guest_user.children.find { |c| c.name == "guestType" }.not_nil!.content
+    guest_type.should eq type
+
+    portal_id = guest_user.children.find { |c| c.name == "portalId" }.not_nil!.content
+    portal_id.should eq portal
+
+    response.status_code = 201
+    response.headers["Location"] = location
+    response.headers["Content-Type"] = "application/xml"
+  end
+
+  sms = "Global Default"
+  exec(:create_guest, start_time.to_unix, attendee_email, "First Last", company_name, phone, sms, "Daily")
+
+  # POST to /guestuser/
+  expect_http_request do |request, response|
     parsed_body = XML.parse(request.body.not_nil!)
     guest_user = parsed_body.first_element_child.not_nil!
 
@@ -36,87 +86,6 @@ DriverSpecs.mock_driver "Cisco::Ise::Guests" do
     email_address = guest_info.children.find { |c| c.name == "emailAddress" }.not_nil!.content
     email_address.should eq attendee_email
     first_name = guest_info.children.find { |c| c.name == "firstName" }.not_nil!.content
-    first_name.should eq first
-    last_name = guest_info.children.find { |c| c.name == "lastName" }.not_nil!.content
-    last_name.should eq last
-    phone_number = guest_info.children.find { |c| c.name == "phoneNumber" }
-    phone_number.should eq nil
-    sms_service_provider = guest_info.children.find { |c| c.name == "smsServiceProvider" }
-    sms_service_provider.should eq nil
-    user_name = guest_info.children.find { |c| c.name == "userName" }.not_nil!.content
-    user = user_name
-    pp "userName = #{user_name}"
-
-    person_being_visited = guest_user.children.find { |c| c.name == "personBeingVisited" }.not_nil!.content
-    person_being_visited.should eq sponsor
-    portal_id = guest_user.children.find { |c| c.name == "portalId" }.not_nil!.content
-    portal_id.should eq "portal101"
-
-    response.status_code = 201
-    response.headers["Location"] = location
-    response.headers["Content-Type"] = "application/xml"
-  end
-
-  pass = "12345"
-  # GET to /guestuser/
-  expect_http_request do |_, response|
-    response.status_code = 200
-    response.headers["Content-Type"] = "application/xml"
-    response << %(
-      <?xml version="1.0" encoding="UTF-8"?>
-      <ns3:guestuser xmlns:ns2="ers.ise.cisco.com" xmlns:ns3="identity.ers.ise.cisco.com" name="user1" id="b4bdf2b0-73e1-11e3-8cdf-000c29c56fc6">
-        <link type="application/xml" href="#{location}" rel="self"/>
-        <customFields/>
-        <guestAccessInfo>
-          <fromDate>#{start_date}</fromDate>
-          <toDate>#{end_date}</toDate>
-          <validDays>1</validDays>
-        </guestAccessInfo>
-        <guestInfo>
-        <company>Cisco</company>
-        <creationTime>#{start_time.to_s(Cisco::Ise::Guests::TIME_FORMAT)}</creationTime>
-        <emailAddress>#{attendee_email}</emailAddress>
-        <enabled>true</enabled>
-        <firstName>#{first}</firstName>
-        <lastName>#{last}</lastName>
-        <notificationLanguage>English</notificationLanguage>
-        <password>#{pass}</password>
-        <userName>#{user}</userName>
-        </guestInfo>
-        <guestType>Daily (default)</guestType>
-        <personBeingVisited>#{sponsor}</personBeingVisited>
-        <reasonForVisit>Interview</reasonForVisit>
-        <sponsorUserName>SponsoredUser1</sponsorUserName>
-        <status>Awaiting Initial Login</status>
-      </ns3:guestuser>
-    )
-  end
-
-  credentials = resp.get.not_nil!
-  credentials["username"].should eq(user)
-  credentials["password"].should eq(pass)
-
-  phone = "0123456789"
-  sms = "Global Default"
-  exec(:create_guest, start_time.to_unix, attendee_email, "First Last", nil, phone, sms)
-
-  # POST to /guestuser/
-  expect_http_request do |request, response|
-    parsed_body = XML.parse(request.body.not_nil!)
-    guest_user = parsed_body.first_element_child.not_nil!
-
-    guest_access_info = guest_user.children.find { |c| c.name == "guestAccessInfo" }.not_nil!
-    from_date = guest_access_info.children.find { |c| c.name == "fromDate" }.not_nil!.content
-    from_date.should eq start_date
-    to_date = guest_access_info.children.find { |c| c.name == "toDate" }.not_nil!.content
-    to_date.should eq end_date
-
-    guest_info = guest_user.children.find { |c| c.name == "guestInfo" }.not_nil!
-    company = guest_info.children.find { |c| c.name == "company" }.not_nil!.content
-    company.should eq "Test"
-    email_address = guest_info.children.find { |c| c.name == "emailAddress" }.not_nil!.content
-    email_address.should eq attendee_email
-    first_name = guest_info.children.find { |c| c.name == "firstName" }.not_nil!.content
     first_name.should eq "First"
     last_name = guest_info.children.find { |c| c.name == "lastName" }.not_nil!.content
     last_name.should eq "Last"
@@ -124,13 +93,12 @@ DriverSpecs.mock_driver "Cisco::Ise::Guests" do
     phone_number.should eq phone
     sms_service_provider = guest_info.children.find { |c| c.name == "smsServiceProvider" }.not_nil!.content
     sms_service_provider.should eq sms
-    user_name = guest_info.children.find { |c| c.name == "userName" }.not_nil!.content
-    pp "userName = #{user_name}"
 
-    person_being_visited = guest_user.children.find { |c| c.name == "personBeingVisited" }.not_nil!.content
-    person_being_visited.should eq "sponsor"
     portal_id = guest_user.children.find { |c| c.name == "portalId" }.not_nil!.content
-    portal_id.should eq "portal101"
+    portal_id.should eq portal
+
+    guest_type = guest_user.children.find { |c| c.name == "guestType" }.not_nil!.content
+    guest_type.should eq "Daily"
 
     response.status_code = 201
     response.headers["Location"] = "https://ise-pan:9060/ers/config/guestuser/e1bb8290-6ccb-11e3-8cdf-000c29c56fc7"
