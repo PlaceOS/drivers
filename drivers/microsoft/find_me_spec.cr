bf3e6e69b992c13122b2e65ec2c3dbf7d4022261
Caspian Baska <caspianbaska@gmail.com>
2021-04-28 12:05:38 +1000

    style(microsoft/find_me_spec): fix lints

--- drivers/microsoft/find_me_spec.cr
+++ drivers/microsoft/find_me_spec.cr
@@ -3,7 +3,7 @@ DriverSpecs.mock_driver "Microsoft::FindMe" do
   retval = exec(:levels)
 
   # sms should send a HTTP request
-  expect_http_request do |request, response|
+  expect_http_request do |_request, response|
     response.status_code = 200
     response << %([{"Building":"SYDNEY","Level":"0","Online":13},{"Building":"SYDNEY","Level":"2","Online":14}])
   end
@@ -16,7 +16,7 @@ DriverSpecs.mock_driver "Microsoft::FindMe" do
   # Send the request
   retval = exec(:user_details, "mbenz")
   details_response = %([{"Alias":"mbenz","LastUpdate":"2020-12-15T13:22:00.8675244Z","CurrentUntil":"0001-01-01T00:00:00","Confidence":0,"Coordinates":null,"GPS":null,"LocationIdentifier":null,"Status":"NoData","LocatedUsing":null,"Type":null,"Comments":null,"ExtendedUserData":null,"WiFiScale":0.0,"userTypes":null}])
-  expect_http_request do |request, response|
+  expect_http_request do |_request, response|
     response.status_code = 200
     response << details_response
   end
@@ -25,7 +25,7 @@ DriverSpecs.mock_driver "Microsoft::FindMe" do
   # Check the time format works
   retval = exec(:user_details, "mbenz")
   details_response = %([{"Alias":"mbenz","LastUpdate":"2020-12-15T13:22:00Z","CurrentUntil":"0001-01-01T00:00:00","Confidence":0,"Coordinates":null,"GPS":null,"LocationIdentifier":null,"Status":"NoData","LocatedUsing":null,"Type":null,"Comments":null,"ExtendedUserData":null,"WiFiScale":0.0,"userTypes":null}])
-  expect_http_request do |request, response|
+  expect_http_request do |_request, response|
     response.status_code = 200
     response << details_response
   end
