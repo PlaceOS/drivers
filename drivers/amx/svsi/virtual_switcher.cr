3a83839869b9f01a5c47599ea1aa0de5c0a8432d
Philip Kheav <philip.kheav@hotmail.com>
2021-05-20 10:55:39 +0100

    feat(svsi_virtual_switcher): implement Switchable

--- drivers/amx/svsi/virtual_switcher.cr
+++ drivers/amx/svsi/virtual_switcher.cr
@@ -1,32 +1,39 @@
+require "placeos-driver/interface/switchable"
+
 # This driver provides an abstraction layer for systems using SVSI based signal
 # distribution. In place of referencing specific decoders and stream id's,
 # this may be used to enable all endpoints associated with a system to be
 # grouped as a virtual matrix switcher and a familiar switcher API used.
 
 class Amx::Svsi::VirtualSwitcher < PlaceOS::Driver
+  include PlaceOS::Driver::Interface::Switchable(Int32, Int32)
+
   descriptive_name "AMX SVSI Virtual Switcher"
   generic_name :Switcher
 
   accessor encoders : Array(Encoder)
   accessor decoders : Array(Decoder)
 
-  alias Map = Hash(Int32, Int32 | Array(Int32))
+  alias Map = Hash(Int32, Array(Int32)) | Hash(String, Hash(Int32, Array(Int32)))
 
-  def switch(signal_map : Map)
-    connect(signal_map) do |mod, value|
-      mod.switch(value)
-    end
+  # TODO: no idea how to implement this as
+  def switch_to(input : Int32)
   end
 
-  def switch_video(signal_map : Map)
-    connect(signal_map) do |mod, value|
-      mod.switch_video(value)
-    end
-  end
-
-  def switch_audio(signal_map : Map)
-    connect(signal_map) do |mod, value|
-      mod.switch_audio(value)
+  def switch(map : Map)
+    case map
+    when FullSwitch
+      connect(map) do |mod, value|
+        mod.switch(value)
+      end
+    when SelectiveSwitch
+      map.each do |layer, inouts|
+        next unless layer = SwitchLayer.parse?(layer)
+        connect(inouts) do |mod, value|
+          mod.switch_audio(value) if layer.audio?
+          mod.switch_video(value) if layer.video?
+        end
+      end
     end
   end
 
