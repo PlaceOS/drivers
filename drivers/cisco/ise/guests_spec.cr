74b961a9ca37906ebfedab8ceac232986d10d3ac
Philip Kheav <philip.kheav@hotmail.com>
2021-05-27 09:45:06 +0100

    Cisco ise (#122)
    * feat(cisco ise): add initial basic guest driver
    
    * feat(ise spec): add beginning of ise spec and cleanup ise driver
    
    * fix(ise): ensure request body is not IO | nil before parsing in ise spec
    
    * fix(ise): initialise @portal_id
    
    * chore(ise): use instance variables instead
    
    * chore(ise spec): fix typo
    
    * fix(guests): use uri_base
    
    * feat(ise): add more settings
    
    * feat(ise): decode xml
    
    * feat(ise): add specs for xml fields
    
    * chore(ise): update default settings
    
    * feat(ise): add params to override default values
    
    * fix(ise): deal with single names
    
    * feat(ise): add mandatory headers
    
    * feat(ise): add basic error handling
    
    * feat(ise): use UUID to generate usernames
    
    * feat(ise): add support for optional sms details
    
    * chore(ise): crystal tool format
    
    * fix(ise): simplify request body check
    
    * fix(ise): make more generic
    
    * chore(ise): use indentation to make xml clearer
    
    * feat(ise): support timezone setting
    
    * fix(ise): convert to local time properly
    
    * fix(ise): correct spec structure
    
    * feat(ise): add support to get guest credentials
    
    * feat(ise): add support for mock data
    
    * fix(ise): generate basic auth token from creds
    
    * feat(ise): add link v2.2 api docs
    
    * fix(ise): make compatible with both v1.4/v2.2
    
    * fix(ise): update XML to match schema
    
    * chore(ise): crystal tool format
    
    * feat(ise): add support to return custom data
    
    * feat(ise): use ISE generated credentials
    
    * fix(ise): make mock data similar to real api
    
    * feat(ise): generate more consistent usernames
    
    Co-authored-by: Cameron Reeves <cam@Camerons-MacBook-Pro.local>

---
+++ drivers/cisco/ise/guests_spec.cr
@@ -0,0 +1,61 @@
+require "xml"
+require "placeos-driver"
+require "./guests"
+
+DriverSpecs.mock_driver "Cisco::Ise::Guests" do
+  portal = "portal101"
+  phone = "0123456789"
+  type = "Contractor"
+  lo = "New York"
+
+  settings({
+    portal_id:  portal,
+    guest_type: type,
+    location:   lo,
+  })
+
+  start_time = Time.local(Time::Location.load("Australia/Sydney"))
+  start_date = start_time.at_beginning_of_day.to_s(Cisco::Ise::Guests::TIME_FORMAT)
+  end_date = start_time.at_end_of_day.to_s(Cisco::Ise::Guests::TIME_FORMAT)
+  attendee_email = "attendee@test.com"
+  company_name = "PlaceOS"
+
+  sms = "Global Default"
+  exec(:create_guest, start_time.to_unix, attendee_email, "First Last", company_name, phone, sms, "Daily")
+
+  # POST to /guestuser/
+  expect_http_request do |request, response|
+    parsed_body = XML.parse(request.body.not_nil!)
+    guest_user = parsed_body.first_element_child.not_nil!
+
+    guest_access_info = guest_user.children.find { |c| c.name == "guestAccessInfo" }.not_nil!
+    from_date = guest_access_info.children.find { |c| c.name == "fromDate" }.not_nil!.content
+    from_date.should eq start_date
+    to_date = guest_access_info.children.find { |c| c.name == "toDate" }.not_nil!.content
+    to_date.should eq end_date
+
+    guest_info = guest_user.children.find { |c| c.name == "guestInfo" }.not_nil!
+    company = guest_info.children.find { |c| c.name == "company" }.not_nil!.content
+    company.should eq company_name
+    email_address = guest_info.children.find { |c| c.name == "emailAddress" }.not_nil!.content
+    email_address.should eq attendee_email
+    first_name = guest_info.children.find { |c| c.name == "firstName" }.not_nil!.content
+    first_name.should eq "First"
+    last_name = guest_info.children.find { |c| c.name == "lastName" }.not_nil!.content
+    last_name.should eq "Last"
+    phone_number = guest_info.children.find { |c| c.name == "phoneNumber" }.not_nil!.content
+    phone_number.should eq phone
+    sms_service_provider = guest_info.children.find { |c| c.name == "smsServiceProvider" }.not_nil!.content
+    sms_service_provider.should eq sms
+
+    portal_id = guest_user.children.find { |c| c.name == "portalId" }.not_nil!.content
+    portal_id.should eq portal
+
+    guest_type = guest_user.children.find { |c| c.name == "guestType" }.not_nil!.content
+    guest_type.should eq "Daily"
+
+    response.status_code = 201
+    response.headers["Location"] = "https://ise-pan:9060/ers/config/guestuser/e1bb8290-6ccb-11e3-8cdf-000c29c56fc7"
+    response.headers["Content-Type"] = "application/xml"
+  end
+end
