fed057790a4d8352aa074069461ee6269572c487
Stephen von Takach <steve@vontaka.ch>
2021-07-07 21:41:51 +1000

    feat(build.cr): move to drivers folder

---
+++ drivers/build.cr
@@ -0,0 +1,10 @@
+{% if env("COMPILE_DRIVER") %}
+  {% if env("COMPILE_DRIVER").ends_with?("_spec.cr") %}
+    require "placeos-driver/driver-specs/runner"
+  {% else %}
+    require "placeos-driver"
+  {% end %}
+
+  # Dynamically require the desired driver
+  {{ ("require \"../" + env("COMPILE_DRIVER") + "\"").id }}
+{% end %}
