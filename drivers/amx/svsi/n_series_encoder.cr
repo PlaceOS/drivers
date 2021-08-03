0f3a23fe1eb4d782b9e64aca3e0efdb186bd29cf
Philip Kheav <philip.kheav@hotmail.com>
2021-08-03 08:58:41 +0100

    Svsi (#166)
    * feat(svsi): port decoder driver
    
    * feat(svsi): port encoder driver
    
    * fix(svsi): minor changes
    
    * feat(svsi): port n_series_switcher
    
    * feat(svsi_virtual_switcher): attempt to port
    
    * fix(svsi): use correct variable name
    
    * fix(svsi_virtual_switcher): use blocks
    
    * fix(svsi_n_series): downcase property
    
    * feat(svsi_virtual_switcher): implement Switchable
    
    * fix(svsi_virtual_switcher): make types clearer
    
    * feat(svsi_virtual_switcher): implement switch_to
    
    * fix(svsi): implement more interfaces
    
    * fix(svsi_virtual_switcher): decoder switch_to
    
    * fix(svsi_virtual_switcher): use switch_to
    
    * fix(svsi_virtual_switcher): workaround for int key
    
    * fix(svsi_virtual_switcher): use correct indexes
    
    * fix(svsi_virtual_switcher): use Int32 for input
    
    * chore(svsi): clearly define delimiter
    
    Co-authored-by: Caspian Baska <caspian@place.tech>
    
    * style(svsi): run formatter
    
    * fix(svsi): use delimiter directly
    
    * refactor(svsi:decoder): use mapped_enum for commands
    
    * refactor(svsi): ensure exhaustive case
    
    * fix(svsi): construct do_send args
    
    * fix(svsi): fix do_send
    
    Co-authored-by: Caspian Baska <caspian@place.tech>
    Co-authored-by: Caspian Baska <email@caspian.computer>

---
+++ drivers/amx/svsi/n_series_encoder.cr
@@ -0,0 +1,120 @@
+require "inactive-support/mapped_enum"
+require "placeos-driver/interface/muteable"
+require "placeos-driver/interface/switchable"
+
+# Documentation: https://aca.im/driver_docs/AMX/SVSIN1000N2000Series.APICommandList.pdf
+
+class Amx::Svsi::NSeriesEncoder < PlaceOS::Driver
+  include Interface::Muteable
+
+  enum Input
+    Hdmionly
+    Vgaonly
+    Hdmivga
+    Vgahdmi
+  end
+
+  include Interface::InputSelection(Input)
+
+  tcp_port 50002
+  descriptive_name "AMX SVSI N-Series Encoder"
+  generic_name :Encoder
+
+  private DELIMITER = "\r"
+
+  mapped_enum Command do
+    GetStatus   = "getStatus"
+    VideoSource = "vidsrc"
+    Live        = "live"
+    Local       = "local"
+    Disable     = "txdisable"
+    Mute        = "mute"
+    Unmute      = "unmute"
+  end
+
+  def on_load
+    transport.tokenizer = Tokenizer.new(DELIMITER)
+  end
+
+  def connected
+    schedule.every(50.seconds, true) { do_poll }
+  end
+
+  def disconnected
+    schedule.clear
+  end
+
+  def do_poll
+    do_send(Command::GetStatus, priority: 0)
+  end
+
+  def switch_to(input : Input, **options)
+    do_send(Command::VideoSource, input, **options)
+  end
+
+  Modes = (1..8).map &.to_s
+
+  def media_source(mode : String)
+    if mode == "live"
+      do_send(Command::Live)
+    elsif Modes.includes?(mode)
+      do_send(Command::Local, mode)
+    else
+      raise("invalid mode #{mode}")
+    end
+  end
+
+  def mute(
+    state : Bool = true,
+    index : Int32 | String = 0,
+    layer : MuteLayer = MuteLayer::AudioVideo
+  )
+    if state
+      do_send(Command::Disable) if layer.audio_video? || layer.video?
+      do_send(Command::Mute) if layer.audio_video? || layer.audio?
+    else
+      do_send(Command::Disable) if layer.audio_video? || layer.video?
+      do_send(Command::Unmute) if layer.audio_video? || layer.audio?
+    end
+  end
+
+  enum Response
+    Name
+    Stream
+    Playmode
+    Mute
+  end
+
+  def received(data, task)
+    data = String.new(data)
+    logger.debug { "Received: #{data}" }
+
+    prop, value = data.split(':')
+
+    case Response.parse? prop
+    in Response::Name
+      self[:device_name] = value
+    in Response::Stream
+      self[:stream_id] = value.to_i
+    in Response::Playmode
+      self[:mute] = value == "off"
+    in Response::Mute
+      self[:audio_mute] = value == "1"
+    in Nil
+      raise "Invalid response: #{prop}"
+    end
+
+    task.try(&.success)
+  end
+
+  def do_send(command : Command, *args, **options)
+    arguments = [command.mapped_value]
+
+    unless (splat = args.to_a).is_a? Array(NoReturn)
+      arguments += splat
+    end
+
+    request = "#{arguments.join(':')}#{DELIMITER}"
+    send(request, **options)
+  end
+end
