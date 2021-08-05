00c421cd627530af8ad06c0c20ef1acf4a3874cf
Kim Burgess <kim@place.technology>
2021-08-05 21:15:19 +1000

    docs: expand description

--- drivers/place/router.cr
+++ drivers/place/router.cr
@@ -10,7 +10,35 @@ require "./router/signal_graph"
 class Place::Router < PlaceOS::Driver
   generic_name :Switcher
   descriptive_name "Signal router"
-  description "A universal matrix switcher."
+  description <<-DESC
+    A virtual matrix switcher for arbitrary signal networks.
+
+    Following configuration, this driver can be used to perform simple input → \
+    output routing, regardless of intermediate hardware. Drivers it interacts \
+    with _must_ implement the `Switchable`, `InputSelection` or `Mutable` \
+    interfaces.
+
+    Configuration is specified as a map of devices and their attached inputs. \
+    This must exist under a top-level `connections` key.
+
+    Inputs can be either named:
+
+        Display_1:
+          hdmi: VidConf_1
+
+    Or, index based:
+
+        Switcher_1:
+          - Camera_1
+          - Camera_2
+
+    If an input is not a device with an associated module, prefix with an \
+    asterisk (`*`) to create a named alias.
+
+        Display_1:
+          hdmi: *Laptop
+
+    DESC
 
   # Core routing methods and functionality. This exists as module to enable
   # inclusion in other drivers, such as room logic, that provide auxillary
