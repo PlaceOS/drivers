50aed962af4b14f06432a848d80548353f8f067c
Kim Burgess <kim@place.technology>
2021-08-09 14:15:45 +1000

    feat: base meet room logic (#210)
    Adds base driver with core signal routing, power and volume control. Functionality to be expanded in subsequent PR's.

---
+++ drivers/place/meet.cr
@@ -0,0 +1,50 @@
+require "placeos-driver"
+require "placeos-driver/interface/powerable"
+require "./router"
+
+class Place::Meet < PlaceOS::Driver
+  generic_name :System
+  descriptive_name "Meeting room logic"
+  description <<-DESC
+    Room level state and behaviours.
+
+    This driver provides a high-level API for interaction with devices, systems \
+    and integrations found within common workplace collaboration spaces.
+    DESC
+
+  include Router::Core
+
+  protected def on_siggraph_loaded(inputs, outputs)
+    outputs.each &.watch { |node| on_output_change node }
+  end
+
+  protected def on_output_change(output)
+    case output.source
+    when nil
+      output.proxy.power false
+    when Mute
+      # nothing to do here
+    else
+      output.proxy.power true
+    end
+  end
+
+  def powerup
+    logger.debug { "Powering up" }
+    self[:active] = true
+  end
+
+  def shutdown
+    logger.debug { "Shutting down" }
+    system.implementing(PlaceOS::Driver::Interface::Powerable).power false
+    self[:active] = false
+  end
+
+  # Set the volume of a signal node within the system.
+  def volume(input_or_output : String, level : Int32)
+    logger.info { "setting volume on #{input_or_output} to #{level}" }
+    node = signal_node input_or_output
+    node.proxy.volume level
+    node["volume"] = level
+  end
+end
