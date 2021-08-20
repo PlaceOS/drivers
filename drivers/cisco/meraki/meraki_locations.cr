5058ca63ab3506026b36ee43b3cd3f7f31eb9481
William Le <will@place.tech>
2021-08-20 12:26:47 +0800

    chore(meraki/locations): more detailed loggging when device OS is filtered out

--- drivers/cisco/meraki/meraki_locations.cr
+++ drivers/cisco/meraki/meraki_locations.cr
@@ -656,7 +656,7 @@ class Cisco::Meraki::Locations < PlaceOS::Driver
         logger.debug { "parsing new observation for #{client_mac}" } if @debug_webhook
         # If a filter is set, then ignore this device unless it matches
         if @regex_filter_device_os && /#{@regex_filter_device_os}/.match(observation.os || "").nil?
-          logger.debug { "FILTERED OUT #{client_mac}: OS did not match" } if @debug_webhook
+          logger.debug { "FILTERED OUT #{client_mac}: OS \"#{observation.os}\" did not match \"#{@regex_filter_device_os}\"" } if @debug_webhook
           next
         end
         location = parse(existing, ignore_older, drift_older, observation)
