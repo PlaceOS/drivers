e40d2ba0022cad29d171449121c9df34b01024e7
Stephen von Takach <steve@vontaka.ch>
2021-09-16 09:27:12 +1000

    fix(floorsense desk ws): default ws user and pass to rest API values

--- drivers/floorsense/desks_websocket.cr
+++ drivers/floorsense/desks_websocket.cr
@@ -42,8 +42,8 @@ class Floorsense::Desks < PlaceOS::Driver
   def on_update
     @username = setting(String, :username)
     @password = setting(String, :password)
-    @ws_username = setting?(String, :ws_username)
-    @ws_password = setting?(String, :ws_password)
+    @ws_username = setting?(String, :ws_username) || @username
+    @ws_password = setting?(String, :ws_password) || @password
 
     schedule.clear
     schedule.every(1.hour) { sync_locker_list }
