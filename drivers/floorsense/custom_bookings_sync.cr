886106afea1c04698b1d79df98693f93b6bab2b4
Stephen von Takach <steve@vontaka.ch>
2021-10-20 15:29:15 +1100

    fix(floorsense booking sync): don't recreate bookings if tapped out

--- drivers/floorsense/custom_bookings_sync.cr
+++ drivers/floorsense/custom_bookings_sync.cr
@@ -462,8 +462,13 @@ class Floorsense::CustomBookingsSync < PlaceOS::Driver
       booking_id = booking.id.to_s
       next if place_booking_checked.includes?(booking_id)
       place_booking_checked << booking_id
-
       next if time_now >= booking.booking_end
+
+      # if we get to here then the floor booking was released
+      if (ext_data = booking.extension_data) && (floor_id = ext_data["floorsense_booking_id"]?.try(&.as_s))
+        release_place_bookings << {booking, 1.minute.ago.to_unix}
+        next
+      end
 
       found = false
       other.each do |floor_booking|
