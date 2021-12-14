13393ac69053a4dfcb966b74215423baee2866bd
Stephen von Takach <steve@vontaka.ch>
2021-12-14 16:41:43 +1100

    finished

--- drivers/lutron/vive_leap_models.cr
+++ drivers/lutron/vive_leap_models.cr
@@ -1,6 +1,24 @@
 require "json"
 
 module Lutron
+  macro upper_enum(name)
+    {% if name.type.resolve.nilable? %} @{{name.var}} : String? {% else %} @{{name.var}} : String {% end %}
+    {% enum_type = name.type.resolve.union_types.reject(&.nilable?).first %}
+
+    def {{name.var}} : {{name.type}}
+      if value = @{{name.var}}
+        {{enum_type}}.parse(value)
+      else
+        nil
+      end
+    end
+
+    def {{name.var}}=(value : {{name.type}}) : {{name.type}}
+      @{{name.var}} = value.try &.to_s
+      value
+    end
+  end
+
   enum CommuniqueType
     ReadRequest
     ReadResponse
@@ -21,7 +39,7 @@ module Lutron
     include JSON::Serializable
 
     @[JSON::Field(key: "CommuniqueType")]
-    property type : CommuniqueType
+    Lutron.upper_enum type : CommuniqueType
 
     @[JSON::Field(key: "Header")]
     property header : Hash(String, String)
@@ -35,21 +53,13 @@ module Lutron
       header["Url"]?
     end
 
-    def initialize(@type, @header, body = nil)
-      case body
-      when String, Nil
-        @body = body
-      else
-        @body = body.to_json
-      end
-    end
-
     def initialize(
       url : String,
-      @type = CommuniqueType::ReadRequest,
+      req_type : CommuniqueType = CommuniqueType::ReadRequest,
       body = nil,
-      @header = {} of String => String,
+      @header = {} of String => String
     )
+      @type = req_type.to_s
       @body = case body
               when String, Nil
                 body
@@ -81,5 +91,97 @@ module Lutron
     end
   end
 
+  struct ExceptionDetail
+    include JSON::Serializable
 
+    @[JSON::Field(key: "Message")]
+    getter message : String
+
+    @[JSON::Field(key: "ErrorCode")]
+    getter error_code : Int32?
+  end
+
+  struct MultipleAreaStatus
+    include JSON::Serializable
+
+    @[JSON::Field(key: "AreaStatuses")]
+    getter states : Array(AreaStatus)
+  end
+
+  enum OccupancyStatus
+    Occupied
+    Unoccupied
+    Unknown
+  end
+
+  struct AreaStatus
+    include JSON::Serializable
+
+    # /area/3/status
+    getter href : String
+
+    @[JSON::Field(key: "Level")]
+    getter level : Float64?
+
+    @[JSON::Field(key: "OccupancyStatus")]
+    Lutron.upper_enum occupancy : OccupancyStatus?
+
+    def status_key
+      _blank, component, area_id, status = href.split("/", 4)
+      "#{component}#{area_id}"
+    end
+  end
+
+  struct MultipleZoneStatus
+    include JSON::Serializable
+
+    @[JSON::Field(key: "ZoneStatuses")]
+    getter states : Array(ZoneStatus)
+  end
+
+  struct OneZoneStatus
+    include JSON::Serializable
+
+    @[JSON::Field(key: "ZoneStatus")]
+    getter status : ZoneStatus
+  end
+
+  enum SwitchedLevel
+    On
+    Off
+  end
+
+  enum ContactClosureState
+    Open
+    Closed
+  end
+
+  enum Availability
+    Available
+    Unavailable
+    Unknown
+  end
+
+  struct ZoneStatus
+    include JSON::Serializable
+
+    getter href : String
+
+    @[JSON::Field(key: "Level")]
+    getter level : Float64?
+
+    @[JSON::Field(key: "SwitchedLevel")]
+    Lutron.upper_enum switched_level : SwitchedLevel?
+
+    @[JSON::Field(key: "Availability")]
+    Lutron.upper_enum availability : Availability?
+
+    @[JSON::Field(key: "CCOLevel")]
+    Lutron.upper_enum contact_closure : ContactClosureState?
+
+    def status_key
+      _blank, component, zone_id, status = href.split("/", 4)
+      "#{component}#{zone_id}"
+    end
+  end
 end
--- drivers/lutron/vive_leap_spec.cr
+++ drivers/lutron/vive_leap_spec.cr
@@ -1,19 +1,86 @@
 require "placeos-driver/spec"
 
-DriverSpecs.mock_driver "Lutron::ViveBacnet" do
-  system({
-    BACnet: {BACnetMock},
+DriverSpecs.mock_driver "Lutron::ViveLeap" do
+  puts "---- Confirming protocol version"
+  should_send %({
+    "CommuniqueType":"UpdateRequest",
+    "Header":{
+      "Url":"/clientsetting"
+    },
+    "Body":{
+      "ClientSetting":{
+        "ClientMajorVersion":1
+      }
+    }
+  }).gsub(/\s/, "")
+  transmit %({
+    "CommuniqueType":"UpdateResponse",
+    "Header": {
+      "MessageBodyType":"OneClientSettingDefinition",
+      "StatusCode":"200 OK",
+      "Url":"/clientsetting"
+    },
+    "Body":{
+      "ClientSetting":{
+        "href":"/clientsetting",
+        "ClientMajorVersion":1,
+        "ClientMinorVersion":3
+      }
+    }
   })
 
-  level = exec(:level, 120.0).get
-  level.should eq(100.0)
-  status[:lighting_level].should eq(100.0)
-end
+  puts "---- Logging on"
+  should_send %({
+    "CommuniqueType":"UpdateRequest",
+    "Header": {"Url":"/login"},
+    "Body": {
+      "Login":{
+        "ContextType":"Application",
+        "LoginId":"user",
+        "Password":"pass"
+      }
+    }
+  }).gsub(/\s/, "")
+  transmit %({
+    "CommuniqueType":"UpdateResponse",
+    "Header": {
+      "StatusCode":"200 OK",
+      "Url":"/login"
+    }
+  })
 
-# :nodoc:
-class BACnetMock < DriverSpecs::MockDriver
-  def write_real(device_id : UInt32, instance_id : UInt32, value : Float32, object_type : String = "AnalogValue")
-    raise "over 100!" if value > 100.0
-    self["#{device_id}.#{object_type}[#{instance_id}]"] = value
-  end
+  puts "---- Testing API"
+  status["zonez45"]?.should eq(nil)
+  level = exec(:zone_lighting, "z45", true)
+
+  should_send %({
+    "CommuniqueType":"CreateRequest",
+    "Header":{ "Url":"/zone/z45/commandprocessor" },
+    "Body":{
+      "Command":{
+        "CommandType":"GoToSwitchedLevel",
+        "SwitchedLevelParameters":{
+          "SwitchedLevel":"On"
+        }
+      }
+    }
+  }).gsub(/\s/, "")
+  transmit %({
+    "CommuniqueType": "CreateResponse",
+    "Header":{
+      "MessageBodyType":"OneZoneStatus",
+      "StatusCode":"200 Created",
+      "Url":"/zone/z45/commandprocessor"
+    },
+    "Body":{
+      "ZoneStatus":{
+        "href":"/zone/z45/status",
+        "SwitchedLevel":"On",
+        "Zone":{ "href":"/zone/z45" }
+      }
+    }
+  })
+
+  level.get
+  status["zonez45"].should eq(true)
 end
