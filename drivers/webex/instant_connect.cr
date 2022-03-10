787202ab229b92d94058d79cc60b95315bc15b70
tassja <xtassja@gmail.com>
2022-03-11 09:46:23 +1100

    refactor(webex): return correct meeting details

--- drivers/webex/instant_connect.cr
+++ drivers/webex/instant_connect.cr
@@ -1,5 +1,3 @@
-require "./instant_connect_models.cr"
-
 class Webex::InstantConnect < PlaceOS::Driver
   # Discovery Information
   generic_name :InstantConnect
@@ -12,7 +10,7 @@ class Webex::InstantConnect < PlaceOS::Driver
 
   @audience_setting = "a4d886b0-979f-4e2c-a958-3e8c14605e51"
 
-  def create_meeting(room_id : String, meeting_parameters : String)
+  def create_meeting(room_id : String)
     expiry = 24.hours.from_now.to_unix
     payload = {
       "aud": @audience_setting,
@@ -23,20 +21,25 @@ class Webex::InstantConnect < PlaceOS::Driver
     }.to_json
 
     return_hash = get_hash(payload)
-    host_token = get_host_token(return_hash)
-
-    response = post("/api/v1/joseencrypt", body: meeting_parameters, headers: {
-      "Content-Type"  => "application/json",
-      "Authorization" => "Bearer #{host_token}",
-    })
-    raise "create meeting request failed with #{response.status_code}" unless response.status_code == 200 && !response.body.nil?
-    MeetingResponse.from_json(response.body)
+    get_meeting_details(return_hash)
   end
 
-  def get_host_token(hash : Hash(String, String))
+  def get_meeting_details(hash : Hash(String, String))
     response = get("api/v1/space/?int=jose&data=#{hash["host"]}")
     raise "host token request failed with #{response.status_code}" unless response.status_code == 200 && !response.body.nil?
-    NamedTuple(token: String).from_json(response.body)[:token]
+
+    meeting_config = Hash(String, String | Bool).from_json(response.body)
+    response_keys = meeting_config.keys
+    response_keys.delete("token")
+    response_keys.delete("spaceId")
+    meeting_config = meeting_config.reject(response_keys) 
+
+    response = get("api/v1/space/?int=jose&data=#{hash["guest"]}")
+    raise "guest token request failed with #{response.status_code}" unless response.status_code == 200 && !response.body.nil?
+
+    guest_token = String.from_json(response.body, root: "token")
+    meeting_config["guest_token"] = guest_token
+    meeting_config
   end
 
   def get_hash(payload : String)
--- drivers/webex/instant_connect_spec.cr
+++ drivers/webex/instant_connect_spec.cr
@@ -1,10 +1,7 @@
-require "./instant_connect_models.cr"
-
 DriverSpecs.mock_driver "Webex::InstantConnect" do
   # Send the request
   retval = exec(:create_meeting,
-    room_id: "1",
-    meeting_parameters: RAW_MEETING_PARAMETERS
+    room_id: "1"
   )
 
   # HTTP request to get host/guest hash
@@ -25,39 +22,32 @@ DriverSpecs.mock_driver "Webex::InstantConnect" do
     end
   end
 
-  # HTTP request to get token using host JWT
+  # HTTP request to get token/spaceId using host JWT
   expect_http_request do |request, response|
     headers = request.headers
     if request.resource.includes?("api/v1/space/?int=jose&data=")
       response.status_code = 200
-      response << RAW_TOKEN_RESPONSE
+      response << RAW_HOST_RESPONSE
     else
       response.status_code = 401
     end
   end
 
-  # final HTTP request to create meeting
+  # HTTP request to get token using guest JWT
   expect_http_request do |request, response|
     headers = request.headers
-    io = request.body
-    if io
-      data = io.gets_to_end
-      request = JSON.parse(data)
-      if request.to_s.includes?(%("title" => "Example Daily Meeting")) && headers["Authorization"].includes?(%(Bearer))
-        response.status_code = 200
-        response << RAW_MEETING_RESPONSE
-      else
-        response.status_code = 401
-      end
+    if request.resource.includes?("api/v1/space/?int=jose&data=")
+      response.status_code = 200
+      response << RAW_GUEST_RESPONSE
     else
-      raise "expected request to include meeting details #{request.inspect}"
+      response.status_code = 401
     end
   end
 
-  retval.get.to_json.should eq(RETVAL_RESPONSE)
+  retval.get.should eq(JSON.parse(RETVAL))
 end
 
-RAW_TOKEN_RESPONSE = %({
+RAW_HOST_RESPONSE = %({
   "userIdentifier": "Host",
   "isLoggedIn": false,
   "isHost": true,
@@ -69,132 +59,17 @@ RAW_TOKEN_RESPONSE = %({
   "integrationType": "jose"
 })
 
-RAW_MEETING_PARAMETERS = %({
-  "title": "Example Daily Meeting",
-  "agenda": "Example Agenda",
-  "password": "BgJep@43",
-  "start": "2019-11-01 20:00:00",
-  "end": "2019-11-01 21:00:00",
-  "timezone": "Asia/Shanghai",
-  "recurrence": "FREQ=DAILY;INTERVAL=1;COUNT=10",
-  "enabledAutoRecordMeeting": false,
-  "allowAnyUserToBeCoHost": false,
-  "enabledJoinBeforeHost": false,
-  "enableConnectAudioBeforeHost": false,
-  "joinBeforeHostMinutes": 0,
-  "excludePassword": false,
-  "publicMeeting": false,
-  "reminderTime": 10,
-  "enableAutomaticLock": false,
-  "automaticLockMinutes": 0,
-  "allowFirstUserToBeCoHost": false,
-  "allowAuthenticatedDevices": false,
-  "invitees": [
-      {
-          "email": "john.andersen@example.com",
-          "displayName": "John Andersen",
-          "coHost": false
-      },
-      {
-          "email": "brenda.song@example.com",
-          "displayName": "Brenda Song",
-          "coHost": false
-      }
-  ],
-  "sendEmail": true,
-  "hostEmail": "john.andersen@example.com",
-  "siteUrl": "site4-example.webex.com",
-  "registration": {
-      "requireFirstName": "true",
-      "requireLastName": "true",
-      "requireEmail": "true",
-      "requireCompanyName": "true",
-      "requireCountryRegion": "true",
-      "requireWorkPhone": "true"
-  },
-  "integrationTags": [
-      "dbaeceebea5c4a63ac9d5ef1edfe36b9",
-      "85e1d6319aa94c0583a6891280e3437d",
-      "27226d1311b947f3a68d6bdf8e4e19a1"
-  ]
+RAW_GUEST_RESPONSE = %({
+  "userIdentifier": "Guest",
+  "isLoggedIn": false,
+  "isHost": false,
+  "organizationId": "16917798-5582-49a7-92d0-4410f6964000",
+  "orgName": "PlaceOS",
+  "token": "NmFmZGQwODYtZmIzNi05OTlmLWE3N2QtMzUyNzk2MDk4NDU5MeZlNmM2YmQtNjY2_PF84_e2d06a2e-ac4e-464f-968d-a5f8a5ac6303",
+  "spaceId": "Y2lzY29zcGFyazovL3VzL1JPT00vODhhZGM1ODAtOThmMi0xMWVjLThiYjQtZjM2MmNkNDBlZDQ1",
+  "visitId": "1",
+  "integrationType": "jose"
 })
-
-RAW_MEETING_RESPONSE = %({
-  "id": "870f51ff287b41be84648412901e0402",
-  "meetingNumber": "123456789",
-  "title": "Example Daily Meeting",
-  "agenda": "Example Agenda",
-  "password": "BgJep@43",
-  "phoneAndVideoSystemPassword": "12345678",
-  "meetingType": "meetingSeries",
-  "state": "active",
-  "timezone": "Asia/Shanghai",
-  "start": "2019-11-01T20:00:00+08:00",
-  "end": "2019-11-01T21:00:00+08:00",
-  "recurrence": "FREQ=DAILY;COUNT=10;INTERVAL=1",
-  "hostUserId": "Y2lzY29zcGFyazovL3VzL1BFT1BMRS9jN2ZkNzNmMi05ZjFlLTQ3ZjctYWEwNS05ZWI5OGJiNjljYzY",
-  "hostDisplayName": "John Andersen",
-  "hostEmail": "john.andersen@example.com",
-  "hostKey": "123456",
-  "siteUrl": "site4-example.webex.com",
-  "webLink": "https://site4-example.webex.com/site4/j.php?MTID=md41817da6a55b0925530cb88b3577b1e",
-  "sipAddress": "123456789@site4-example.webex.com",
-  "dialInIpAddress": "192.168.100.100",
-  "enabledAutoRecordMeeting": false,
-  "allowAnyUserToBeCoHost": false,
-  "enabledJoinBeforeHost": false,
-  "enableConnectAudioBeforeHost": false,
-  "joinBeforeHostMinutes": 0,
-  "excludePassword": false,
-  "publicMeeting": false,
-  "reminderTime": 10,
-  "sessionTypeId": 3,
-  "scheduledType": "meeting",
-  "enableAutomaticLock": false,
-  "automaticLockMinutes": 0,
-  "allowFirstUserToBeCoHost": false,
-  "allowAuthenticatedDevices": false,
-  "telephony": {
-      "accessCode": "1234567890",
-      "callInNumbers": [
-          {
-              "label": "US Toll",
-              "callInNumber": "123456789",
-              "tollType": "toll"
-          }
-      ],
-      "links": [
-          {
-              "rel": "globalCallinNumbers",
-              "href": "/api/v1/meetings/870f51ff287b41be84648412901e0402/globalCallinNumbers",
-              "method": "GET"
-          }
-      ]
-  },
-  "registration": {
-      "autoAcceptRequest": "false",
-      "requireFirstName": "true",
-      "requireLastName": "true",
-      "requireEmail": "true",
-      "requireJobTitle": "false",
-      "requireCompanyName": "true",
-      "requireAddress1": "false",
-      "requireAddress2": "false",
-      "requireCity": "false",
-      "requireState": "false",
-      "requireZipCode": "false",
-      "requireCountryRegion": "true",
-      "requireWorkPhone": "true",
-      "requireFax": "false"
-  },
-  "integrationTags": [
-      "dbaeceebea5c4a63ac9d5ef1edfe36b9",
-      "85e1d6319aa94c0583a6891280e3437d",
-      "27226d1311b947f3a68d6bdf8e4e19a1"
-  ]
-})
-
-RETVAL_RESPONSE = "{\"id\":\"870f51ff287b41be84648412901e0402\",\"meetingNumber\":\"123456789\",\"title\":\"Example Daily Meeting\",\"agenda\":\"Example Agenda\",\"password\":\"BgJep@43\",\"phoneAndVideoSystemPassword\":\"12345678\",\"meetingType\":\"meeting_series\",\"state\":\"active\",\"timezone\":\"Asia/Shanghai\",\"start\":\"2019-11-01T20:00:00+08:00\",\"end\":\"2019-11-01T21:00:00+08:00\",\"recurrence\":\"FREQ=DAILY;COUNT=10;INTERVAL=1\",\"hostUserId\":\"Y2lzY29zcGFyazovL3VzL1BFT1BMRS9jN2ZkNzNmMi05ZjFlLTQ3ZjctYWEwNS05ZWI5OGJiNjljYzY\",\"hostDisplayName\":\"John Andersen\",\"hostEmail\":\"john.andersen@example.com\",\"hostKey\":\"123456\",\"siteUrl\":\"site4-example.webex.com\",\"sipAddress\":\"123456789@site4-example.webex.com\",\"dialInIpAddress\":\"192.168.100.100\",\"enabledAutoRecordMeeting\":false,\"allowAnyUserToBeCoHost\":false,\"enabledJoinBeforeHost\":false,\"enableConnectAudioBeforeHost\":false,\"joinBeforeHostMinutes\":0,\"excludePassword\":false,\"publicMeeting\":false,\"reminderTime\":10,\"sessionTypeId\":3,\"scheduledType\":\"meeting\",\"enableAutomaticLock\":false,\"automaticLockMinutes\":0,\"allowFirstUserToBeCoHost\":false,\"allowAuthenticatedDevices\":false,\"telephony\":{\"accessCode\":\"1234567890\",\"callInNumbers\":[{\"label\":\"US Toll\",\"callInNumber\":\"123456789\",\"tollType\":\"toll\"}],\"links\":[{\"rel\":\"globalCallinNumbers\",\"href\":\"/api/v1/meetings/870f51ff287b41be84648412901e0402/globalCallinNumbers\",\"method\":\"GET\"}]},\"registration\":{\"autoAcceptRequest\":null,\"requireFirstName\":\"true\",\"requireLastName\":\"true\",\"requireEmail\":\"true\",\"requireJobTitle\":null,\"requireCompanyName\":\"true\",\"requireAddress1\":null,\"requireAddress2\":null,\"requireCity\":null,\"requireState\":null,\"requireZipCode\":null,\"requireCountryRegion\":\"true\",\"requireWorkPhone\":\"true\",\"requireFax\":null},\"integrationTags\":[\"dbaeceebea5c4a63ac9d5ef1edfe36b9\",\"85e1d6319aa94c0583a6891280e3437d\",\"27226d1311b947f3a68d6bdf8e4e19a1\"]}"
 
 RAW_HASH_RESPONSE = %({
   "host": [
@@ -203,4 +78,10 @@ RAW_HASH_RESPONSE = %({
   "guest": [
       "eyJwMnMiOiJaVVJsejNsb1"
   ]
+})
+
+RETVAL = %({
+  "token":"NmFmZGQwODYtZmIzNi00OTlmLWE3N2QtNzUyNzk2MDk4NDU5MjZlNmM2YmQtNjY2_PF84_e2d06a2e-ac4e-464f-968d-a5f8a5ac6303",
+  "spaceId":"Y2lzY29zcGFyazovL3VzL1JPT00vODhhZGM1ODAtOThmMi0xMWVjLThiYjQtZjM2MmNkNDBlZDQ1",
+  "guest_token":"NmFmZGQwODYtZmIzNi05OTlmLWE3N2QtMzUyNzk2MDk4NDU5MeZlNmM2YmQtNjY2_PF84_e2d06a2e-ac4e-464f-968d-a5f8a5ac6303"
 })
