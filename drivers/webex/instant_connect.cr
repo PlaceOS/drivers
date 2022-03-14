a084373ceed9712cab0af90461bab21eeb17657a
Stephen von Takach <steve@vontaka.ch>
2022-03-14 12:28:59 +1100

    simplify data extraction

--- drivers/webex/instant_connect.cr
+++ drivers/webex/instant_connect.cr
@@ -23,7 +23,7 @@ class Webex::InstantConnect < PlaceOS::Driver
 
   def create_meeting(room_id : String)
     expiry = 24.hours.from_now.to_unix
-    payload = {
+    request = {
       "aud": @jwt_audience,
       "jwt": {
         "sub": room_id,
@@ -31,41 +31,38 @@ class Webex::InstantConnect < PlaceOS::Driver
       },
     }.to_json
 
-    return_hash = get_hash(payload)
-    get_meeting_details(return_hash)
+    get_meeting_details get_hash(request)
   end
 
-  def get_meeting_details(hash : Hash(String, String))
-    response = get("api/v1/space/?int=jose&data=#{hash["host"]}")
+  protected def get_meeting_details(meeting_keys)
+    response = get("api/v1/space/?int=jose&data=#{meeting_keys[:host]}")
     raise "host token request failed with #{response.status_code}" unless response.status_code == 200 && !response.body.nil?
-
     meeting_config = Hash(String, String | Bool).from_json(response.body)
-    response_keys = meeting_config.keys
-    response_keys.delete("token")
-    response_keys.delete("spaceId")
-    meeting_config = meeting_config.reject(response_keys)
 
-    response = get("api/v1/space/?int=jose&data=#{hash["guest"]}")
+    response = get("api/v1/space/?int=jose&data=#{meeting_keys[:guest]}")
     raise "guest token request failed with #{response.status_code}" unless response.status_code == 200 && !response.body.nil?
-
     guest_token = String.from_json(response.body, root: "token")
-    meeting_config["guest_token"] = guest_token
-    meeting_config
+
+    {
+      # space_id seems to be an internal id for the meeting room
+      space_id: meeting_config["spaceId"],
+      host_token: meeting_config["token"],
+      guest_token: guest_token,
+    }
   end
 
-  def get_hash(payload : String)
-    response = post("/api/v1/joseencrypt", body: payload, headers: {
+  protected def get_hash(request : String)
+    response = post("/api/v1/joseencrypt", body: request, headers: {
       "Content-Type"  => "application/json",
       "Authorization" => "Bearer #{@bot_access_token}",
     })
 
     raise "request failed with #{response.status_code}" unless response.status_code == 200 && !response.body.nil?
 
-    response_body = NamedTuple(host: Tuple(String), guest: Tuple(String)).from_json(response.body)
-    return_hash = Hash(String, String).new(initial_capacity: 2)
-    response_body.each do |key, value|
-      return_hash[key.to_s] = value.first.to_s
-    end
-    return_hash
+    response = NamedTuple(host: Array(String), guest: Array(String)).from_json(response.body)
+    {
+      host: response[:host].first,
+      guest: response[:guest].first,
+    }
   end
 end
