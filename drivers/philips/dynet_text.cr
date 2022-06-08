6f021013d027280dc5638317a9fcaa72c6b6a6a9
Stephen von Takach <steve@place.technology>
2022-06-08 18:28:52 +1000

    implement some basic queries

--- drivers/philips/dynet_text.cr
+++ drivers/philips/dynet_text.cr
@@ -12,9 +12,12 @@ class Philips::DyNetText < PlaceOS::Driver
 
   @ready : Bool = false
 
+  protected getter! telnet : Telnet
+
   def on_load
     new_telnet_client
-    transport.pre_processor { |bytes| @telnet.try &.buffer(bytes) }
+    transport.pre_processor { |bytes| telnet.buffer(bytes) }
+    transport.tokenizer = Tokenizer.new("\r\n")
   end
 
   def connected
@@ -23,7 +26,7 @@ class Philips::DyNetText < PlaceOS::Driver
 
     schedule.every(60.seconds) do
       logger.debug { "-- polling gateway" }
-      # TODO:: send a request here
+      get_date
     end
   end
 
@@ -45,14 +48,85 @@ class Philips::DyNetText < PlaceOS::Driver
 
     if @ready
       # Extract response
-      process_response data.split(", "), task
+      components = data.split(", ").map { |component|
+        parts = component.downcase.split
+        value = parts.pop
+        key = parts.join(' ')
+        {key, value}
+      }.to_h
+      process_response components, task
     elsif data =~ /Connection Established/i
       @ready = true
       self[:ready] = true
     end
   end
 
-  protected def process_response(parts, task)
-    task.try &.success
+  protected def process_response(parts : Hash(String, String), task)
+    task_name = task.try(&.name)
+    success = task_name.nil?
+
+    check_key = parts.first_key
+    case check_key
+    when "preset"
+    when "channel level channel"
+    when .starts_with?("date")
+      success = true if task_name == "date"
+    when .starts_with?("time")
+      success = true if task_name == "time"
+    when .starts_with?("reply")
+      case check_key
+      when .ends_with?("current preset")
+        preset = parts.first_value.to_i
+        area = parts["area"].to_i
+        area_key = "area#{area}"
+        self[area_key] = preset
+        task.not_nil!.success(preset) if task_name == area_key
+      when .ends_with?("level ch")
+        area = parts["area"].to_i
+        area_key = "area#{area}_level"
+        level = parts["currlev"].to_i(strict: false)
+        self[area_key] = level
+        task.not_nil!.success(level) if task_name == area_key
+      end
+    end
+
+    # ignore unless sucess
+    task.try(&.success) if success
+  end
+
+  protected def do_send(command, **options)
+    send telnet.prepare(command), **options
+  end
+
+  def get_date
+    do_send "RequestDate", name: :date
+  end
+
+  def get_time
+    do_send "RequestTime", name: :time
+  end
+
+  def trigger(area : Int32, scene : UInt16, fade : Int32 = 1000)
+
+  end
+
+  def get_current_preset(area : UInt16)
+    do_send "RequestCurrentPreset #{area}", name: "area#{area}"
+  end
+
+  def lighting(area : Int32, state : Bool, fade : Int32 = 1000)
+
+  end
+
+  def light_level(area : Int32, level : Float64, fade : Int32 = 1000, channel : Int32 = 0xFF)
+
+  end
+
+  def get_light_level(area : UInt16, channel : UInt16 = 1_u16)
+    do_send "RequestChannelLevel #{channel} #{area}", name: "area#{area}_level"
+  end
+
+  def stop_fading(area : Int32, channel : Int32 = 0xFF)
+
   end
 end
--- drivers/philips/dynet_text_spec.cr
+++ drivers/philips/dynet_text_spec.cr
@@ -6,12 +6,36 @@ DriverSpecs.mock_driver "Philips::DyNetText" do
   transmit "fffd01fffb03fffd03fffb05fffd05".hexbytes
   transmit "Telnet Connection Established ...\r\n\r\n"
 
+  sleep 200.milliseconds
+  status[:ready].should eq true
+
   # Process some data
   transmit "Preset 2, Area 103, Fade 0, Join 0xff\r\n"
 
+  # WTF philips, why are there multiple hex representations
+  transmit "Channel Level Channel 43, Level 100%, Area 137, Fade 0, Join ffhex\r\n"
+
+  # This is a good ping request
+  transmit "Date Wed 8 Jun 2022\r\n"
+
+  transmit "Time 13:35:51 Standard Time\r\n"
+
   # Execute some requests
-  #exec :trigger, 1, 4, 320
-  #should_send ""
-  #status["area1"].should eq(4)
-  #responds ""
+  resp = exec :get_current_preset, 58
+  should_send "RequestCurrentPreset 58\r\x00"
+  responds "RequestCurrentPreset 58\r\n"
+  responds "OK\r\n"
+  responds "Reply with Current Preset 2, Area 58, Join ffhex\r\n"
+
+  resp.get.should eq 2
+  status["area58"].should eq(2)
+
+  resp = exec :get_light_level, 58
+  should_send "RequestChannelLevel 1 58\r\x00"
+  responds "RequestChannelLevel 1 58\r\n"
+  responds "OK\r\n"
+  responds "Reply with Current Level Ch 1, Area 58, TargLev 100%, CurrLev 100%, Join ffhex\r\n"
+
+  resp.get.should eq 100
+  status["area58_level"].should eq(100)
 end
