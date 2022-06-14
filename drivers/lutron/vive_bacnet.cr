c3dd69c4629aa4c811b940a6e230cc3be8edb241
Stephen von Takach <steve@place.technology>
2022-06-14 18:10:36 +1000

    feat(bacnet): add datapoints logic for analytics (#336)
    * add datapoints logic for analytics
    * add power usage to lighting driver

--- drivers/lutron/vive_bacnet.cr
+++ drivers/lutron/vive_bacnet.cr
@@ -31,6 +31,9 @@ class Lutron::ViveBacnet < PlaceOS::Driver
     # Light level
     system.subscribe(:BACnet, 1, "#{@device_id}.AnalogValue[2]") { |_sub, value| self[:lighting_level] = value.to_f }
 
+    # Total Power (in watts)
+    system.subscribe(:BACnet, 1, "#{@device_id}.AnalogValue[18]") { |_sub, value| self[:power_usage] = value.to_f }
+
     # lighting on / off
     system.subscribe(:BACnet, 1, "#{@device_id}.BinaryValue[3]") { |_sub, value| self[:lighting] = value == "1" }
 
