f1f04ffeaf2782dcca63239d438c745a7a77a3f7
Mia Bennett <mia@chillfox.com>
2022-07-06 10:07:49 +0930

    feat(crestron): empty files for fusion driver

---
+++ drivers/crestron/fusion.cr
@@ -0,0 +1,9 @@
+require "placeos-driver"
+require "json"
+require "uri"
+
+
+# Documentation: https://sdkcon78221.crestron.com/sdk/Fusion_APIs/Content/Topics/Default.htm
+
+class Crestron::Fusion < PlaceOS::Driver
+end
---
+++ drivers/crestron/fusion_spec.cr
@@ -0,0 +1,4 @@
+require "placeos-driver/spec"
+
+DriverSpecs.mock_driver "Crestron::Fusion" do
+end
