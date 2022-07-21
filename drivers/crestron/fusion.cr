590454d4e109fd9840adc972c110e6d6d54f65b2
Mia Bennett <mia@chillfox.com>
2022-07-21 15:30:52 +0930

    refactor(crestron): fusion specs

--- drivers/crestron/fusion.cr
+++ drivers/crestron/fusion.cr
@@ -3,8 +3,6 @@ require "xml"
 require "json"
 require "uri"
 
-require "./fusion_models"
-
 # TODO: add handling of security level 2
 # TODO: parse returend results into models
 #
@@ -20,16 +18,16 @@ class Crestron::Fusion < PlaceOS::Driver
     # Security level: 0 (No Security), 1 (Clear Text), 2 (Encrypted)
     security_level: 1,
 
-    user_id:        "FUSION_USER_ID",
+    user_id: "FUSION_USER_ID",
 
     # Should be the same as set in the Fusion configuration client
-    api_pass_code:  "FUSION_API_PASS_CODE",
+    api_pass_code: "FUSION_API_PASS_CODE",
 
     # API Service URL, should be like http://FUSION_SERVER/RoomViewSE/APIService/
-    service_url:    "http://FUSION_SERVER/RoomViewSE/APIService/",
+    service_url: "http://FUSION_SERVER/RoomViewSE/APIService/",
 
     # xml or json
-    content_type:   "json",
+    content_type: "json",
   })
 
   @security_level : Int32 = 1
@@ -58,15 +56,15 @@ class Crestron::Fusion < PlaceOS::Driver
     params = URI::Params.new
     params["search"] = name if name
     params["room"] = room_id if room_id
-    params["page"] = page if page
+    params["page"] = page.to_s if page
 
     response = perform_request("GET", "/actions", params)
-    @content_type == "xml" ? XML.parse(response_body) : JSON.parse(response_body)
+    @content_type == "xml" ? XML.parse(response.body) : JSON.parse(response.body)
   end
 
   def get_action(action_id : String)
     response = perform_request("GET", "/actions/#{action_id}")
-    @content_type == "xml" ? XML.parse(response_body) : JSON.parse(response_body)
+    @content_type == "xml" ? XML.parse(response.body) : JSON.parse(response.body)
   end
 
   def send_action(action_id : String?, room_id : String? = nil, node_id : String? = nil)
@@ -75,10 +73,10 @@ class Crestron::Fusion < PlaceOS::Driver
     params["node"] = node_id if node_id
 
     path = if (id = action_id) && !id.empty?
-      "/actions/#{id}"
-    else
-      "/actions"
-    end
+             "/actions/#{id}"
+           else
+             "/actions"
+           end
 
     response = perform_request("POST", path, params)
     JSON.parse(response.body)
@@ -89,17 +87,17 @@ class Crestron::Fusion < PlaceOS::Driver
   ##########
 
   # Severity should be in the range 1-4
-  def get_alerts(node_ids : Array(String)? = nil, room_ids : Array(String)? = nil, start_time : Time? = nil, end_time : Time? = nil, severity : Int32? = nil, active_alerts : Bool = true)
+  def get_alerts(node_ids : Array(String)? = nil, room_ids : Array(String)? = nil, start_time : String? = nil, end_time : String? = nil, severity : Int32? = nil, active_alerts : Bool = true)
     params = URI::Params.new
     params["nodes"] = node_ids.join(',') if node_ids
     params["rooms"] = room_ids.join(',') if room_ids
     params["start"] = start_time if start_time
     params["end"] = end_time if end_time
-    params["severity"] = severity if severity
-    params["activeAlerts"] = active_alerts if active_alerts
+    params["severity"] = severity.to_s if severity
+    params["activeAlerts"] = active_alerts.to_s if active_alerts
 
     response = perform_request("GET", "/rooms", params)
-    @content_type == "xml" ? XML.parse(response_body) : JSON.parse(response_body)
+    @content_type == "xml" ? XML.parse(response.body) : JSON.parse(response.body)
   end
 
   #########
@@ -110,18 +108,17 @@ class Crestron::Fusion < PlaceOS::Driver
     params = URI::Params.new
     params["search"] = name if name
     params["node"] = node_id if node_id
-    params["page"] = page if page
+    params["page"] = page.to_s if page
 
     response = perform_request("GET", "/rooms", params)
-    @content_type == "xml" ? XML.parse(response_body) : JSON.parse(response_body)
+    @content_type == "xml" ? XML.parse(response.body) : JSON.parse(response.body)
   end
 
   def get_room(room_id : String)
     response = perform_request("GET", "/rooms/#{room_id}")
-    @content_type == "xml" ? XML.parse(response_body) : JSON.parse(response_body)
+    @content_type == "xml" ? XML.parse(response.body) : JSON.parse(response.body)
   end
 
-  
   ###########
   # Helpers #
   ###########
@@ -132,7 +129,7 @@ class Crestron::Fusion < PlaceOS::Driver
     end
 
     uri = URI.parse("@service_url/#{path}")
-    uri.params = params
+    uri.query_params = params
 
     headers = HTTP::Headers.new
     headers["Content-Type"] = @content_type
@@ -144,5 +141,5 @@ class Crestron::Fusion < PlaceOS::Driver
     else
       raise "Fusion API request failed. Status code: #{response.status_code}"
     end
-  end  
+  end
 end
--- drivers/crestron/fusion_spec.cr
+++ drivers/crestron/fusion_spec.cr
@@ -1,45 +1,31 @@
 require "placeos-driver/spec"
 
 DriverSpecs.mock_driver "Crestron::Fusion" do
-  before_all do
-    settings({
-      security_level: 0,
-      user_id:        "spec-user-id",
-      api_pass_code:  "spec-api-pass-code",
-      service_url:    "http://spec.example.com/RoomViewSE/APIService/",
-      content_type:   "xml",
-    })
+  settings({
+    security_level: 0,
+    user_id:        "spec-user-id",
+    api_pass_code:  "spec-api-pass-code",
+    service_url:    "http://spec.example.com/RoomViewSE/APIService/",
+    content_type:   "xml",
+  })
+
+  resp = exec(:get_rooms, "Meeting Room A")
+
+  expect_http_request do |request, response|
+    response.status_code = 200
+    response << rooms.to_json
   end
 
-  before_each do
-    pp "========================================"
+  resp.get
+
+  resp = exec(:get_room, "room-id")
+
+  expect_http_request do |request, response|
+    response.status_code = 200
+    response << room.to_json
   end
 
-  it "returns rooms" do
-    resp = exec(:get_rooms, "Meeting Room A")
-
-    expect_http_request do |request, response|
-      pp request
-
-      response.status_code = 200
-      response << rooms.to_json
-    end
-
-    # resp.get
-  end
-
-  it "returns a room" do
-    resp = exec(:get_room, "room-id")
-
-    expect_http_request do |request, response|
-      pp request
-
-      response.status_code = 200
-      response << room.to_json
-    end
-
-    # resp.get
-  end
+  resp.get
 end
 
 private def rooms
