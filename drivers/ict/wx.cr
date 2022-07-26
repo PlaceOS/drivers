be1691d574b60c03f4b6dfa14185b7a1499e59ea
Cameron Reeves <cam@place.technology>
2022-07-26 17:01:14 +1000

    feat(ict): initial access control driver

--- drivers/ict/wx.cr
+++ drivers/ict/wx.cr
@@ -37,6 +37,8 @@ class Ict::Wx < PlaceOS::Driver
     32.times{ |b| @session_id ||= ""; @session_id += (16  * rand).to_i.to_s(16).upcase }
     @password_hash = Digest::SHA1.hexdigest(@password)
     init_session_url = "#{@domain}/PRT_CTRL_DIN_ISAPI.dll?Command&Type=Session&SubType=InitSession&SessionID=#{@session_id}"
+    logger.info "SESSION URL IS:"
+    logger.info init_session_url
     get(init_session_url).body
   end
 
