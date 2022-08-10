dc16c2210bfa7739d919a1f3454a11a673f16c5c
Cameron Reeves <cam@place.technology>
2022-08-10 16:10:38 +1000

    feat(wx): add more methods

--- drivers/ict/wx.cr
+++ drivers/ict/wx.cr
@@ -1,5 +1,8 @@
 require "placeos-driver"
+require "http/client"
 require "digest"
+require "openssl"
+require "random/secure"
 
 class Ict::Wx < PlaceOS::Driver
   descriptive_name "ICT Protege WX"
@@ -11,21 +14,26 @@ class Ict::Wx < PlaceOS::Driver
 
   default_settings({
     # PlaceOS API creds, so we can query the zone metadata
-    username:     "place"
+    username:     "place",
+    password:     "QeS4dijJE9NRJ9"
   })
 
-  @domain : String = "http://159.196.131.157:88/"
+  @domain : String = "159.196.131.157"
+  @port : String = "88"
   @session_id : String = ""
   @username : String = ""
   @password : String = ""
-  @session_key : String = ""
+  @password_hash : String = ""
+  @session_key : Int64 = 0
   @api_key : String = ""
+  @client : HTTP::Client = HTTP::Client.new(host:  "159.196.131.157", port: "88")
 
   def on_load
     on_update
   end
 
   def on_update
+    @client = HTTP::Client.new(host: @domain, port: @port)
     @username = setting(String, :username)
     @password = setting(String, :password)
     @session_key = get_session_key
@@ -33,46 +41,103 @@ class Ict::Wx < PlaceOS::Driver
   end
 
   def get_session_key
-    # Generate a session ID
-    32.times{ |b| @session_id ||= ""; @session_id += (16  * rand).to_i.to_s(16).upcase }
+    @session_id = ""
+    32.times{ |b| @session_id += (16  * rand).to_i.to_s(16).upcase }
     @password_hash = Digest::SHA1.hexdigest(@password)
-    init_session_url = "#{@domain}/PRT_CTRL_DIN_ISAPI.dll?Command&Type=Session&SubType=InitSession&SessionID=#{@session_id}"
-    get(init_session_url).body
+    init_session_url = "/PRT_CTRL_DIN_ISAPI.dll?Command&Type=Session&SubType=InitSession&SessionID=#{@session_id}"
+    response = @client.get(init_session_url)
+    response.body.to_i64
   end
 
   def get_api_key
-    b = xor(@username, @session_key.to_i(10) + 1)
+    b = xor(@username, @session_key + 1)
     b = Digest::SHA1.hexdigest(b).upcase
-    if !@password_hash.nil?
-        a = xor(@password_hash, @session_key)
-        g = Digest::SHA1.hexdigest(a).upcase
-        password_response = get("#{@domain}/PRT_CTRL_DIN_ISAPI.dll?Command&Type=Session&SubType=CheckPassword&Name=#{b}&Password=#{g}&SessionID=#{@session_id}", 
-        params: {
-            "Command"   => "",
-            "Type"      => "Session",
-            "SubType"   => "CheckPassword",
-            "Name"      => b,
-            "Password"  => g,
-            "SessionID" => @session_id,
-        }).body
-        password_response = Digest::SHA1.hexdigest(xor(@password_hash, password_response)).upcase[0..15]
-    end
-    (password_response || "")
+    a = xor(@password_hash, @session_key)
+    g = Digest::SHA1.hexdigest(a).upcase
+    logger.debug{"URL IS:"}
+    logger.debug{"#{@domain}:88/PRT_CTRL_DIN_ISAPI.dll?Command&Type=Session&SubType=CheckPassword&Name=#{b}&Password=#{g}&SessionID=#{@session_id}"}
+    password_response = @client.get("/PRT_CTRL_DIN_ISAPI.dll?Command&Type=Session&SubType=CheckPassword&Name=#{b}&Password=#{g}&SessionID=#{@session_id}").body
+    password_response = password_response.to_i64
+    Digest::SHA1.hexdigest(xor(@password_hash, password_response)).upcase[0..15]
   end
 
-  protected def xor(a,c)
+  def xor(a : String, c : Int32 | Int64)
     # Pad out the second string
-    c = "%032d" % c.to_s.to_i(10).to_s(2)
-    b = c.size
-    d = ""
-    (a || "").chars.each do |l|
-      f = l.ord
-      b = 0 == b ? c.size - 8 : b - 8
-      g = c[b..b+7].to_i(2).to_s(10)
-      f = (f.to_i ^ g.to_i).to_s(16)
-      1 == f.size && (f = "0" + f)
-      d += f
-    end
+	  binary_string = c.to_s.to_i64(10).to_s(2).rjust(32,'0')
+    b = binary_string.size
+    d : String = ""
+    a.chars.each do |l|
+        f = l.ord
+        b = 0 == b ? binary_string.size - 8 : b - 8
+        g = binary_string[b..b+7].to_i(2).to_s(10)
+        f = (f.to_i ^ g.to_i).to_s(16)
+        1 == f.size && (f = "0" + f)
+        d += f
+      end
     d.upcase
+  end
+
+  def decrypt_aes(cipherhex : String, api_key : (String | Nil) = nil)
+    api_key ||= @api_key
+    iv = String.new(cipherhex.hexbytes[0..15])
+    ciphertext = String.new(cipherhex.hexbytes[16..-1])
+    cipher = OpenSSL::Cipher.new("aes-128-cbc")
+    cipher.decrypt
+    cipher.key = api_key
+    cipher.iv = iv
+    io = IO::Memory.new
+    io.write(cipher.update(ciphertext))
+    io.write(cipher.final)
+    io.rewind
+    io.gets_to_end
+  end
+
+  def encrypt_aes(plaintext : String, api_key : (String | Nil) = nil, session_id : (String | Nil) = nil)
+    api_key ||= @api_key
+    session_id ||= @session_id
+    iv = Random::Secure.random_bytes(8).hexstring.upcase
+    cipher = OpenSSL::Cipher.new("aes-128-cbc")
+    cipher.encrypt
+    cipher.key = api_key
+    cipher.iv = iv
+    io = IO::Memory.new
+    io.write(cipher.update(plaintext))
+    io.write(cipher.final)
+    io.rewind
+    cipher_final = io.to_slice
+    session_id + (iv.to_slice.hexstring + cipher_final.hexstring).upcase
+  end
+
+
+  def encrypted_request(path : String, api_key : (String | Nil) = nil, session_id : (String | Nil) = nil)
+    begin
+      api_key ||= @api_key
+      session_id ||= @session_id
+      enc_path =  encrypt_aes(path, @api_key, session_id)
+      url : String = "/PRT_CTRL_DIN_ISAPI.dll?#{enc_path}"
+      res = @client.get(url)
+      decrypt_aes(res.body, @api_key)
+    rescue 
+      auth
+      puts "RETYING!"
+      api_key ||= @api_key
+      session_id ||= @session_id
+      enc_path =  encrypt_aes(path, @api_key, session_id)
+      url = "/PRT_CTRL_DIN_ISAPI.dll?#{enc_path}"
+      res = @client.get(url)
+      decrypt_aes(res.body, @api_key)
+    end
+  end
+
+  def get_events(type : String = "Latest")
+    encrypted_request("Request&Type=Events&SubType=#{type}&Sequence=")
+  end
+
+  def get_doors
+    encrypted_request("Request&Type=List&SubType=GXT_DOORS_TBL&Sequence=").split("&").map{ |r| r.split("=")[1] }
+  end
+
+  def open_door(door_id : String)
+    encrypted_request("Command&Type=Control&SubType=GXT_DOORS_TBL&RecId=#{door_id}&Command=1&Sequence=")
   end
 end
--- drivers/ict/wx_spec.cr
+++ drivers/ict/wx_spec.cr
@@ -1,5 +1,22 @@
 require "placeos-driver/spec"
 
 DriverSpecs.mock_driver "Ict::Wx" do
-  exec(:get_session_key).get.should eq(123)
+  resp = exec(:get_session_key)
+  resp = exec(:get_api_key)
+  # # expect_http_request do |_request, response|
+  # #   response.status_code = 200
+  # #   response.output.puts("TEST")
+  # # end
+  # backend = ::Log::IOBackend.new(STDOUT)
+  # ::Log.setup { |c| c.bind("*", :info, backend) }
+  # # Log.info {resp}
+  # # Log.info{resp.inspect}
+  # # expect_http_request do |_request, response|
+  # #   response.status_code = 200
+  # #   response << %([{"Building":"SYDNEY","Level":"0","Online":13},{"Building":"SYDNEY","Level":"2","Online":14}])
+  # # end
+  # # res = JSON.parse(resp.get)
+  # Log.info { resp.get }
+  # resp.get.should eq("TEST")
+  # resp.get.should be < 1000000
 end
