325794d27b67c7722b3dd6060cb91b1f6cb90c96
William Le <will@place.tech>
2022-08-24 21:22:15 +0800

    feat(lutron/quantum): add zones query

--- drivers/lutron/quantum.cr
+++ drivers/lutron/quantum.cr
@@ -27,34 +27,38 @@ class Lutron::Quantum < PlaceOS::Driver
     @client = Client.new(host_name: host_name.not_nil!, api_key: api_key, device_key: device_key)
   end
 
-  def level?(id : Int32)
-    status = client.zone.get_status(id)
-    self["area#{id}_level"] = status["Level"]
+  def level?(zone_id : Int32)
+    status = client.zone.get_status(zone_id)
+    self["area#{zone_id}_level"] = status["Level"]
   end
 
   @[PlaceOS::Driver::Security(Level::Support)]
-  def level(id : Int32, level : String)
-    client.zone.set_status_level(id: id, level: level)
-    self["area#{id}_level"] = level
+  def level(zone_id : Int32, level : String)
+    client.zone.set_status_level(id: zone_id, level: level)
+    self["zone#{zone_id}_level"] = level
   end
 
   @[PlaceOS::Driver::Security(Level::Support)]
-  def scene(id : Int32, scene : Int32)
-    client.area.set_scene(id: id, scene: scene)
-    self["area#{id}"] = scene
+  def scene(area_id : Int32, scene : Int32)
+    client.area.set_scene(id: area_id, scene: scene)
+    self["area#{area_id}"] = scene
   end
 
-  def scene?(id : Int32)
-    status = client.area.get_status(id: id)
-    self["area#{id}"] = status["CurrentScene"]
+  def scene?(area_id : Int32)
+    status = client.area.get_status(area_id: area_id)
+    self["area#{area_id}"] = status["CurrentScene"]
   end
 
-  def scenes?(id : Int32)
-    client.area.get_scenes(id: id)
+  def scenes?(area_id : Int32)
+    client.area.get_scenes(id: area_id)
   end
 
-  def area?(id : Int32)
-    client.area.get_by_id(id: id)
+  def area?(area_id : Int32)
+    client.area.get_by_id(id: area_id)
+  end
+
+  def zones?(area_id : Int32)
+    client.area.get_zones(id: area_id)
   end
 
   @[PlaceOS::Driver::Security(Level::Support)]
