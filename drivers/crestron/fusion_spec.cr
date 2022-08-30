7d2044575c3cff7a61c56ca2aaed39e24a0a6319
Mia Bennett <mia@chillfox.com>
2022-08-30 13:55:26 +0930

    test(crestron): fusion signal values

--- drivers/crestron/fusion_spec.cr
+++ drivers/crestron/fusion_spec.cr
@@ -10,23 +10,37 @@ DriverSpecs.mock_driver "Crestron::Fusion" do
   })
 
   resp = exec(:get_rooms, "Meeting Room A")
-
   expect_http_request do |request, response|
     response.status_code = 200
     response << rooms.to_json
   end
-
   resp.get
 
   resp = exec(:get_room, "room-id")
-
   expect_http_request do |request, response|
     response.status_code = 200
     response << room.to_json
   end
+  resp.get
 
+  resp = exec(:get_signal_value, "symbol-id", "attribute-id")
+  expect_http_request do |request, response|
+    response.status_code = 200
+    response << signal_value.to_json
+  end
+  resp.get
+
+  resp = exec(:put_signal_value, "symbol-id", "attribute-id", "start")
+  expect_http_request do |request, response|
+    response.status_code = 200
+    response << put_signal_value_response.to_json
+  end
   resp.get
 end
+
+###########
+# Helpers #
+###########
 
 private def rooms
   {
@@ -39,5 +53,23 @@ end
 private def room
   {
     "RoomName" => "Meeting Room A",
+  }
+end
+
+private def signal_value
+  {
+    "API_Signals" => [{
+      "AttributeID"   => "attribute-id",
+      "AttributeName" => "PlaceOS_Enabled",
+      "RawValue"      => "False",
+      "SymbolID"      => "symbol-id",
+    }],
+    "Status" => "Success",
+  }
+end
+
+private def put_signal_value_response
+  {
+    "Status": "Success",
   }
 end
