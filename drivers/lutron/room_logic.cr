f82aee6d2730f246ae681cfef397722eb0de2fad
William Le <will@place.tech>
2022-09-15 00:05:59 +0800

    fix(lutron): generic_name

--- drivers/lutron/room_logic.cr
+++ drivers/lutron/room_logic.cr
@@ -3,7 +3,7 @@ require "placeos-driver"
 # Currently this logic driver is designed for Quantum only but ideally there should be just one Lutron room logic that handles all Lutron APIs
 class Lutron::RoomLogic < PlaceOS::Driver
   descriptive_name "Lutron Room level status "
-  generic_name :RoomEnvironment
+  generic_name :RoomLighting
   description "Exposes the room's lighting state"
 
   default_settings({
