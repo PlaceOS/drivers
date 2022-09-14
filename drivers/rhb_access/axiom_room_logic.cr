029206480ca545d436293c977708c48fac6f1568
William Le <will@place.tech>
2022-09-14 20:06:57 +0800

    faet(axiom/logic): status for analytics

--- drivers/rhb_access/axiom_room_logic.cr
+++ drivers/rhb_access/axiom_room_logic.cr
@@ -7,11 +7,13 @@ class RHBAccess::AxiomRoomLogic < PlaceOS::Driver
 
   default_settings({
     axiom_door_ids: [] of String,
+    axiom_status_poll_cron:  "*/5 * * * *",
   })
 
   accessor axiom : AxiomXa
 
   @door_ids = [] of String
+  @cron_string : String = "*/5 * * * *"
 
   def on_load
     on_update
@@ -19,22 +21,34 @@ class RHBAccess::AxiomRoomLogic < PlaceOS::Driver
 
   def on_update
     @door_ids = setting(Array(String), :axiom_door_ids)
+    @cron_string = setting(String, :axiom_status_poll_cron)
+    schedule.cron(@cron_string) { status? }
   end
 
   def lock
-    @door_ids.map { |d| axiom.lock(d) }
-    self["locked_at"] = Time.local
-    self["doors_locked"] = true
+    @door_ids.map { |d| axiom.lock(d).get }
+  rescue
+    logger.error {"AxiomXa: ERROR while LOCKING #{@door_ids}"}
+  else
+    self["locked_by_placeos_at"] = Time.local
+    status?
   end
 
   def unlock
-    @door_ids.map { |d| axiom.unlock(d) }
-    self["unlocked_at"] = Time.local
-    self["doors_locked"] = false
+    @door_ids.map { |d| axiom.unlock(d).get }
+  rescue
+    logger.error {"AxiomXa: ERROR while UNLOCKING #{@door_ids}"}
+  else
+    self["unlocked_by_placeos_at"] = Time.local
+    status?
   end
 
   def status?
     result = @door_ids.map { |id| {id, axiom.status?(id).get} }
-    result.map { |id, status| self[id] = status }
+  rescue
+    logger.error {"AxiomXa: ERROR requesting STATUS of #{@door_ids}"}
+  else
+    result.map { |id, status| self[id] = status["Status"] }
+    self["doors_locked"] = result.count { |status| status.includes? "Locked" }
   end
 end
