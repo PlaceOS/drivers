aa66ee0aa9a1f9762af3a0e22db1cc33535137fa
Cameron Reeves <cam@place.technology>
2022-10-07 15:24:48 +1100

    feat(leviton): finish basic log saving

--- drivers/leviton/acquisuite.cr
+++ drivers/leviton/acquisuite.cr
@@ -53,11 +53,42 @@ class Leviton::Acquisuite < PlaceOS::Driver
           file = files.not_nil!
           config_contents = file["CONFIGFILE"][0]
           config_file = config_contents.body.gets_to_end
+
           # First update our manifest with the new config data
           @manifest_list << "CONFIGFILE,#{config_contents.filename},#{form_data["MD5CHECKSUM"]},#{form_data["FILETIME"]}"
           define_setting(:manifest_list, @manifest_list)
-          "SUCCESS"
 
+          # Now update our config list with the new config
+          store_config(form_data["MODBUSDEVICE"], config_file)
+        when "LOGFILEUPLOAD"
+          file = files.not_nil!
+          log_contents = file["LOGFILE"][0]
+          log_file = log_contents.body.gets_to_end
+
+          # logger.debug { "GOT LOGFILE" }
+          # logger.debug { @config_list[form_data["MODBUSDEVICE"]] }
+          csv = CSV.new(log_file, headers: true)
+
+          logger.debug { "CSV RANDOM CHECK" }
+          logger.debug { @config_list[form_data["MODBUSDEVICE"]] }
+          logger.debug { @config_list[form_data["MODBUSDEVICE"]].size }
+          logger.debug { @config_list[form_data["MODBUSDEVICE"]].class }
+          data_line = [] of NamedTuple(time: Int64, data: Float64, name: String, units: String)
+          # NOTE: This csv.next structure assumes that there will be a header row we don't need
+          # if this is not the case we should add logic to check for a header 
+          while csv.next
+            @config_list[form_data["MODBUSDEVICE"]].each_with_index do |conf, i|
+              next if @config_list[form_data["MODBUSDEVICE"]][i]["NAME"] == "-\r"
+              csv_index = i + 3
+              reading = {
+                time: Time.parse(csv[0].gsub("'","").strip, "%Y-%m-%d %H:%M:%S", Time::Location::UTC).to_unix,
+                data: csv[csv_index],
+                name: @config_list[form_data["MODBUSDEVICE"]][i]["NAME"],
+                units: @config_list[form_data["MODBUSDEVICE"]][i]["UNITS"]
+              }
+              self["mb-#{form_data["MODBUSDEVICE"]}"] = reading 
+            end
+          end
       end
       
     end
@@ -67,7 +98,7 @@ class Leviton::Acquisuite < PlaceOS::Driver
   end
 
   def store_config(modbusid : String, config : String)
-    index_max = config.split("\n").map{|line|
+    index_max = config.split("\n").map{ |line|
       reg = /POINT(?<index>\d*)(?<name>.*)=(?<value>.*)/.match(line)
       reg[1].to_i if reg
     }.compact.sort.pop
@@ -90,6 +121,7 @@ class Leviton::Acquisuite < PlaceOS::Driver
       end
     end
     @config_list[modbusid] = configs
+    define_setting(:config_list, @config_list)
   end
 
   # Converts the device list to the starting manifest format 
