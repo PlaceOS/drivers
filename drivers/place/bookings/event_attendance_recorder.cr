a70d41cc60a10f6f9f7719e79649796a4201aa5b
Mia Bennett <mia@chillfox.com>
2022-10-17 13:18:01 +0930

    debug(event_attendance_recorder): debug logs

--- drivers/place/bookings/event_attendance_recorder.cr
+++ drivers/place/bookings/event_attendance_recorder.cr
@@ -71,6 +71,7 @@ class Place::EventAttendanceRecorder < PlaceOS::Driver
 
     logger.debug { "#apply_new_state called with new_booking_id: #{new_booking_id}, new_status: #{new_status}" }
     logger.debug { "#apply_new_state current booking_id: #{booking_id}, status: #{status}" }
+    logger.debug { "#apply_new_state current @booking_id: #{@booking_id}, @status: #{@status}" }
 
     if new_booking_id != booking_id || new_status != status
       save_booking_stats(booking_id.not_nil!, people_counts) if @should_save && booking_id
