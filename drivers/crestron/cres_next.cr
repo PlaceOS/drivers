5e2c100eae43e9f1fbf5da237f7341386b4ec087
Stephen von Takach <steve@place.technology>
2022-11-04 02:19:24 +1100

    fix(crestron\occupancy_sensor): doesn't support websockets

--- drivers/crestron/cres_next.cr
+++ drivers/crestron/cres_next.cr
@@ -3,13 +3,14 @@ require "json"
 require "path"
 require "uri"
 require "./nvx_models"
+require "./cres_next_auth"
 
 # Documentation: https://sdkcon78221.crestron.com/sdk/DM_NVX_REST_API/Content/Topics/Prerequisites-Assumptions.htm
 # inspecting request - response packets from the device webui is also useful
 
 # Parent module for Crestron DM NVX devices.
 abstract class Crestron::CresNext < PlaceOS::Driver
-  @xsrf_token : String = ""
+  include Crestron::CresNextAuth
 
   def websocket_headers
     authenticate
@@ -24,61 +25,6 @@ abstract class Crestron::CresNext < PlaceOS::Driver
     schedule.every(10.minutes) { maintain_session }
 
     headers
-  end
-
-  def authenticate
-    logger.debug { "Authenticating" }
-
-    # some devices require referer and origin to accept the login
-    uri = URI.parse config.uri.not_nil!
-    host = uri.host
-
-    response = post("/userlogin.html", headers: {
-      "Content-Type" => "application/x-www-form-urlencoded",
-      "Referer"      => "https://#{host}/userlogin.html",
-      "Origin"       => "https://#{host}",
-    }, body: URI::Params.build { |form|
-      form.add("login", setting(String, :username))
-      form.add("passwd", setting(String, :password))
-    })
-
-    case response.status_code
-    when 200, 302
-      auth_cookies = %w(AuthByPasswd iv tag userid userstr)
-      if (auth_cookies - response.cookies.to_h.keys).empty?
-        @xsrf_token = response.headers["CREST-XSRF-TOKEN"]? || ""
-        logger.debug { "Authenticated" }
-      else
-        error = "Device did not return all auth information"
-      end
-    when 403
-      error = "Invalid credentials"
-    else
-      error = "Unexpected response (HTTP #{response.status})"
-    end
-
-    if error
-      logger.error { error }
-      raise error
-    end
-  end
-
-  def logout
-    response = post "/logout"
-
-    case response.status
-    when 302
-      logger.debug { "Logout successful" }
-      true
-    else
-      logger.warn { "Unexpected response (HTTP #{response.status})" }
-      false
-    end
-  ensure
-    @xsrf_token = ""
-    transport.cookies.clear
-    schedule.clear
-    disconnect
   end
 
   def tokenize(path : String)
