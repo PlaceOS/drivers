113734d8cbb660c0cafbad9526e26a7ea2379fbf
Mia Bennett <mia@chillfox.com>
2023-01-18 14:37:42 +0930

    feat(survey): send invitation emails (#399)

--- drivers/place/staff_api.cr
+++ drivers/place/staff_api.cr
@@ -569,6 +569,38 @@ class Place::StaffAPI < PlaceOS::Driver
     JSON.parse(response.body)
   end
 
+  # ===================================
+  # SURVEYS
+  # ===================================
+
+  def get_survey_invites(survey_id : Int64?, sent : Bool?)
+    logger.debug { "getting survey_invites (survey #{survey_id}, sent #{sent})" }
+    params = URI::Params.new
+    params["survey_id"] = survey_id.to_s if survey_id
+    params["sent"] = sent.to_s if sent
+    params = params.empty? ? "" : "?#{params}"
+    response = get("/api/staff/v1/surveys/invitations#{params}", headers: authentication)
+    raise "issue getting survey invitations (survey #{survey_id}, sent #{sent}): #{response.status_code}" unless response.success?
+    JSON.parse(response.body)
+  end
+
+  @[Security(Level::Support)]
+  def update_survey_invite(
+    token : String,
+    email : String? = nil,
+    sent : Bool? = nil
+  )
+    logger.debug { "updating survey invite #{token}" }
+    response = patch("/api/staff/v1/surveys/invitations/#{token}", headers: authentication, body: {
+      "email" => email,
+      "sent"  => sent,
+    }.compact.to_json)
+    raise "issue updating survey invite #{token}: #{response.status_code}" unless response.success?
+    true
+  end
+
+  # ===================================
+
   @[Security(Level::Support)]
   def signal(channel : String, payload : JSON::Any? = nil)
     placeos_client.root.signal(channel, payload)
---
+++ drivers/place/survey_mailer.cr
@@ -0,0 +1,73 @@
+require "placeos-driver"
+require "placeos-driver/interface/mailer"
+
+class Place::SurveyMailer < PlaceOS::Driver
+  descriptive_name "PlaceOS Survey Mailer"
+  generic_name :SurveyMailer
+  description %(emails survey invites)
+
+  default_settings({
+    timezone:       "GMT",
+    send_invites:   "*/3 * * * *",
+    email_template: "survey",
+  })
+
+  accessor mailer : Mailer_1, implementing: PlaceOS::Driver::Interface::Mailer
+  accessor staff_api : StaffAPI_1
+
+  def on_load
+    on_update
+  end
+
+  @time_zone : Time::Location = Time::Location.load("GMT")
+
+  @visitor_emails_sent : UInt64 = 0_u64
+  @visitor_email_errors : UInt64 = 0_u64
+
+  @email_template : String = "survey"
+  @send_invites : String? = nil
+
+  def on_update
+    @send_invites = setting?(String, :send_invites).presence
+    @email_template = setting?(String, :email_template) || "survey"
+
+    time_zone = setting?(String, :timezone).presence || "GMT"
+    @time_zone = Time::Location.load(time_zone)
+
+    schedule.clear
+    if invites = @send_invites
+      schedule.cron(invites, @time_zone) { send_survey_emails }
+    end
+  end
+
+  @[Security(Level::Support)]
+  def send_survey_emails
+    invites = Array(SurveyInvite).from_json staff_api.get_survey_invites(sent: false).get.to_json
+
+    invites.each do |invite|
+      begin
+        mailer.send_template(
+          to: invite.email,
+          template: {@email_template, "invite"},
+          args: {
+            email: invite.email,
+            token: invite.token,
+          })
+        staff_api.update_survey_invite(sent: true)
+      rescue error
+        logger.warn(exception: error) { "failed to send survey email to #{invite.email}" }
+      end
+    end
+  end
+
+  struct SurveyInvite
+    include JSON::Serializable
+
+    property id : Int64
+    property token : String
+    property email : String
+    property sent : Bool
+    property created_at : Time
+    property updated_at : Time
+  end
+end
