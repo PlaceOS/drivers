cbce0bc6c606cedfb363557f8060b213ed1b5ed3
Giorgi Kavrelishvili <giorgi.kavrelishvili@pm.me>
2023-06-26 06:27:51 +0400

    Update the functionality with additional endpoints (#449)

--- drivers/johnson_controls/metasys_models.cr
+++ drivers/johnson_controls/metasys_models.cr
@@ -13,6 +13,201 @@ module JohnsonControls
     property expires : Time
   end
 
+  class NetworkDevice
+    include JSON::Serializable
+
+    @[JSON::Field(key: "id")]
+    property id : String
+
+    @[JSON::Field(key: "itemReference")]
+    property item_reference : String
+
+    @[JSON::Field(key: "name")]
+    property name : String
+
+    @[JSON::Field(key: "typeUrl")]
+    property type_url : String
+
+    @[JSON::Field(key: "self")]
+    property self : String
+
+    @[JSON::Field(key: "parentUrl")]
+    property parent_url : String
+
+    @[JSON::Field(key: "objectsUrl")]
+    property objects_url : String
+
+    @[JSON::Field(key: "networkDeviceUrl")]
+    property network_device_url : String
+
+    @[JSON::Field(key: "pointsUrl")]
+    property points_url : String
+
+    @[JSON::Field(key: "trendedAttributesUrl")]
+    property trended_attributes_url : String
+
+    @[JSON::Field(key: "alarmsUrl")]
+    property alarms_url : String
+
+    @[JSON::Field(key: "auditsUrl")]
+    property audits_url : String
+  end
+
+  class Equipment
+    include JSON::Serializable
+
+    @[JSON::Field(key: "id")]
+    property id : String
+
+    @[JSON::Field(key: "itemReference")]
+    property item_reference : String
+
+    @[JSON::Field(key: "name")]
+    property name : String
+
+    @[JSON::Field(key: "type")]
+    property type : String
+
+    @[JSON::Field(key: "self")]
+    property self : String
+
+    @[JSON::Field(key: "spacesUrl")]
+    property spaces_url : String
+
+    @[JSON::Field(key: "networkDeviceUrl")]
+    property network_device_url : String
+
+    @[JSON::Field(key: "equipmentUrl")]
+    property equipment_url : String
+
+    @[JSON::Field(key: "upstreamEquipmentUrl")]
+    property upstream_equipment_url : String
+
+    @[JSON::Field(key: "pointsUrl")]
+    property points_url : String
+  end
+
+  class Attribute
+    include JSON::Serializable
+
+    @[JSON::Field(key: "smaplesUrl")]
+    property smaples_url : String
+
+    @[JSON::Field(key: "attributeUrl")]
+    property attribute_url : String
+  end
+
+  class Sample
+    include JSON::Serializable
+
+    @[JSON::Field(key: "timestamp")]
+    property timestamp : String
+
+    @[JSON::Field(key: "isReliable")]
+    property reliable : Bool
+
+    @[JSON::Field(key: "value")]
+    property value : Hash(String, JSON::Any)
+  end
+
+  class GetSamplesForAnObjectAttributeResponse
+    include JSON::Serializable
+
+    @[JSON::Field(key: "total")]
+    property total : Int32
+
+    @[JSON::Field(key: "items")]
+    property items : Array(Sample)
+
+    @[JSON::Field(key: "next")]
+    property next : String?
+
+    @[JSON::Field(key: "previous")]
+    property previous : String?
+
+    @[JSON::Field(key: "self")]
+    property self : String
+
+    @[JSON::Field(key: "attributeUrl")]
+    property attribute_url : String
+
+    @[JSON::Field(key: "objectUrl")]
+    property object_url : String
+  end
+
+  class GetNetworkDeviceChildrenResponse
+    include JSON::Serializable
+
+    @[JSON::Field(key: "total")]
+    property total : Int32
+
+    @[JSON::Field(key: "items")]
+    property items : Array(NetworkDevice)
+
+    @[JSON::Field(key: "next")]
+    property next : String?
+
+    @[JSON::Field(key: "previous")]
+    property previous : String?
+
+    @[JSON::Field(key: "self")]
+    property self : String
+  end
+
+  class GetObjectAttributesWithSamplesResponse
+    include JSON::Serializable
+
+    @[JSON::Field(key: "total")]
+    property total : Int32
+
+    @[JSON::Field(key: "items")]
+    property items : Array(Attribute)
+
+    @[JSON::Field(key: "self")]
+    property self : String
+  end
+
+  class GetEquipmentHostedByNetworkDeviceResponse
+    include JSON::Serializable
+
+    @[JSON::Field(key: "total")]
+    property total : Int32
+
+    @[JSON::Field(key: "items")]
+    property items : Array(Equipment)
+
+    @[JSON::Field(key: "next")]
+    property next : String?
+
+    @[JSON::Field(key: "previous")]
+    property previous : String?
+
+    @[JSON::Field(key: "self")]
+    property self : String
+  end
+
+  class Command
+    include JSON::Serializable
+
+    @[JSON::Field(key: "commandId")]
+    property command_id : String
+
+    @[JSON::Field(key: "title")]
+    property title : String
+
+    @[JSON::Field(key: "type")]
+    property type : String = "array"
+
+    @[JSON::Field(key: "items")]
+    property items : Array(JSON::Any)
+
+    @[JSON::Field(key: "minItems")]
+    property minimum_items : Int32
+
+    @[JSON::Field(key: "maxItems")]
+    property maximum_items : Int32
+  end
+
   class EquipmentPoints
     include JSON::Serializable
 
