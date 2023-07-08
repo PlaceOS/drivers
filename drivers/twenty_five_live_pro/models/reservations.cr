c6d91bd466c4e5d830feffbab7500c8243ebab25
MarioFavalliR <mario@place.technology>
2023-07-08 12:40:06 +0100

    fix(25LivePro): fix res model/getter

--- drivers/twenty_five_live_pro/models/reservations.cr
+++ drivers/twenty_five_live_pro/models/reservations.cr
@@ -4,10 +4,9 @@ module TwentyFiveLivePro
   module Models
     struct Reservation
       include JSON::Serializable
-      include JSON::Serializable::Unmapped
 
-      @[JSON::Field(key: "post_event_dt")]
-      property post_event_dt : String
+      @[JSON::Field(converter: String::RawConverter)]
+      getter post_event_dt : String?
 
       @[JSON::Field(key: "registration_url")]
       property registration_url : String
