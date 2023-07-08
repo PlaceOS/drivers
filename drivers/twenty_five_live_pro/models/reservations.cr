646b09c1893aa2d41bd627665dcbb2346a901d34
MarioFavalliR <mario@place.technology>
2023-07-08 12:56:00 +0100

    fix(25LivePro): fix to reservationBody_post_event

--- drivers/twenty_five_live_pro/models/reservations.cr
+++ drivers/twenty_five_live_pro/models/reservations.cr
@@ -5,8 +5,8 @@ module TwentyFiveLivePro
     struct Reservation
       include JSON::Serializable
 
-      @[JSON::Field(converter: String::RawConverter)]
-      getter post_event_dt : String?
+      @[JSON::Field(key: "postEventDt")]
+      property post_event_dt : String
 
       @[JSON::Field(key: "registration_url")]
       property registration_url : String
