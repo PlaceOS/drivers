1114cc380f10c48c9c106dcfc1e7be27210171af
MarioFavalliR <mario@place.technology>
2023-07-17 20:59:44 +0300

    fix(25LivePro): reservation model and room_schedul with reservation

---
+++ drivers/twenty_five_live_pro/models/reservation.cr
@@ -0,0 +1,25 @@
+require "json"
+
+module TwentyFiveLivePro
+  module Models
+    struct Reservation
+      include JSON::Serializable
+
+      @[JSON::Field(key: "reservation_id")]
+      property reservation_id : Int64
+
+      @[JSON::Field(key: "event_title")]
+      property event_title : String?
+
+      @[JSON::Field(key: "reservation_start_dt" )]
+      property reservation_start_dt : String
+
+      @[JSON::Field(key: "reservation_end_dt")]
+      property reservation_end_dt : String
+
+      @[JSON::Field(key: "registered_count")]
+      property registered_count : Int64?
+
+    end
+  end
+end
