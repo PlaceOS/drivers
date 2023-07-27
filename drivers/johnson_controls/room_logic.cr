bbd347e7781ae0706408b3743caa2b54f35eb785
MarioFavalliR <mario@place.technology>
2023-07-27 20:48:03 +0300

    fix: id to string

--- drivers/johnson_controls/room_logic.cr
+++ drivers/johnson_controls/room_logic.cr
@@ -7,14 +7,14 @@ class JohnsonControls::RoomLogic < PlaceOS::Driver
   description %(Polls Johnson Controls Metasys API Module to expose object present value)
 
   default_settings({
-    object_id: "set obcject ID here",
+    id: "set obcject ID here",
     polling_cron:                  "*/15 * * * *",
     debug:                         false,
   })
 
   accessor johnson_controls : Control
 
-  @object_id : String = "set obcject ID here"
+  @id : String = "set obcject ID here"
   @cron_string : String = "*/15 * * * *"
   @debug : Bool = false
   @next_countdown : PlaceOS::Driver::Proxy::Scheduler::TaskWrapper? = nil
@@ -27,14 +27,14 @@ class JohnsonControls::RoomLogic < PlaceOS::Driver
 
   def on_update
     @debug = setting(Bool, :debug) || false
-    @object_id = setting(String, :object_id)
+    @id = setting(String, :id)
     @cron_string = setting(String, :polling_cron)
     schedule.clear
     schedule.cron(@cron_string, immediate: true) { fetch_present_value }
   end
 
   def fetch_present_value
-    values = GetSingleObjectPresentValueResponse.from_json(johnson_controls.get_single_object_presentValue(object_id.to_s).get.not_nil!.to_json)
+    values = GetSingleObjectPresentValueResponse.from_json(johnson_controls.get_single_object_presentValue(@id).get.to_json)
     self["present_value"] = values.item.presentValue.value
   end
 end
