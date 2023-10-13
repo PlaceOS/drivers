22f35403aee2d6b599e5e12975379d5e332b6b3f
Stephen von Takach <steve@place.technology>
2023-10-13 12:20:32 +1100

    feat: add example LLM chat capability

---
+++ drivers/place/llm/todo_list.cr
@@ -0,0 +1,34 @@
+require "placeos-driver"
+require "placeos-driver/interface/chat_functions"
+
+class Place::TODOs < PlaceOS::Driver
+  include PlaceOS::Driver::Interface::ChatFunctions
+
+  descriptive_name "PlaceOS TODO list"
+  generic_name :TODO
+  description %(an example of a driver providing functions to a LLM)
+
+  @todos = [] of NamedTuple(complete: Bool, task: String)
+
+  def capabilities : String
+    "manages the list of tasks a user needs to complete throughout the day"
+  end
+
+  @[Description("returns the list of tasks and their current status")]
+  def list_tasks
+    @todos
+  end
+
+  @[Description("adds a new task to the list")]
+  def add_task(description : String)
+    task = {complete: false, task: description}
+    @todos << task
+    task
+  end
+
+  @[Description("marks a task as completed")]
+  def complete_task(index : Int32)
+    task = @todos[index]
+    @todos[index] = {complete: true, task: task[:task]}
+  end
+end
