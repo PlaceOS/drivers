91846f3e4e4c209b658cc044fff86bcb95373167
Stephen von Takach <steve@place.technology>
2023-10-13 11:34:26 +1100

    feat(place/llm): implement LLM driver interaction framework

---
+++ drivers/place/llm/llm.cr
@@ -0,0 +1,52 @@
+require "placeos-driver"
+require "placeos-driver/interface/chat_functions"
+
+class Place::LLM < PlaceOS::Driver
+  descriptive_name "PlaceOS LLM Interface"
+  generic_name :LLM
+  description %(an interface for LLMs such as ChatGPT for discovering capabilities)
+
+  default_settings({
+    prompt: %(
+you are an AI assistant in a smart building.
+Helping a staff member with every day tasks.
+Don't disclose that you're an AI
+Skip language that implies regret or apology
+say 'I don't know' for unknowns
+skip expert disclaimers
+no repetitive answers
+don't direct to other sources
+focus on key points in questions
+simplify complex issues with steps
+clarify unclear questions before answering
+correct errors in previous answers
+),
+  })
+
+  def on_load
+    on_update
+  end
+
+  def on_update
+    @prompt = setting(String, :prompt)
+  end
+
+  getter! prompt : String
+
+  def capabilities
+    system.implementing(Interface::ChatFunctions).map do |driver|
+      {
+        id:         driver.module_name,
+        capability: driver.capabilities.get.as_s,
+      }
+    end
+  end
+
+  def new_chat
+    {
+      prompt:       @prompt,
+      capabilities: capabilities,
+      system_id:    system.id,
+    }
+  end
+end
---
+++ drivers/place/llm/llm_spec.cr
@@ -0,0 +1,86 @@
+require "placeos-driver/spec"
+require "placeos-driver/interface/chat_functions"
+
+DriverSpecs.mock_driver "Place::Bookings" do
+  system({
+    DeskBookings: {DeskMock},
+    RoomBookings: {RoomMock},
+  })
+
+  sleep 200.milliseconds
+
+  exec(:capabilities).get.should eq [
+    {
+      "id"         => "DeskBookings",
+      "capability" => "provides methods listing current desk bookings and booking or allocating a new desk booking",
+    },
+    {
+      "id"         => "RoomBookings",
+      "capability" => "provides methods listing current meeting room bookings or events and booking new meetings or events",
+    },
+  ]
+
+  system(:DeskBookings_1).function_schemas.should eq({
+    "list_of_levels" => {} of String => JSON::Any,
+    "book"           => {
+      "level" => {
+        "anyOf"   => [{"type" => "null"}, {"type" => "string"}],
+        "title"   => "(String | Nil)",
+        "default" => nil,
+      },
+      "days_in_future" => {
+        "type"    => "integer",
+        "format"  => "Int32",
+        "title"   => "Int32",
+        "default" => 0,
+      },
+    },
+  })
+end
+
+# :nodoc:
+class DeskMock < DriverSpecs::MockDriver
+  include PlaceOS::Driver::Interface::ChatFunctions
+
+  def capabilities : String
+    "provides methods listing current desk bookings and booking or allocating a new desk booking"
+  end
+
+  @[Description("returns the list of levels with available desks")]
+  def list_of_levels
+    {"level 1", "level 2"}
+  end
+
+  @[Description("books a desk, you can optionally provide a preferred level or how many days from now if the booking is for tomorrow etc")]
+  def book(level : String? = nil, days_in_future : Int32 = 0)
+    "you've been allocated desk 123 on #{level || "level 2"}"
+  end
+end
+
+# :nodoc:
+class RoomMock < DriverSpecs::MockDriver
+  include PlaceOS::Driver::Interface::ChatFunctions
+
+  def capabilities : String
+    "provides methods listing current meeting room bookings or events and booking new meetings or events"
+  end
+
+  def my_bookings(days_in_future : Int32 = 0)
+    [{
+      room:       "room@site.com",
+      booking_id: "12345",
+      title:      "Blah",
+      organizer:  "Janis",
+    }]
+  end
+
+  @[Description("returns the list of levels with available rooms")]
+  def list_of_levels(number_of_attendees : Int32)
+    {"level 1", "level 2"}
+  end
+
+  @[Description("books a room, you can optionally provide a preferred level or how many days from now if the booking is for tomorrow etc")]
+  def book(number_of_attendees : Int32, level : String? = nil, days_in_future : Int32 = 0)
+    "you've been allocated room studio 2 on #{level || "level 2"}"
+  end
+end
