ec7309b12ef4c204d0476ad903144248da3e0f73
Giorgi Kavrelishvili <giorgi.kavrelishvili@pm.me>
2023-10-31 10:34:03 +0400

    fix(delta): PROJ-428 Convert ValueProperty class fields from mandatory to optional

--- drivers/delta/models/value_property.cr
+++ drivers/delta/models/value_property.cr
@@ -7,49 +7,49 @@ module Delta
       include JSON::Serializable
 
       @[JSON::Field(key: "$base")]
-      property base : String
+      property base : String?
 
       @[JSON::Field(key: "displayName")]
-      property display_name : String
+      property display_name : String?
 
       @[JSON::Field(key: "object-identifier")]
-      property object_identifier : GenericValue
+      property object_identifier : GenericValue?
 
       @[JSON::Field(key: "object-type")]
-      property object_type : GenericValue
+      property object_type : GenericValue?
 
       @[JSON::Field(key: "object-name")]
-      property object_name : GenericValue
+      property object_name : GenericValue?
 
       @[JSON::Field(key: "exchange-flags")]
-      property exchange_flags : GenericValue
+      property exchange_flags : GenericValue?
 
       @[JSON::Field(key: "exchange-type")]
-      property exchange_type : GenericValue
+      property exchange_type : GenericValue?
 
       @[JSON::Field(key: "last-error")]
-      property last_error : GenericValue
+      property last_error : GenericValue?
 
       @[JSON::Field(key: "local-ref")]
-      property local_ref : Reference
+      property local_ref : Reference?
 
       @[JSON::Field(key: "local-flags")]
-      property local_flags : GenericValue
+      property local_flags : GenericValue?
 
       @[JSON::Field(key: "local-value")]
-      property local_flags : LocalValue
+      property local_flags : LocalValue?
 
       @[JSON::Field(key: "subscribers")]
-      property subscribers : Hash(String, JSON::Any)
+      property subscribers : Hash(String, JSON::Any)?
 
       @[JSON::Field(key: "last-sent")]
-      property last_sent : GenericValue
+      property last_sent : GenericValue?
 
       @[JSON::Field(key: "send-frequency")]
-      property send_frequency : GenericValue
+      property send_frequency : GenericValue?
 
       @[JSON::Field(key: "cov-increment")]
-      property cov_increment : GenericValue
+      property cov_increment : GenericValue?
     end
   end
 end
