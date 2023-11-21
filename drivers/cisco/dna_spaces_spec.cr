da120b62a3006ed285516918110b8e6c591e36e4
Stephen von Takach <steve@place.technology>
2023-11-21 12:52:59 +1100

    fix(cisco/dnaspaces): timestamp where no position is provided

--- drivers/cisco/dna_spaces_spec.cr
+++ drivers/cisco/dna_spaces_spec.cr
@@ -1,6 +1,26 @@
 require "placeos-driver/spec"
 
 DriverSpecs.mock_driver "Cisco::DNASpaces" do
+  settings({
+    dna_spaces_activation_key: "provide this and the API / tenant ids will be generated automatically",
+    dna_spaces_api_key:        "X-API-KEY",
+    tenant_id:                 "sfdsfsdgg",
+    verify_activation_key:     false,
+    max_location_age:          300,
+    floorplan_mappings:        {
+      location_a4cb0: {
+        "level_name" => "optional name",
+        "building"   => "zone-GAsXV0nc",
+        "level"      => "zone-GAsmleH",
+        "offset_x"   => 12.4,
+        "offset_y"   => 5.2,
+        "map_width"  => 50.3,
+        "map_height" => 100.9,
+      },
+    },
+    debug_stream: false,
+  })
+
   # The dashboard should request the streaming API
   expect_http_request do |request, response|
     headers = request.headers
