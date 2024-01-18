9d100adcdc9ff87e02e6d1a1fddff5cb00df57c0
William Le <will@place.tech>
2024-01-18 16:18:16 +0800

    feat(place/rbp_remote_logger): expose past logs as status vars

--- drivers/place/rbp_remote_logger.cr
+++ drivers/place/rbp_remote_logger.cr
@@ -12,7 +12,7 @@ class Place::RbpRemoteLogger < PlaceOS::Driver
   })
 
   @enabled : Bool = false
-  @max_log_entries : UInt32 = 1000_u32
+  @max_log_entries : Int32 = 1000
   @debug : Bool = false
 
   def on_load
@@ -21,7 +21,7 @@ class Place::RbpRemoteLogger < PlaceOS::Driver
 
   def on_update
     @logging_enabled = setting?(Bool, "enabled") || true
-    @max_log_entries = setting?(UInt32, "max_log_entries") || 1000_u32
+    @max_log_entries = setting?(Int32, "max_log_entries") || 1000
     @debug = setting?(Bool, "debug") || false
 
     self[:enabled] = @logging_enabled
@@ -29,12 +29,12 @@ class Place::RbpRemoteLogger < PlaceOS::Driver
 
   def post_event(payload : String)
     logger.debug { "Received: #{payload}" }
-    # log_entry = LogEntry.from_json(payload)
-    # self[log_entry.device_id] ||= [] of JSON::Any
-    # self[log_entry.device_id] = self[log_entry.device_id].as_a.unshift(log_entry).truncate(0, @max_log_entries)
+    log_entry = LogEntry.from_json(payload)
+    self[log_entry.device_id] ||= [] of JSON::Any
+    self[log_entry.device_id] = self[log_entry.device_id].as_a.unshift(JSON::Any.new(log_entry.to_json)).truncate(0, @max_log_entries)
   end
 
-  struct LogEntry
+  class LogEntry
     include JSON::Serializable
     
     property id : String
