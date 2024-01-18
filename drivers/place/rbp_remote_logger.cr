ee691eb3b3449ec2a9e3a7f3c5d7a9562be5b0c4
William Le <will@place.tech>
2024-01-18 15:43:04 +0800

    chore(place/rbp_remote_logger): prelim idea for exposing past logs as status vars

--- drivers/place/rbp_remote_logger.cr
+++ drivers/place/rbp_remote_logger.cr
@@ -27,8 +27,23 @@ class Place::RbpRemoteLogger < PlaceOS::Driver
     self[:enabled] = @logging_enabled
   end
 
-  def post_event(payload : JSON::Any)
+  def post_event(payload : String)
     logger.debug { "Received: #{payload}" }
+    # log_entry = LogEntry.from_json(payload)
+    # self[log_entry.device_id] ||= [] of JSON::Any
+    # self[log_entry.device_id] = self[log_entry.device_id].as_a.unshift(log_entry).truncate(0, @max_log_entries)
   end
 
+  struct LogEntry
+    include JSON::Serializable
+    
+    property id : String
+    property device_id : String
+    property type : String
+    property subtype : String
+    property timestamp : Int32
+    property raw : JSON::Any
+    property data : JSON::Any
+    property metadata : JSON::Any
+  end
 end
