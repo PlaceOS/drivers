4aac2bab0be40c40bc14460a09980d03b2230d80
Mia Bennett <mia@chillfox.com>
2024-06-12 10:54:27 +0930

    doc(auto_release): comment time comparison code

--- drivers/place/auto_release.cr
+++ drivers/place/auto_release.cr
@@ -144,7 +144,11 @@ class Place::AutoRelease < PlaceOS::Driver
       if preferences = get_user_preferences?(booking.user_id)
         day_of_week = Time.unix(booking.booking_start).day_of_week.value
         day_of_week = 0 if day_of_week == 7 # Crystal uses 7 for Sunday, but we use 0 (all other days match up)
+
+        # convert unix timestamp to float hours/minutes
+        # e.g. 7:30AM = 7.5
         event_time = Time.unix(booking.booking_start).hour + (Time.unix(booking.booking_start).minute / 60.0)
+
         if (override = preferences[:work_overrides][Time.unix(booking.booking_start).to_s(format: "%F")]?) &&
            in_preference_hours?(override.start_time, override.end_time, event_time) &&
            (@release_locations.includes? override.location)
