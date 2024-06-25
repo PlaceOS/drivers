e8bd23780dbf9930114a5929eec55255036f3776
William Le <will@place.technology>
2024-06-25 18:40:37 +0800

    chore(place/visitor_mailer): Default QR generation to FALSE as that is much more common

--- drivers/place/visitor_mailer.cr
+++ drivers/place/visitor_mailer.cr
@@ -25,7 +25,7 @@ class Place::VisitorMailer < PlaceOS::Driver
     booking_template:                   "booking",
     notify_checkin_template:            "notify_checkin",
     group_event_template:               "group_event",
-    disable_qr_code:                    false,
+    disable_qr_code:                    true,
     send_network_credentials:           false,
     network_password_length:            DEFAULT_PASSWORD_LENGTH,
     network_password_exclude:           DEFAULT_PASSWORD_EXCLUDE,
@@ -69,7 +69,7 @@ class Place::VisitorMailer < PlaceOS::Driver
 
   @visitor_emails_sent : UInt64 = 0_u64
   @visitor_email_errors : UInt64 = 0_u64
-  @disable_qr_code : Bool = false
+  @disable_qr_code : Bool = true
   @host_domain_filter : Array(String) = [] of String
 
   # See: https://crystal-lang.org/api/0.35.1/Time/Format.html
@@ -110,7 +110,7 @@ class Place::VisitorMailer < PlaceOS::Driver
     @booking_template = setting?(String, :booking_template) || "booking"
     @notify_checkin_template = setting?(String, :notify_checkin_template) || "notify_checkin"
     @group_event_template = setting?(String, :group_event_template) || "group_event"
-    @disable_qr_code = setting?(Bool, :disable_qr_code) || false
+    @disable_qr_code = setting?(Bool, :disable_qr_code) || true
     @determine_host_name_using = setting?(String, :determine_host_name_using) || "calendar-driver"
     @send_network_credentials = setting?(Bool, :send_network_credentials) || false
     @network_password_length = setting?(Int32, :password_length) || DEFAULT_PASSWORD_LENGTH
