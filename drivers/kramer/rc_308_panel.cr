7eb8c03f4f266397136b066b0ce17d7955a451d4
Stephen von Takach <steve@place.technology>
2024-07-04 12:38:53 +1000

    fix(kramer/rc_308_panel): set button state for triggers

--- drivers/kramer/rc_308_panel.cr
+++ drivers/kramer/rc_308_panel.cr
@@ -32,6 +32,10 @@ class Kramer::RC308Panel < PlaceOS::Driver
   def on_load
     transport.tokenizer = Tokenizer.new(DELIMITER)
     on_update
+
+    (0..@button_count).each do |idx|
+      self["button#{idx}_state"] = ButtonAction::Released
+    end
   end
 
   def on_update
@@ -60,6 +64,7 @@ class Kramer::RC308Panel < PlaceOS::Driver
   end
 
   def button_state?(index : UInt8, priority : Int32 = 0)
+    send "#BTN? #{index}\r", priority: priority
     send "#RGB? #{index}\r", priority: priority
   end
 
