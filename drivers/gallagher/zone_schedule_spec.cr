a9fbc114970ab2322dd50751593b2d4f7b5ed866
Stephen von Takach <steve@place.technology>
2024-11-18 15:14:38 +1100

    chore(gallagher/zone_schedule): update door zone setting

--- drivers/gallagher/zone_schedule_spec.cr
+++ drivers/gallagher/zone_schedule_spec.cr
@@ -11,11 +11,26 @@ DriverSpecs.mock_driver "Gallagher::ZoneSchedule" do
   exec(:count).get.should eq 0
   bookings = system(:Bookings).as(BookingsMock)
   bookings.new_meeting
-  sleep 1
+  sleep 500.milliseconds
   exec(:count).get.should eq 1
 
   # check the update that was applied
-  system(:Gallagher)[:freed].should eq("1234")
+  system(:Gallagher)[:state].should eq(["free", "1234"])
+
+  bookings.presence(true)
+  sleep 500.milliseconds
+  exec(:count).get.should eq 2
+  system(:Gallagher)[:state].should eq(["free", "1234"])
+
+  bookings.end_meeting
+  sleep 500.milliseconds
+  exec(:count).get.should eq 3
+  system(:Gallagher)[:state].should eq(["free", "1234"])
+
+  bookings.presence(false)
+  sleep 500.milliseconds
+  exec(:count).get.should eq 4
+  system(:Gallagher)[:state].should eq(["locked", "1234"])
 end
 
 # :nodoc:
@@ -23,12 +38,25 @@ class BookingsMock < DriverSpecs::MockDriver
   def new_meeting : Nil
     self[:status] = "pending"
   end
+
+  def presence(state : Bool)
+    self[:presence] = state
+  end
+
+  def end_meeting : Nil
+    self[:status] = "free"
+  end
 end
 
 # :nodoc:
 class GallagherMock < DriverSpecs::MockDriver
   def free_zone(zone_id : String | Int32)
-    self[:freed] = zone_id.to_s
+    self[:state] = {:free, zone_id.to_s}
+    true
+  end
+
+  def reset_zone(zone_id : String | Int32)
+    self[:state] = {:locked, zone_id.to_s}
     true
   end
 end
