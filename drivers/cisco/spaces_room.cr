fb8fb3da77a6cc7fcd96463bb33df55f2ab9ee3f
Stephen von Takach <steve@place.technology>
2024-11-19 15:56:04 +1100

    feat(cisco/spaces_room): driver for room local sensor data

---
+++ drivers/cisco/spaces_room.cr
@@ -0,0 +1,63 @@
+require "placeos-driver"
+require "simple_retry"
+
+class Cisco::SpacesRoom < PlaceOS::Driver
+  include Interface::Sensor
+
+  descriptive_name "Cisco Spaces Room Sensors"
+  generic_name :SpacesRoomSensors
+  description "exposes sensor information to the room"
+
+  default_settings({
+    _cisco_spaces_system: "sys-12345",
+    _cisco_spaces_module: "Cisco_Spaces",
+
+    space_room_id: "a410b6d676",
+  })
+
+  getter system_id : String = ""
+  getter module_name : String = ""
+  getter room_id : String = ""
+
+  def on_load
+    on_update
+  end
+
+  def on_update
+    @system_id = setting?(String, :cisco_spaces_system).presence || config.control_system.not_nil!.id
+    @module_name = setting?(String, :cisco_spaces_module).presence || "Cisco_Spaces"
+    @room_id = setting(String, :space_room_id)
+  end
+
+  private def cisco_spaces
+    system(system_id)[module_name]
+  end
+
+  # ======================
+  # Sensor interface
+  # ======================
+
+  SENSOR_TYPES = {SensorType::PeopleCount, SensorType::Presence, SensorType::Humidity, SensorType::Temperature, SensorType::AirQuality, SensorType::SoundPressure}
+  NO_MATCH     = [] of Interface::Sensor::Detail
+
+  def sensors(type : String? = nil, mac : String? = nil, zone_id : String? = nil) : Array(Interface::Sensor::Detail)
+    logger.debug { "sensors of type: #{type}, mac: #{mac}, zone_id: #{zone_id} requested" }
+
+    return NO_MATCH if mac && mac != @room_id
+    if type
+      sensor_type = SensorType.parse(type)
+      return NO_MATCH unless SENSOR_TYPES.includes?(sensor_type)
+    end
+    return NO_MATCH if zone_id && !system.zones.includes?(zone_id)
+
+    Array(Interface::Sensor::Detail).from_json cisco_spaces.sensors(type, mac, zone_id).get.to_json
+  end
+
+  def sensor(mac : String, id : String? = nil) : Interface::Sensor::Detail?
+    logger.debug { "sensor mac: #{mac}, id: #{id} requested" }
+    return nil unless id
+    return nil unless mac == @room_id
+
+    Interface::Sensor::Detail?.from_json(cisco_spaces.sensors(mac, id).get.to_json)
+  end
+end
---
+++ drivers/cisco/spaces_room_spec.cr
@@ -0,0 +1,4 @@
+require "placeos-driver/spec"
+
+DriverSpecs.mock_driver "Cisco::SpacesRoom" do
+end
