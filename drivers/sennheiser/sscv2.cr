d6a7e1c23dd5fdfe229476991c9107f1f21bbe21
Stephen von Takach <steve@place.technology>
2025-06-20 13:27:16 +1000

    add methods

--- drivers/sennheiser/sscv2.cr
+++ drivers/sennheiser/sscv2.cr
@@ -4,7 +4,7 @@ require "./sscv2_models"
 class Sennheiser::SSCv2Driver < PlaceOS::Driver
   descriptive_name "Sennheiser Sound Control Protocol v2"
   generic_name :AudioDevice
-  description "Driver for Sennheiser devices supporting SSCv2 protocol. Requires third-party access to be enabled in Sennheiser Control Cockpit with configured password. Automatically subscribes to /api/device/status and /api/device/info when running_specs is false or nil."
+  description "Driver for Sennheiser TeamConnect Ceiling Microphone (TCCM) using SSCv2 protocol. Requires third-party access to be enabled in Sennheiser Control Cockpit with configured password. Automatically subscribes to real-time device state, beam direction, audio levels, and room occupancy when running_specs is false or nil."
 
   uri_base "https://device_ip"
 
@@ -53,7 +53,14 @@ class Sennheiser::SSCv2Driver < PlaceOS::Driver
     return if @running_specs
 
     # Add default subscriptions
-    default_resources = ["/api/device/status", "/api/device/info"]
+    default_resources = [
+      "/api/device/state",
+      "/api/device/identity",
+      "/api/audio/inputs/microphone/beam/direction",
+      "/api/audio/inputs/microphone/level",
+      "/api/audio/roomInUse",
+      "/api/audio/roomInUse/activityLevel",
+    ]
     default_resources.each do |resource|
       @subscription_resources << resource unless @subscription_resources.includes?(resource)
     end
@@ -87,32 +94,358 @@ class Sennheiser::SSCv2Driver < PlaceOS::Driver
 
   # === API Methods ===
 
-  def device_info
-    get("/api/device/info")
+  # SSC and Device Info
+  def ssc_version
+    get("/api/ssc/version")
+  end
+
+  def ssc_schema
+    get("/api/ssc/schema")
+  end
+
+  def device_identity
+    get("/api/device/identity")
+  end
+
+  def device_identification
+    get("/api/device/identification")
+  end
+
+  def set_device_identification(visual : Bool)
+    put("/api/device/identification", body: {visual: visual}.to_json, headers: {"Content-Type" => "application/json"})
   end
 
   def device_site
     get("/api/device/site")
   end
 
-  def device_status
-    get("/api/device/status")
+  def device_state
+    get("/api/device/state")
   end
 
-  def ssc_version
-    get("/api/ssc/version")
+  def firmware_update_state
+    get("/api/firmware/update/state")
   end
 
-  def set_device_name(name : String)
-    put("/api/device/site", body: {name: name}.to_json, headers: {"Content-Type" => "application/json"})
+  def set_device_name(deviceName : String)
+    put("/api/device/site", body: {deviceName: deviceName}.to_json, headers: {"Content-Type" => "application/json"})
   end
 
   def set_device_location(location : String)
     put("/api/device/site", body: {location: location}.to_json, headers: {"Content-Type" => "application/json"})
   end
 
-  def set_device_site(site : String)
-    put("/api/device/site", body: {site: site}.to_json, headers: {"Content-Type" => "application/json"})
+  def set_device_position(position : String)
+    put("/api/device/site", body: {position: position}.to_json, headers: {"Content-Type" => "application/json"})
+  end
+
+  # Audio Control
+  def audio_global_mute
+    get("/api/audio/outputs/global/mute")
+  end
+
+  def set_audio_global_mute(enabled : Bool)
+    put("/api/audio/outputs/global/mute", body: {enabled: enabled}.to_json, headers: {"Content-Type" => "application/json"})
+  end
+
+  def microphone_beam
+    get("/api/audio/inputs/microphone/beam")
+  end
+
+  def set_microphone_beam(installationType : String? = nil, sourceDetectionThreshold : String? = nil, offset : Int32? = nil)
+    body = {} of String => JSON::Any::Type
+    body["installationType"] = installationType if installationType
+    body["sourceDetectionThreshold"] = sourceDetectionThreshold if sourceDetectionThreshold
+    body["offset"] = offset.to_i64 if offset
+    put("/api/audio/inputs/microphone/beam", body: body.to_json, headers: {"Content-Type" => "application/json"})
+  end
+
+  def beam_direction
+    get("/api/audio/inputs/microphone/beam/direction")
+  end
+
+  def microphone_level
+    get("/api/audio/inputs/microphone/level")
+  end
+
+  def reference_level
+    get("/api/audio/inputs/reference/level")
+  end
+
+  def room_in_use
+    get("/api/audio/roomInUse")
+  end
+
+  def room_in_use_activity_level
+    get("/api/audio/roomInUse/activityLevel")
+  end
+
+  def room_in_use_config
+    get("/api/audio/roomInUse/config")
+  end
+
+  # Audio Outputs
+  def analog_output
+    get("/api/audio/outputs/analog")
+  end
+
+  def set_analog_output(gain : Int32? = nil, switch : String? = nil)
+    body = {} of String => JSON::Any::Type
+    body["gain"] = gain.to_i64 if gain
+    body["switch"] = switch if switch
+    put("/api/audio/outputs/analog", body: body.to_json, headers: {"Content-Type" => "application/json"})
+  end
+
+  def dante_farend_output
+    get("/api/audio/outputs/dante/farEnd")
+  end
+
+  def set_dante_farend_output(gain : Int32? = nil, noiseGateEnabled : Bool? = nil, equalizerEnabled : Bool? = nil, delay : Int32? = nil)
+    body = {} of String => JSON::Any::Type
+    body["gain"] = gain.to_i64 if gain
+    body["noiseGateEnabled"] = noiseGateEnabled unless noiseGateEnabled.nil?
+    body["equalizerEnabled"] = equalizerEnabled unless equalizerEnabled.nil?
+    body["delay"] = delay.to_i64 if delay
+    put("/api/audio/outputs/dante/farEnd", body: body.to_json, headers: {"Content-Type" => "application/json"})
+  end
+
+  def dante_local_output
+    get("/api/audio/outputs/dante/local")
+  end
+
+  def set_dante_local_output(gain : Int32? = nil, noiseGateEnabled : Bool? = nil, equalizerEnabled : Bool? = nil, voiceLiftEnabled : Bool? = nil, delay : Int32? = nil)
+    body = {} of String => JSON::Any::Type
+    body["gain"] = gain.to_i64 if gain
+    body["noiseGateEnabled"] = noiseGateEnabled unless noiseGateEnabled.nil?
+    body["equalizerEnabled"] = equalizerEnabled unless equalizerEnabled.nil?
+    body["voiceLiftEnabled"] = voiceLiftEnabled unless voiceLiftEnabled.nil?
+    body["delay"] = delay.to_i64 if delay
+    put("/api/audio/outputs/dante/local", body: body.to_json, headers: {"Content-Type" => "application/json"})
+  end
+
+  # Audio Inputs
+  def reference_input
+    get("/api/audio/inputs/dante/reference")
+  end
+
+  def set_reference_input(gain : Int32? = nil, farEndAutoAdjustEnabled : Bool? = nil)
+    body = {} of String => JSON::Any::Type
+    body["gain"] = gain.to_i64 if gain
+    body["farEndAutoAdjustEnabled"] = farEndAutoAdjustEnabled unless farEndAutoAdjustEnabled.nil?
+    put("/api/audio/inputs/dante/reference", body: body.to_json, headers: {"Content-Type" => "application/json"})
+  end
+
+  # Audio Processing
+  def voice_lift
+    get("/api/audio/voiceLift")
+  end
+
+  def set_voice_lift(emergencyMuteThreshold : Int32? = nil, emergencyMuteTime : Int32? = nil)
+    body = {} of String => JSON::Any::Type
+    body["emergencyMuteThreshold"] = emergencyMuteThreshold.to_i64 if emergencyMuteThreshold
+    body["emergencyMuteTime"] = emergencyMuteTime.to_i64 if emergencyMuteTime
+    put("/api/audio/voiceLift", body: body.to_json, headers: {"Content-Type" => "application/json"})
+  end
+
+  def equalizer
+    get("/api/audio/equalizer")
+  end
+
+  def set_equalizer(gains : Array(Int32))
+    put("/api/audio/equalizer", body: {gains: gains}.to_json, headers: {"Content-Type" => "application/json"})
+  end
+
+  def noise_gate
+    get("/api/audio/noiseGate")
+  end
+
+  def set_noise_gate(threshold : Int32? = nil, holdTime : Int32? = nil)
+    body = {} of String => JSON::Any::Type
+    body["threshold"] = threshold.to_i64 if threshold
+    body["holdTime"] = holdTime.to_i64 if holdTime
+    put("/api/audio/noiseGate", body: body.to_json, headers: {"Content-Type" => "application/json"})
+  end
+
+  def denoiser
+    get("/api/audio/inputs/microphone/denoiser")
+  end
+
+  def set_denoiser(setting : String)
+    put("/api/audio/inputs/microphone/denoiser", body: {setting: setting}.to_json, headers: {"Content-Type" => "application/json"})
+  end
+
+  # Zones
+  def exclusion_zones
+    get("/api/audio/inputs/microphone/exclusionZones")
+  end
+
+  def exclusion_zone(id : Int32)
+    get("/api/audio/inputs/microphone/exclusionZones/#{id}")
+  end
+
+  def set_exclusion_zone(id : Int32, enabled : Bool? = nil, elevation_min : Int32? = nil, elevation_max : Int32? = nil, azimuth_min : Int32? = nil, azimuth_max : Int32? = nil)
+    body = {} of String => (String | Int64 | Bool | Hash(String, Int64))
+    body["enabled"] = enabled unless enabled.nil?
+
+    if elevation_min || elevation_max
+      elevation = {} of String => Int64
+      elevation["min"] = elevation_min.to_i64 if elevation_min
+      elevation["max"] = elevation_max.to_i64 if elevation_max
+      body["elevation"] = elevation
+    end
+
+    if azimuth_min || azimuth_max
+      azimuth = {} of String => Int64
+      azimuth["min"] = azimuth_min.to_i64 if azimuth_min
+      azimuth["max"] = azimuth_max.to_i64 if azimuth_max
+      body["azimuth"] = azimuth
+    end
+
+    put("/api/audio/inputs/microphone/exclusionZones/#{id}", body: body.to_json, headers: {"Content-Type" => "application/json"})
+  end
+
+  def priority_zones
+    get("/api/audio/inputs/microphone/priorityZones")
+  end
+
+  def priority_zone(id : Int32)
+    get("/api/audio/inputs/microphone/priorityZones/#{id}")
+  end
+
+  def set_priority_zone(id : Int32, enabled : Bool? = nil, weight : Float32? = nil, elevation_min : Int32? = nil, elevation_max : Int32? = nil, azimuth_min : Int32? = nil, azimuth_max : Int32? = nil)
+    body = {} of String => (String | Int64 | Bool | Float64 | Hash(String, Int64))
+    body["enabled"] = enabled unless enabled.nil?
+    body["weight"] = weight.to_f64 if weight
+
+    if elevation_min || elevation_max
+      elevation = {} of String => Int64
+      elevation["min"] = elevation_min.to_i64 if elevation_min
+      elevation["max"] = elevation_max.to_i64 if elevation_max
+      body["elevation"] = elevation
+    end
+
+    if azimuth_min || azimuth_max
+      azimuth = {} of String => Int64
+      azimuth["min"] = azimuth_min.to_i64 if azimuth_min
+      azimuth["max"] = azimuth_max.to_i64 if azimuth_max
+      body["azimuth"] = azimuth
+    end
+
+    put("/api/audio/inputs/microphone/priorityZones/#{id}", body: body.to_json, headers: {"Content-Type" => "application/json"})
+  end
+
+  # Device LEDs
+  def led_ring
+    get("/api/device/leds/ring")
+  end
+
+  def set_led_ring(brightness : Int32? = nil, showFarendActivity : Bool? = nil, micOn_color : String? = nil, micMute_color : String? = nil, micCustom_enabled : Bool? = nil, micCustom_color : String? = nil)
+    body = {} of String => (String | Int64 | Bool | Hash(String, String) | Hash(String, String | Bool))
+    body["brightness"] = brightness.to_i64 if brightness
+    body["showFarendActivity"] = showFarendActivity unless showFarendActivity.nil?
+
+    if micOn_color
+      body["micOn"] = {"color" => micOn_color}
+    end
+
+    if micMute_color
+      body["micMute"] = {"color" => micMute_color}
+    end
+
+    if micCustom_enabled || micCustom_color
+      micCustom = {} of String => (String | Bool)
+      micCustom["enabled"] = micCustom_enabled unless micCustom_enabled.nil?
+      micCustom["color"] = micCustom_color if micCustom_color
+      body["micCustom"] = micCustom
+    end
+
+    put("/api/device/leds/ring", body: body.to_json, headers: {"Content-Type" => "application/json"})
+  end
+
+  # Device Power
+  def poe_daisychain
+    get("/api/device/power/poe/daisychain")
+  end
+
+  # License Agreements
+  def license_agreements_hash
+    get("/api/device/licenseAgreements/hash")
+  end
+
+  def license_agreements_licenses
+    get("/api/device/licenseAgreements/licenses")
+  end
+
+  # === Convenience Methods ===
+
+  # Mute/unmute microphone
+  def mute(state : Bool = true)
+    set_audio_global_mute(state)
+  end
+
+  def unmute
+    mute(false)
+  end
+
+  # Device identification (visual LED flash)
+  def identify(enable : Bool = true)
+    set_device_identification(enable)
+  end
+
+  # Quick beam configuration
+  def configure_beam(installation_type : String, detection_threshold : String = "NormalRoom", offset : Int32 = 0)
+    set_microphone_beam(installation_type, detection_threshold, offset)
+  end
+
+  # LED ring brightness control
+  def set_brightness(level : Int32)
+    set_led_ring(brightness: level)
+  end
+
+  # Set LED colors for different states
+  def set_led_colors(mic_on : String? = nil, mic_mute : String? = nil)
+    set_led_ring(micOn_color: mic_on, micMute_color: mic_mute)
+  end
+
+  # Quick equalizer presets
+  def equalizer_flat
+    set_equalizer([0, 0, 0, 0, 0, 0, 0])
+  end
+
+  def equalizer_voice_boost
+    # Boost 1-4kHz range for voice clarity
+    set_equalizer([0, 0, 2, 3, 3, 1, 0])
+  end
+
+  # Room sensing configuration
+  def configure_room_sensing(trigger_time : Int32 = 15, release_time : Int32 = 300, threshold : Int32 = 10)
+    # Note: This would require a PUT endpoint for room config which isn't in the current API
+    # For now, users would need to use the web interface to configure this
+    logger.info { "Room sensing config - use web interface: trigger=#{trigger_time}s, release=#{release_time}s, threshold=#{threshold}dB" }
+  end
+
+  # Get current audio status summary
+  def audio_status
+    {
+      "muted"            => self[:global_mute]?.try(&.as_h).try(&.["enabled"]?),
+      "beam_direction"   => self[:beam_direction]?,
+      "microphone_level" => self[:microphone_level]?,
+      "room_in_use"      => self[:room_in_use]?.try(&.as_h).try(&.["active"]?),
+      "room_activity"    => self[:room_activity_level]?.try(&.as_h).try(&.["peak"]?),
+    }
+  end
+
+  # Get device information summary
+  def device_info
+    {
+      "identity" => self[:device_identity]?,
+      "site"     => {
+        "name"     => self[:device_name]?,
+        "location" => self[:device_location]?,
+        "position" => self[:device_position]?,
+      },
+      "state" => self[:device_state]?,
+    }
   end
 
   # === Subscription Management ===
@@ -173,16 +506,58 @@ class Sennheiser::SSCv2Driver < PlaceOS::Driver
     when "/api/device/site"
       begin
         site_data = Sennheiser::SSCv2::DeviceSite.from_json(data.to_json)
-        self[:device_name] = site_data.name
+        self[:device_name] = site_data.deviceName
         self[:device_location] = site_data.location
-        self[:device_site] = site_data.site
+        self[:device_position] = site_data.position
       rescue JSON::ParseException
         logger.warn { "Failed to parse device site data: #{data}" }
       end
-    when "/api/device/info"
-      self[:device_info] = data.as_h
-    when "/api/device/status"
-      self[:device_status] = data.as_h
+    when "/api/device/identity"
+      self[:device_identity] = data.as_h
+    when "/api/device/identification"
+      self[:device_identification] = data.as_h
+    when "/api/device/state"
+      self[:device_state] = data.as_h
+    when "/api/audio/outputs/global/mute"
+      self[:global_mute] = data.as_h
+    when "/api/audio/inputs/microphone/beam"
+      self[:microphone_beam] = data.as_h
+    when "/api/audio/inputs/microphone/beam/direction"
+      self[:beam_direction] = data.as_h
+    when "/api/audio/inputs/microphone/level"
+      self[:microphone_level] = data.as_h
+    when "/api/audio/inputs/reference/level"
+      self[:reference_level] = data.as_h
+    when "/api/audio/roomInUse"
+      self[:room_in_use] = data.as_h
+    when "/api/audio/roomInUse/activityLevel"
+      self[:room_activity_level] = data.as_h
+    when "/api/audio/outputs/analog"
+      self[:analog_output] = data.as_h
+    when "/api/audio/outputs/dante/farEnd"
+      self[:dante_farend_output] = data.as_h
+    when "/api/audio/outputs/dante/local"
+      self[:dante_local_output] = data.as_h
+    when "/api/audio/inputs/dante/reference"
+      self[:reference_input] = data.as_h
+    when "/api/audio/voiceLift"
+      self[:voice_lift] = data.as_h
+    when "/api/audio/equalizer"
+      self[:equalizer] = data.as_h
+    when "/api/audio/noiseGate"
+      self[:noise_gate] = data.as_h
+    when "/api/device/leds/ring"
+      self[:led_ring] = data.as_h
+    when "/api/device/power/poe/daisychain"
+      self[:poe_daisychain] = data.as_h
+    when "/api/audio/inputs/microphone/denoiser"
+      self[:denoiser] = data.as_h
+    when .starts_with?("/api/audio/inputs/microphone/exclusionZones/")
+      zone_id = resource_path.split("/").last
+      self["exclusion_zone_#{zone_id}"] = data.as_h
+    when .starts_with?("/api/audio/inputs/microphone/priorityZones/")
+      zone_id = resource_path.split("/").last
+      self["priority_zone_#{zone_id}"] = data.as_h
     else
       # Store generic resource data
       resource_key = resource_path.gsub("/", "_").gsub(/^_/, "")
--- drivers/sennheiser/sscv2_models.cr
+++ drivers/sennheiser/sscv2_models.cr
@@ -79,11 +79,298 @@ module Sennheiser::SSCv2
   struct DeviceSite
     include JSON::Serializable
 
-    getter name : String
+    getter deviceName : String
     getter location : String
-    getter site : String
+    getter position : String
 
-    def initialize(@name : String, @location : String, @site : String)
+    def initialize(@deviceName : String, @location : String, @position : String)
+    end
+  end
+
+  # Device identity
+  struct DeviceIdentity
+    include JSON::Serializable
+
+    getter product : String
+    getter hardwareRevision : String
+    getter serial : String
+    getter vendor : String
+
+    def initialize(@product : String, @hardwareRevision : String, @serial : String, @vendor : String)
+    end
+  end
+
+  # Device identification (visual indicator)
+  struct DeviceIdentification
+    include JSON::Serializable
+
+    getter visual : Bool
+
+    def initialize(@visual : Bool)
+    end
+  end
+
+  # Device state
+  struct DeviceState
+    include JSON::Serializable
+
+    getter state : String
+    getter warnings : Array(String)
+
+    def initialize(@state : String, @warnings : Array(String))
+    end
+  end
+
+  # Audio global mute
+  struct AudioGlobalMute
+    include JSON::Serializable
+
+    getter enabled : Bool
+
+    def initialize(@enabled : Bool)
+    end
+  end
+
+  # Microphone beam settings
+  struct MicrophoneBeam
+    include JSON::Serializable
+
+    getter installationType : String
+    getter sourceDetectionThreshold : String
+    getter offset : Int32
+
+    def initialize(@installationType : String, @sourceDetectionThreshold : String, @offset : Int32)
+    end
+  end
+
+  # Beam direction
+  struct BeamDirection
+    include JSON::Serializable
+
+    getter azimuth : Int32
+    getter elevation : Int32
+    getter beamFreezeActive : Bool
+
+    def initialize(@azimuth : Int32, @elevation : Int32, @beamFreezeActive : Bool)
+    end
+  end
+
+  # Audio level
+  struct AudioLevel
+    include JSON::Serializable
+
+    getter peak : Int32?
+    getter rms : Int32?
+
+    def initialize(@peak : Int32? = nil, @rms : Int32? = nil)
+    end
+  end
+
+  # Room in use
+  struct RoomInUse
+    include JSON::Serializable
+
+    getter active : Bool
+
+    def initialize(@active : Bool)
+    end
+  end
+
+  # Room in use activity level
+  struct RoomInUseActivityLevel
+    include JSON::Serializable
+
+    getter peak : Int32
+
+    def initialize(@peak : Int32)
+    end
+  end
+
+  # Room in use config
+  struct RoomInUseConfig
+    include JSON::Serializable
+
+    getter triggerTime : Int32
+    getter releaseTime : Int32
+    getter threshold : Int32
+
+    def initialize(@triggerTime : Int32, @releaseTime : Int32, @threshold : Int32)
+    end
+  end
+
+  # Analog output settings
+  struct AnalogOutput
+    include JSON::Serializable
+
+    getter gain : Int32
+    getter switch : String
+
+    def initialize(@gain : Int32, @switch : String)
+    end
+  end
+
+  # Dante output settings
+  struct DanteOutput
+    include JSON::Serializable
+
+    getter gain : Int32
+    getter noiseGateEnabled : Bool
+    getter equalizerEnabled : Bool
+    getter delay : Int32
+    getter voiceLiftEnabled : Bool?
+
+    def initialize(@gain : Int32, @noiseGateEnabled : Bool, @equalizerEnabled : Bool, @delay : Int32, @voiceLiftEnabled : Bool? = nil)
+    end
+  end
+
+  # Reference input settings
+  struct ReferenceInput
+    include JSON::Serializable
+
+    getter gain : Int32
+    getter farEndAutoAdjustEnabled : Bool
+
+    def initialize(@gain : Int32, @farEndAutoAdjustEnabled : Bool)
+    end
+  end
+
+  # Voice lift settings
+  struct VoiceLift
+    include JSON::Serializable
+
+    getter emergencyMuteThreshold : Int32
+    getter emergencyMuteTime : Int32
+
+    def initialize(@emergencyMuteThreshold : Int32, @emergencyMuteTime : Int32)
+    end
+  end
+
+  # Equalizer settings
+  struct Equalizer
+    include JSON::Serializable
+
+    getter gains : Array(Int32)
+
+    def initialize(@gains : Array(Int32))
+    end
+  end
+
+  # Noise gate settings
+  struct NoiseGate
+    include JSON::Serializable
+
+    getter threshold : Int32
+    getter holdTime : Int32
+
+    def initialize(@threshold : Int32, @holdTime : Int32)
+    end
+  end
+
+  # LED ring color
+  struct LedColor
+    include JSON::Serializable
+
+    getter color : String
+
+    def initialize(@color : String)
+    end
+  end
+
+  # LED ring custom
+  struct LedCustom
+    include JSON::Serializable
+
+    getter enabled : Bool
+    getter color : String
+
+    def initialize(@enabled : Bool, @color : String)
+    end
+  end
+
+  # LED ring settings
+  struct LedRing
+    include JSON::Serializable
+
+    getter brightness : Int32
+    getter showFarendActivity : Bool
+    getter micOn : LedColor
+    getter micMute : LedColor
+    getter micCustom : LedCustom
+
+    def initialize(@brightness : Int32, @showFarendActivity : Bool, @micOn : LedColor, @micMute : LedColor, @micCustom : LedCustom)
+    end
+  end
+
+  # PoE daisy chain info
+  struct PoeDaisyChain
+    include JSON::Serializable
+
+    getter sufficientPower : Bool
+    getter inUse : Bool
+
+    def initialize(@sufficientPower : Bool, @inUse : Bool)
+    end
+  end
+
+  # Zone angle range
+  struct AngleRange
+    include JSON::Serializable
+
+    getter min : Int32
+    getter max : Int32
+
+    def initialize(@min : Int32, @max : Int32)
+    end
+  end
+
+  # Exclusion zone
+  struct ExclusionZone
+    include JSON::Serializable
+
+    getter id : Int32
+    getter enabled : Bool
+    getter elevation : AngleRange
+    getter azimuth : AngleRange
+
+    def initialize(@id : Int32, @enabled : Bool, @elevation : AngleRange, @azimuth : AngleRange)
+    end
+  end
+
+  # Priority zone
+  struct PriorityZone
+    include JSON::Serializable
+
+    getter id : Int32
+    getter enabled : Bool
+    getter weight : Float32
+    getter elevation : AngleRange
+    getter azimuth : AngleRange
+
+    def initialize(@id : Int32, @enabled : Bool, @weight : Float32, @elevation : AngleRange, @azimuth : AngleRange)
+    end
+  end
+
+  # Denoiser setting
+  struct Denoiser
+    include JSON::Serializable
+
+    getter setting : String
+
+    def initialize(@setting : String)
+    end
+  end
+
+  # Firmware update state
+  struct FirmwareUpdateState
+    include JSON::Serializable
+
+    getter deviceVersion : String
+    getter danteVersion : String
+    getter state : String
+    getter progress : Int32
+    getter lastStatus : String
+
+    def initialize(@deviceVersion : String, @danteVersion : String, @state : String, @progress : Int32, @lastStatus : String)
     end
   end
 
