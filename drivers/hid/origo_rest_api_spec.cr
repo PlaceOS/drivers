6746d45a9a9ab65dba038b88d0afe886ee9c9320
Stephen von Takach <steve@place.technology>
2025-07-30 10:39:48 +1000

    fix(hid): schema responses are in arrays

--- drivers/hid/origo_rest_api_spec.cr
+++ drivers/hid/origo_rest_api_spec.cr
@@ -34,4 +34,47 @@ DriverSpecs.mock_driver "HID::OrigoRestApiDriver" do
     status["authenticated"].should eq true
     status["token_expires"]?.should_not be_nil
   end
+
+  # Test getting a part
+  it "should obtain some part details" do
+    resp = exec(:get_part, "4924_644243")
+    expect_http_request do |request, response|
+      request.method.should eq "GET"
+      request.path.should eq "/credential-management/customer/test/part-number/4924_644243"
+
+      response.status_code = 200
+      response << %({
+        "schemas": [
+          "urn:hid:scim:api:ma:2.2:PartNumber"
+        ],
+        "urn:hid:scim:api:ma:2.2:PartNumber": [
+          {
+            "meta": {
+              "resourceType": "PartNumber",
+              "lastModified": "2017-12-03T14:03:25Z",
+              "location": "https://cert.mi.api.origo.hidglobal.com/credential-management/customer/test/part-number/4924_644243"
+            },
+            "id": "4924_644243",
+            "partNumber": "MID-SUB-CRD_FTPN_644245",
+            "friendlyName": "MOB0022",
+            "availableQty": 445,
+            "offset": 0,
+            "prefix": "",
+            "suffix": "",
+            "nextNumber": 56,
+            "lastNumber": 500,
+            "badge_type": "FTPN",
+            "programmingData": {
+              "facility code": "99",
+              "incremental_card id number": "1",
+              "format_number": "TRK-H10301"
+            },
+            "replenishmentState": "Not Needed"
+          }
+        ]
+      })
+    end
+
+    resp.get["partNumber"].should eq "MID-SUB-CRD_FTPN_644245"
+  end
 end
