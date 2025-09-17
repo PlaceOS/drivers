cf717e9c81db4f2268784338acd7f67feba3cd18
Stephen von Takach <steve@place.technology>
2025-09-17 14:21:21 +1000

    fix(crestron/occupancy_sensor): queue remains online after a network disconnect

--- drivers/crestron/occupancy_sensor.cr
+++ drivers/crestron/occupancy_sensor.cr
@@ -3,7 +3,7 @@ require "placeos-driver/interface/sensor"
 require "./cres_next_auth"
 
 # This device doesn't seem to support a websocket interface
-# and relies on long polling
+# and relies on long polling.
 
 class Crestron::OccupancySensor < PlaceOS::Driver
   include Crestron::CresNextAuth
