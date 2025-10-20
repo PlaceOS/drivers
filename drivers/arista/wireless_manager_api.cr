23e9405a52bf66e08c09a8adb3424ea4a7dce0e0
Stephen von Takach <steve@place.technology>
2025-10-20 15:31:43 +1100

    feat(arista/wireless_manager_api): add driver

---
+++ drivers/arista/wireless_manager_api.cr
@@ -0,0 +1,118 @@
+require "placeos-driver"
+require "./wireless_manager_models"
+
+# Documentation: https://apihelp.wifi.arista.com/usecases#toc1
+# https://apihelp.wifi.arista.com/api/wm/clients/15
+
+class Arista::WirelessManagerAPI < PlaceOS::Driver
+  descriptive_name "Arista Wireless Manager"
+  generic_name :Arista_Wifi
+
+  uri_base "https://launchpad.wifi.arista.com/"
+  description %(
+    add the api key id and secret and use launch pad
+    to discover the wireless manager service URI
+  )
+
+  default_settings({
+    key_id:    "KEY-ATNxxxxxxxx",
+    key_value: "ddfxxxxxxxx",
+    key_type:  "apikeycredentials",
+  })
+
+  def on_update
+    @key_type = setting?(String, :key_type).presence || "apikeycredentials"
+    @key_value = setting(String, :key_value)
+    @key_id = setting(String, :key_id)
+
+    schedule.clear
+    schedule.every(50.minutes, immediate: true) { new_session }
+  end
+
+  getter? authenticated : Bool = false
+  getter key_type : String = ""
+  getter key_id : String = ""
+  @key_value : String = ""
+
+  # make this request to obtain the base URI for the module
+  def launchpad_service_locations
+    response = HTTP::Client.post("https://launchpad.wifi.arista.com/api/v2/session", body: {
+      type:     key_type,
+      keyId:    key_id,
+      keyValue: @key_value,
+      timeout:  3600,
+    }.to_json)
+    raise "session failed with: #{response.status} (#{response.status_code})\n#{response.body}" unless response.success?
+    cookies = HTTP::Cookies.from_server_headers(response.headers)
+    headers = cookies.add_request_headers(HTTP::Headers.new)
+
+    response = HTTP::Client.get("https://launchpad.wifi.arista.com/rest/api/v2/services?type=amc", headers: headers)
+    raise "services failed with: #{response.status} (#{response.status_code})\n#{response.body}" unless response.success?
+    JSON.parse(response.body)
+  end
+
+  def new_session
+    response = post("/wifi/api/session", body: {
+      type:             key_type,
+      keyId:            key_id,
+      keyValue:         @key_value,
+      clientIdentifier: "PlaceOS",
+      timeout:          3600,
+    }.to_json)
+
+    if response.success?
+      self[:authenticated] = @authenticated = true
+    else
+      self[:authenticated] = @authenticated = false
+      queue.set_connected(false)
+      raise "session failed with: #{response.status} (#{response.status_code})\n#{response.body}"
+    end
+  end
+
+  protected def check(response)
+    return response if response.success?
+    @authenticated = false if response.status.unauthorized?
+    raise "locations failed with: #{response.status} (#{response.status_code})\n#{response.body}"
+  end
+
+  def locations
+    new_session unless authenticated?
+    response = check get("/new/wifi/api/locations")
+    Location.from_json(response.body)
+  end
+
+  def client_positions(at_location : String | Int64? = nil)
+    new_session unless authenticated?
+
+    query = URI::Params.build do |form|
+      form.add("pagesize", "1000")
+      form.add("filter", %({"property":"activestatus","operator":"=","value":[true]}))
+      form.add("locationid", at_location.to_s) if at_location
+    end
+
+    locations = [] of ClientDetails
+
+    # requires two requests per-page
+    next_page = URI.parse("/new/wifi/api/locations?#{query}")
+    loop do
+      break unless next_page
+
+      # Request the locations
+      response = check post(next_page.request_target)
+      loc_req = LocationsRequest.from_json(response.body)
+
+      # get the location data
+      data_url = URI.parse(loc_req.result_url).request_target
+      response = check get(data_url)
+
+      # process extract the device locations
+      loc_response = LocationTracking.from_json(response.body)
+      locations.concat loc_response.results.flat_map(&.clients)
+
+      # get the next page
+      next_page = loc_req.next_uri
+    end
+
+    locations
+  end
+end
---
+++ drivers/arista/wireless_manager_models.cr
@@ -0,0 +1,199 @@
+require "json"
+require "uri"
+
+module Arista
+  enum Unit
+    Feet
+    Meter
+  end
+
+  struct ID
+    include JSON::Serializable
+
+    getter id : Int64
+    getter type : String
+  end
+
+  struct GeoInfo
+    include JSON::Serializable
+
+    coordinates : Coordinates
+  end
+
+  struct Coordinates
+    include JSON::Serializable
+
+    getter lat : Float64
+    getter lng : Float64
+    # getter altitude : Int32
+  end
+
+  struct Location
+    include JSON::Serializable
+
+    getter type : String
+    getter name : String
+
+    @[JSON::Field(key: "accessibleToUser")]
+    getter visible : Bool
+
+    @[JSON::Field(key: "timezoneId")]
+    getter timezone : String?
+
+    # "{\"coordinates\": {\"lat\": -37.78754839955705, \"lng\": 175.32221218440074, \"altitude\": 0}}"
+    @[JSON::Field(key: "geoInfo")]
+    getter geo_info_raw : String?
+
+    getter children : Array(Location)
+
+    def geo_info : Coordinates?
+      json = geo_info_raw
+      return unless json
+
+      GeoInfo.from_json(json).coordinates
+    end
+  end
+
+  struct LocationsRequest
+    include JSON::Serializable
+
+    @[JSON::Field(key: "nextLink")]
+    getter next_link : String?
+
+    @[JSON::Field(key: "pollingUrl")]
+    getter result_url : String
+
+    @[JSON::Field(key: "deviceCount")]
+    getter page_size : Int32
+
+    @[JSON::Field(key: "totalDeviceCount")]
+    getter total_results : Int32
+
+    def next_uri : URI?
+      if url = next_link
+        URI.parse(url)
+      end
+    end
+  end
+
+  struct LocationTracking
+    include JSON::Serializable
+
+    @[JSON::Field(key: "locationTrackingResult")]
+    getter results : Array(LocationResult)
+  end
+
+  struct LocationResult
+    include JSON::Serializable
+
+    @[JSON::Field(key: "devicesType")]
+    getter type : String
+
+    @[JSON::Field(key: "locationId")]
+    getter location : ID
+
+    getter clients : Array(ClientDetails)
+  end
+
+  struct ClientDetails
+    include JSON::Serializable
+
+    getter device : ClientDevice
+    getter position : ClientPosition
+    getter proximity : Array(ClientProximity)
+  end
+
+  struct ClientDevice
+    include JSON::Serializable
+
+    @[JSON::Field(key: "locationId")]
+    getter location : ID
+
+    @[JSON::Field(key: "boxId")]
+    getter box_id : Int64
+    getter name : String
+
+    @[JSON::Field(key: "userName")]
+    getter username : String?
+
+    # "Android"
+    @[JSON::Field(key: "osType")]
+    getter os : String?
+
+    @[JSON::Field(key: "macAddress")]
+    getter macaddress : String
+
+    @[JSON::Field(key: "vlanId")]
+    getter macaddress : Int64?
+
+    @[JSON::Field(key: "ipAddress")]
+    getter ip_v4 : String?
+
+    @[JSON::Field(key: "ipv6Addresses")]
+    getter ip_v6 : Array(String)?
+  end
+
+  struct ClientPosition
+    include JSON::Serializable
+
+    struct Coordinates
+      include JSON::Serializable
+
+      getter unit : Unit
+
+      @[JSON::Field(key: "xCordinate")]
+      getter x : Float64
+
+      @[JSON::Field(key: "yCordinate")]
+      getter y : Float64
+
+      @[JSON::Field(key: "xPosition")]
+      getter x_pos : Float64
+
+      @[JSON::Field(key: "yPosition")]
+      getter y_pos : Float64
+    end
+
+    @[JSON::Field(key: "errorCode")]
+    getter error : String?
+
+    # nil if there is an error
+    getter coordinates : Coordinates?
+  end
+
+  struct ClientProximity
+    include JSON::Serializable
+
+    struct ObservedDeviceDistance
+      include JSON::Serializable
+
+      struct Distance
+        include JSON::Serializable
+
+        getter value : Float64
+        getter unit : Unit
+      end
+
+      getter min : Distance
+      getter max : Distance
+    end
+
+    struct ObservingDevice
+      include JSON::Serializable
+
+      @[JSON::Field(key: "locationId")]
+      getter location : ID
+
+      @[JSON::Field(key: "boxId")]
+      getter box_id : Int64
+      getter name : String
+      getter macaddress : String
+    end
+
+    @[JSON::Field(key: "observedDeviceDistance")]
+    getter distance : ObservedDeviceDistance
+
+    @[JSON::Field(key: "observingDevice")]
+    getter device : ObservingDevice
+  end
+end
