2549313b29d1be035025514eb535a1c3193c1e93
Stephen von Takach <steve@place.technology>
2025-12-01 11:45:45 +1100

    feat(milesight): add door counter

--- drivers/milesight/webhook_receiver_spec.cr
+++ drivers/milesight/webhook_receiver_spec.cr
@@ -1,4 +1,6 @@
 require "placeos-driver/spec"
+require "./cayenne_lpp_models"
+require "./webhook_models"
 
 DriverSpecs.mock_driver "Milesight::Webhook" do
   payload = %({
@@ -39,4 +41,9 @@ DriverSpecs.mock_driver "Milesight::Webhook" do
   status["24e124443f171044.Temperature"]?.should eq 23.7
 
   exec(:sensors).get.as_a.size.should eq 4
+
+  payload = Milesight::WebhookPayload.from_json "{\"applicationID\":\"1\",\"applicationName\":\"PlaceOS\",\"deviceName\":\"TC_4.15_VS351_Door_Counters\",\"devEUI\":\"24e124799f179505\",\"rxInfo\":[{\"mac\":\"24e124fffef1ad25\",\"time\":\"2025-11-30T22:09:57.737969Z\",\"rssi\":-88,\"loRaSNR\":13,\"name\":\"Local Gateway\",\"latitude\":-30.715,\"longitude\":15.313,\"altitude\":57}],\"txInfo\":{\"frequency\":923200000,\"dataRate\":{\"modulation\":\"LORA\",\"bandwidth\":125,\"spreadFactor\":7},\"adr\":true,\"codeRate\":\"4/5\"},\"fCnt\":1389,\"fPort\":85,\"data\":\"BcwAAAAAAXVj\",\"time\":\"2025-11-30T22:09:57.737969Z\"}"
+  bytes = Base64.decode(payload.data)
+  io = IO::Memory.new(bytes)
+  puts io.read_bytes(Milesight::Frame).items.inspect
 end
