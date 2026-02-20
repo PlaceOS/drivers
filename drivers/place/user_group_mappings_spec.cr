955384d6d237653a1d8fdd6846756ae7f489dad9
William Le <will@place.tech>
2026-02-20 20:41:52 +0800

    spec(place/user_group_mappings): +1 test

--- drivers/place/user_group_mappings_spec.cr
+++ drivers/place/user_group_mappings_spec.cr
@@ -46,4 +46,17 @@ DriverSpecs.mock_driver "Place::LogicExample" do
 
   exec(:check_user, "user-1234").get
   system(:StaffAPI_1)["user-1234"].should eq({"groups" => ["existing_group_to_be_ignored", "intune"]}.to_json)
+
+
+  settings({
+    group_mappings: {
+      "5f4694-96f3-4209-a432-b04ac06ca7" => {"place_id" => "intune"},
+      "admins"                           => {"place_id" => "im an admin"},
+    },
+    ignore_placeos_groups: [] of String,
+    authority_id: "authority-12345",
+  })
+
+  exec(:check_user, "user-1234").get
+  system(:StaffAPI_1)["user-1234"].should eq({"groups" => ["intune"]}.to_json)
 end
